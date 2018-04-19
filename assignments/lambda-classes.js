8c6ada73e0ab65bb972363814480ab7c755ec566
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-04-19 14:49:57 -0400

    Test students

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -86,6 +86,39 @@ console.log(frank.speak());
 console.log(jacob);
 console.log(jacob.speak());
 
+// Test student
+const george = new Student({
+  'name': 'George',
+  'age': '25',
+  'location': 'New York, NY',
+  'gender': 'M',
+  'previousBackground': 'Math Tutor',
+  'className': 'CS20',
+  'favSubjects': ['Python', 'R']
+});
+
+const paige = new Student({
+  'name': 'Paige',
+  'age': '18',
+  'location': 'Reno, NV',
+  'gender': 'F',
+  'previousBackground': 'High School Student',
+  'className': 'CS11',
+  'favSubjects': ['HTML', 'CSS', 'JS']
+});
+
+console.log(george);
+console.log(george.speak());
+console.log(george.listsSubjects());
+console.log(george.PRAssignment('Node I'));
+console.log(george.sprintChallenge('Node'));
+
+console.log(paige);
+console.log(paige.speak());
+console.log(paige.listsSubjects());
+console.log(paige.PRAssignment('Graphs II'));
+console.log(paige.sprintChallenge('Graphs'));
+
 // Test instructor
 const jackie = new Instructor({
   'name': 'Jackie',
