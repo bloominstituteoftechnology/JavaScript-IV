470b7c5404490c563a21e113ab5c315d2bc3070b
Thuy Pham <tpham045@hotmail.com>
2018-04-19 11:26:30 -0700

    feat: Person class completed

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -2,33 +2,44 @@
 // Students - extensions of Person
 // Project Managers - extensions of Instructors
 
-class Person {
-    constructor(){
 
+// Person receives name age location gender all as props
+// Person receives speak as a method.
+// This method logs out a phrase Hello my name is Fred, I am from Bedrock​​​​​ where name and location are the object's own props
+class Person {
+    constructor(obj) {
+        this.name = obj.name;
+        this.age = obj.age;
+        this.location = obj.location;
+        this.gender = obj.gender;
+    }
+    speak() {
+        return `Hello, my name is ${this.name}, I am from ${this.location}`;
     }
 }
 
 class Instructors extends Person {
-    constructor(){
+    constructor(instructorsObj){
+        super(instructorsObj);
 
     }
 }
 
 class Students extends Person {
-    constructor(){
-
+    constructor(studentsObj){
+        super(studentsObj);
     }
 }
 
-class ProjectManager extends Instructors {
-    constructor(){
-
+class ProjectManagers extends Instructors {
+    constructor(projectManagersObj){
+        super(projectManagersObj);
     }
 }
 
 // Objects
 
-const fred = new Instructor({
+const fred = new Instructors({
     name: 'Fred',
     location: 'Bedrock',
     age: 37,
@@ -38,9 +49,9 @@ const fred = new Instructor({
     catchPhrase: `Don't forget the homies`
 });
 
-const fred = new Student({
+const alice = new Students({
     name: 'Alice',
-    location: 'Jonhson',
+    location: 'Seattle',
     age: 27,
     gender: 'female',
     favLanguage: 'JavaScript',
@@ -48,9 +59,9 @@ const fred = new Student({
     catchPhrase: ``
 });
 
-const fred = new Student({
+const pete = new Students({
     name: 'Pete',
-    location: 'William',
+    location: 'Denver',
     age: 30,
     gender: 'male',
     favLanguage: 'JavaScript',
@@ -58,12 +69,15 @@ const fred = new Student({
     catchPhrase: ``
 });
 
-const fred = new ProjectManager({
+const jess = new ProjectManagers({
     name: 'Jess',
-    location: 'Rock',
+    location: 'New York',
     age: 34,
     gender: 'female',
     favLanguage: 'JavaScript',
     specialty: 'Front-end',
     catchPhrase: ``
 });
+
+// Tests
+console.log(pete.speak());
