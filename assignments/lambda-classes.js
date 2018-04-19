15e26c5fd236837ee2d5ca7ba24004cc88f40e47
jesuarva <jean.ariza@jesuarva.com>
2018-04-19 21:20:15 +0200

    lambda-classes -> END class Parent.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,43 @@
 // CODE here for your Lambda Classes
+
+/**
+ * ------- Person -------
+ */
+class Person {
+    constructor(options){
+        this.age = options.age;
+        this.location = options.location;
+        this.gender = options.gender;
+    }
+    speak () {
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}`)
+    }
+}
+/**
+ * ------- Instructor -------
+ */
+class Instructor extends Person {
+    constructor(options){
+        super(options)
+        this.speciality = options.speciality;
+        this.favLanguage = options.favLanguage;
+        this.catchPhrase = options.catchPhrase;
+    }
+    
+}
+/**
+ * ------- Student -------
+ */
+class Student {
+    constructor(options){
+        this. = options.;
+    }
+}
+/**
+ * ------- ProjectM -------
+ */
+class ProjectM {
+    constructor(options){
+        this. = options.;
+    }
+}
