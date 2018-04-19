0040ab034aff3388362744e4f6688a3ed6ac5120
Cassandra Lamendola <cass.lamendola@gmail.com>
2018-04-19 09:56:10 -0700

    Complete all lambda classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,59 @@
 // CODE here for your Lambda Classes
+class Person {
+	constructor(personAttributes) {
+		this.name = personAttributes.name;
+		this.age = personAttributes.age;
+		this.location = personAttributes.location;
+		this.gender = personAttributes.gender;
+	}
+	speak() {
+		return `Hello my name is ${this.name}, I am from ${location}`;
+	}
+}
+
+class Instructor extends Person {
+	constructor(instructorAttributes) {
+		super(instructorAttributes);
+		this.specialty = instructorAttributes.specialty;
+		this.favLanguage = instructorAttributes.favLanguage;
+		this.catchPhrase = instructorAttributes.catchPhrase;
+	}
+	demo(subject) {
+		return `Today we are learning about ${subject}`;
+	}
+	grade(student, subject) {
+		return `${student.name} receives a perfect score on ${subject}`;
+	}
+}
+
+class Student extends Person {
+	constructor(studentAttributes) {
+		super(studentAttributes);
+		this.previousBackground = studentAttributes.previousBackground;
+		this.className = studentAttributes.className;
+		this.favSubjects = studentAttributes.favSubjects;
+	}
+	listSubjects() {
+		this.favSubjects.forEach(subject => console.log(subject));
+	}
+	PRAssignment(subject) {
+		return `${this.name} has submitted a PR for ${subject}`;
+	}
+	sprintChallenge(subject) {
+		return `${this.name} has begun sprint challenge on ${subject}`;
+	}
+}
+
+class ProjectManager extends Instructor {
+	constructor(pmAttributes) {
+		super(pmAttributes);
+		this.gradClassName = pmAttributes.gradClassName;
+		this.favInstructor = pmAttributes.favInstructor;
+	}
+	standUp(channel) {
+		return `${this.name} announces to ${channel}, @channel standy times!`;
+	}
+	debugsCode(student, subject) {
+		return `${this.name} debugs ${student.name}'s code on ${subject}`;
+	}
+}
