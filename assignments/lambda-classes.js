d5185000480422b52cff0460cf06f26953f30a7e
mr-burton <taylormade800@gmail.com>
2018-04-19 13:12:17 -0700

    lambda classes updating file

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,2 +1,11 @@
 // CODE here for your Lambda Classes
-
+class Person {
+    constructor(personProps) {
+        this.gender = personProps.gender;
+        this.name = personProps.name;
+        this.age = personProps.age;
+    }
+    speak() {
+        return ``
+    }
+}
