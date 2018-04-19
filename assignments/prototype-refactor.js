8507badf40213bcea0319e3700086148135439f1
Andrew Kolumbic <andrewkolumbic@Andrews-Mac-mini.local>
2018-04-19 10:42:39 -0700

    initial push

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -88,3 +88,4 @@ console.log(archer.language); // Elvish
 console.log(archer.greet()); // Lilith offers a greeting in Elvish.
 console.log(mage.takeDamage()); // Bruce took damage.
 console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+
