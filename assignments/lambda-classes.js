1cbe48e777e097bf5f310f01c7c00ce34dfef39f
MacBookPro <macbookpro@MacBookPros-MacBook-Pro.local>
2018-04-19 13:36:25 -0500

    prototype-refactor.js complete

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,27 @@
 // CODE here for your Lambda Classes
+
+class Person {
+    constructor(){
+    this.name = attributes.name;
+    this.age = attributes.age;
+    this.location = attributes.location;
+    this.gender = attributes.gender;
+    }
+    speak() {
+        return `Hellon my name is ${this.name}, I am from
+        ${this.location}.`;
+    }
+}
+
+class Instructor extends Person {
+    constructor(instructorAttributes){
+        super(instructorAttributes);
+        this.specialty = instructorAttributes.specialty;
+        this.favLanguage = instructorAttributes.favLanguage;
+        this.catchPhrase = instructorAttributes.catchPhrase;
+    }
+    demo(subject) {
+        return `Today we are learning about ${this.subject}`;
+    }
+    grade(student, subject)
+}
