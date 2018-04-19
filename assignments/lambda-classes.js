9631423c5bc9c4583d4bec0cb2d4cfd19e33971e
MacBookPro <macbookpro@MacBookPros-MacBook-Pro.local>
2018-04-19 14:05:57 -0500

    lambda-class file classes complete.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,11 +1,11 @@
 // CODE here for your Lambda Classes
 
 class Person {
-    constructor(){
-    this.name = attributes.name;
-    this.age = attributes.age;
-    this.location = attributes.location;
-    this.gender = attributes.gender;
+    constructor(attributes){
+        this.name = attributes.name;
+        this.age = attributes.age;
+        this.location = attributes.location;
+        this.gender = attributes.gender;
     }
     speak() {
         return `Hellon my name is ${this.name}, I am from
@@ -23,5 +23,38 @@ class Instructor extends Person {
     demo(subject) {
         return `Today we are learning about ${this.subject}`;
     }
-    grade(student, subject)
+    grade(student, subject) {
+        return `${student.name} receives a perfect score on ${subject}`;
+    }
+}
+
+class Students extends Person {
+    constructor(studentAttributes){
+        super(studentAttributes);
+        this.className = studentAttributes.className;
+        this.favSubjects = studentAttributes.favSubjects;
+    }
+    listSubjects() {
+        return `${this.favSubjects}`;
+    }
+    PRAssignment(subject) {
+        return `${student.name} has submitted a PR for ${subject}`;
+    }
+    sprintChallenge(subject) {
+        return `${student.name} has begun sprint challenge on ${subject}`;
+    }        
+}
+
+class ProjectManager extends Instructor {
+    constructor(projectManagerAttributes){
+        super(instructorAttributes);
+        this.gradClassName = projectManagerAttributes.gradClassName;
+        this.favInstructor = projectManagerAttributes.favInstructor;
+    }
+    standUP(channle) {
+        return `${this.name} annouces to ${channel}, @channel standy times!`;
+    }
+    debugsCode(student, subject) {
+        return `${name} debugs ${student.name}'s code on ${subject}`;
+    }
 }
