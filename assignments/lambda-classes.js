3d9ad9c03138340b18d5abf21b0f6c499eddbff4
Andrew Kolumbic <andrewkolumbic@Andrews-Mac-mini.local>
2018-04-19 15:34:33 -0700

    completed lambda-classes.js challenge!

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -7,7 +7,7 @@ class Person {
         this.location = stuff.location;
         this.gender = stuff.gender;
     }
-    greeting() {
+    speak() {
       return `Hello, my name is ${this.name}, I am from ${this.location}.`;
     }
 }
@@ -20,10 +20,10 @@ class Instructor extends Person {
         this.catchPhrase = things.catchPhrase;
     }
     demo(subject){
-        return `Today we are learning about ${this.subject}.`; 
+        return `Today we are learning about ${subject}.`; 
     }
     grade(student, subject) {
-       return `${this.Student.name} receives a perfect score on ${this.subject}`;
+       return `${student.name} receives a perfect score on ${subject}`;
     }
 }
 
@@ -35,13 +35,13 @@ class Student extends Person {
         this.favSubjects = info.favSubjects;
     }
     listsSubjects(){
-        //logout all of the student's favSubjects one by one.
+        return this.favSubjects;
     }
     prAssignment(subject){
-       return `${this.Student.name} has submitted a PR for ${this.subject}.`;
+       return `${this.name} has submitted a PR for ${subject}.`;
     }
     sprintChallenge(subject){
-        return `${this.Student.name} has begun sprint challenge on ${this.subject}.`;
+        return `${this.name} has begun sprint challenge on ${subject}.`;
     }
 }
 
@@ -52,80 +52,88 @@ class ProjectManager extends Instructor {
         this.favInstructor = data.favInstructor;
     }
     standUp(slackChannel){
-        `${this.ProjectManager.name} announces to ${this.slackChannel}, @channel stand up times!`;
+        return `${this.name} announces to ${slackChannel}, @channel stand up times!`;
     }
-    debugsCode(subject){
-        `${this.ProjectManager.name} debugs ${this.Student.name}'s code on ${this.subject}`;
+    debugsCode(student, subject){
+        return `${this.name} debugs ${student.name}'s code on ${subject}`;
     }
 }
 
 // Instructors =============================================================
 const michael = new Instructor ({
-    name = 'Michael Scott',
-    age = 45,
-    location = 'Scranton',
-    gender = 'M',
-    specialty = 'Telling Jokes',
-    favLanguage = 'Pig Latin',
-    catchPhrase = `THATS WHAT SHE SAID!`
+    name : 'Michael Scott',
+    age : 45,
+    location : 'Scranton',
+    gender : 'M',
+    specialty : 'Telling Jokes',
+    favLanguage : 'Pig Latin',
+    catchPhrase : `THATS WHAT SHE SAID!`
 });
 
 const leslie = new Instructor ({
-    name = 'Leslie Knope',
-    age = 35,
-    location = 'Pawnee',
-    gender = 'F',
-    specialty = 'Making life better for Pawneeians',
-    favLanguage = 'French',
-    catchPhrase = `Oh Anne, you're so beautiful.`
+    name : 'Leslie Knope',
+    age : 35,
+    location : 'Pawnee',
+    gender : 'F',
+    specialty : 'Making life better for Pawneeians',
+    favLanguage :'French',
+    catchPhrase : `Oh Anne, you're so beautiful.`
 });
 
 //Students =============================================================
 const paulie = new Student ({
-    name = 'Paulie Walnuts',
-    age = 65,
-    location = 'New Jersey',
-    gender = 'M',
-    previousBackground = 'Mafia',
-    className = 'cs10',
-    favSubjects = 'History'
+    name : 'Paulie Walnuts',
+    age : 65,
+    location : 'New Jersey',
+    gender : 'M',
+    previousBackground : 'Mafia',
+    className : 'cs10',
+    favSubjects : ['History', 'Breaking Legs', 'Gambling']
 })
 
 const chris = new Student ({
-    name = 'Christopher Moltisanti',
-    age = 30,
-    location = 'New Jersey',
-    gender = 'M',
-    previousBackground = 'Mafia',
-    className = 'cs10',
-    favSubjects = 'Film'
+    name : 'Christopher Moltisanti',
+    age : 30,
+    location : 'New Jersey',
+    gender : 'M',
+    previousBackground : 'Mafia',
+    className : 'cs10',
+    favSubjects : 'Film'
 });
 
 const carm = new Student ({
-    name = 'Carmella Soprano',
-    age = 50,
-    location = 'New Jersey',
-    gender = 'F',
-    previousBackground = 'Housewife',
-    className = 'cs10',
-    favSubjects = 'Cooking'
+    name : 'Carmella Soprano',
+    age : 50,
+    location : 'New Jersey',
+    gender : 'F',
+    previousBackground : 'Housewife',
+    className : 'cs10',
+    favSubjects : 'Cooking'
 });
 
 //Project Managers =============================================================
 const tommy = new ProjectManager ({
-    name = 'Tommy Wiseu',
-    age = null,
-    location = 'Los Angeles',
-    gender = 'M',
-    gradClassName = 'cs1',
-    favInstructor = 'James Dean',
+    name : 'Tommy Wiseu',
+    age : null,
+    location : 'Los Angeles',
+    gender : 'M',
+    gradClassName : 'cs1',
+    favInstructor : 'James Dean',
 });
 
 const greg = new ProjectManager ({
-    name = 'Greg Sistero',
-    age = 29,
-    location = 'Los Angeles',
-    gender = 'M',
-    gradClassName = 'cs1',
-    favInstructor = 'Tommy Wiseu',
+    name : 'Greg Sistero',
+    age : 29,
+    location : 'Los Angeles',
+    gender : 'M',
+    gradClassName : 'cs1',
+    favInstructor : 'Tommy Wiseu',
 });
+
+console.log(greg.speak());
+console.log(paulie.speak()); 
+console.log(leslie.demo('Js-III')); 
+console.log(michael.grade(chris, 'Js-III')); 
+console.log(chris.prAssignment('Bootstrap')); 
+console.log(tommy.debugsCode(carm, 'Bootstrap')); 
+console.log(paulie.listsSubjects());
