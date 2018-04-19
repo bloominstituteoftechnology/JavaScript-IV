5b05a63819637560ddbf8ab8f84cd6606f986e8b
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-04-19 14:06:34 -0400

    Built Person class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,29 @@
 // CODE here for your Lambda Classes
+
+// ==== Person ====
+class Person {
+  constructor(personAttributes){
+    this.name = personAttributes.name;
+    this.age = personAttributes.age;
+    this.location = personAttributes.location;
+    this.gender = personAttributes.gender;
+  }
+  speak(){
+    return `Hello my name is ${this.name}, I am from ${this.location}.`;
+  }
+}
+
+// ==== Instructor ====
+class Instructor extends Person {
+
+}
+
+// ==== Student ====
+class Student extends Person {
+
+}
+
+// ==== Project Manager ====
+class ProjectManager extends Instructor {
+  
+}
