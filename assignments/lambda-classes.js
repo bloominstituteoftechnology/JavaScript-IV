8494072664ccc1c374557ff77056f2b83a6f86f0
Usman Javed <ujaved931@gmail.com>
2018-04-20 01:43:24 +0500

    tried lambda classes, stuck at methods.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,51 @@
 // CODE here for your Lambda Classes
+class Person {
+  constructor(personAttributes){
+  this.name = personAttributes.name;
+  this.age = personAttributes.age;
+  this.location = personAttributes.location;
+  this.gender = personAttributes.gender;
+  }
+ speak() {
+    return `Hello, my name is ${this.name}, I am from ${this.location}`;
+  }
+}
+
+class Instructor extends Person {
+  constructor(intructorAttributes) {
+  super(intructorAttributes);
+  this.specialty = intructorAttributes.specialty;
+  this.favLanguage = intructorAttributes.fav.Language;
+  this.catchPhrase = intructorAttributes.catchPhrase;
+  }
+  demo(subject) {
+  	return `Today we are learning about ${this.subject}`;
+  }
+  
+  grade(student, subject) {
+  	return `${this.name} recieves a perfect score on ${this.subject}`;
+  }
+}
+
+class Student extends Instructor {
+  constructor(studentAttributes) {
+  super(studentAttributes);
+  this.previousBackground = studentAttributes.previousBackground;
+  this.className = studentAttributes.className;
+  this.favSubject = studentAttributes.favSubject;
+  }
+  listsSubject() {
+    let allFavSubjects = [];
+    this.forEach((element) => {
+    allFavSubjects.push(`${element.favSubject}`);
+});
+
+  PRAssignment() {
+      
+  }
+  }
+}
+
+class ProjectManager extends Student {
+
+}
