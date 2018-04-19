cab3b65b0602dec2d011bb08338ea4c2d5775505
jesuarva <jean.ariza@jesuarva.com>
2018-04-19 21:39:09 +0200

    lambda-classes -> END class Project Manager

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -56,8 +56,16 @@ class Student extends Person {
 /**
  * ------- ProjectM -------
  */
-class ProjectM {
+class ProjectM extends Instructor {
     constructor(options){
-        this. = options.;
+        super(options);
+        this.gradClassName = options.gradClassName;
+        this.favInstructor = options.favInstructor;
+    }
+    standUp(chanel) {
+        console.log(`${this.name} announces to ${chanel}, @chanel standy times!`);
+    }
+    debugsCode(student, subject) {
+        console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
     }
 }
