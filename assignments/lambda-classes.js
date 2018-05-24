593a1f833324811cb3c55798c2300c8eafd43aa8
Simon <Simon_Freelance@hotmail.com>
2018-05-24 13:58:20 -0400

    Person class has been added with props.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,12 @@
 // CODE here for your Lambda Classes
+
+class Person {
+    constructor(info) {
+        this.name = info.name;
+        this.location = info.location;
+        this.gender = info.gender;
+    }
+    speak() {
+        return `Hello my name is ${$this.name}, I am from ${this.location}`;
+    }
+}
