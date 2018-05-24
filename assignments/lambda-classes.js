0a4edfc6940b5a80bd94fef54e4c1b42ce5df42b
Katie Gorbell <katiegorbell@gmail.com>
2018-05-24 17:28:34 -0500

    built Lambda-classes Constructors with methods

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,68 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(attributes) {
+        this.name = attributes.name;
+        this.age = attributes.age;
+        this.location = attributes.location;
+        this.gender = attributes.gender;
+    }
+    introduce() {
+        return `Hello, my name is ${this.name}, I am from ${this.location}.`
+    }
+}
+
+class Instructor extends Person{
+    constructor(instructorAttrs) {
+        super(instructorAttrs);
+        this.specialty = instructorAttrs.specialty;
+        this.favLanguage = instructorAttrs.favLanguage;
+        this.catchPhrase = instructorAttrs.catchPhrase;
+    }
+    demo(subject) {
+        return `today we are learning about ${subject}.`
+    }
+    grade(student, subject) {
+        return `${student.name} receives a perfect score on ${subject} `
+    }
+}
+
+class Student extends Person {
+    constructor(studentAttrs) {
+        super(studentAttrs);
+        this.previousBackground = studentAttrs.previousBackground;
+        this.className = studentAttrs.className;
+        this.favSubjects = studentAttrs.favSubjects;
+    }
+    listsSubjects(arr) {
+        return `${this.favSubjects};`
+    }
+    PRAssignment(subject) {
+        return `${this.name} has submitted a PR for ${subject}`;
+    }
+    sprintChallenge(subject) {
+        return `${this.name} has begun a sprint challenge on ${subject}`;
+    }
+}
+
+class ProjectManager extends Instructor {
+    constructor(PMAttrs) {
+        super(PMAttrs);
+        this.gradClassName = PMAttrs.gradClassName;
+        this.favInstructor = PMAttrs.favInstructor;
+    }
+    standUp(slackChannel) {
+        return `${this.name} announces to ${slackChannel}, @channel standy time!`;
+    }
+    debugsCode(student, subject) {
+        return `${this.name} debugs ${student.name}'s code on ${subject}`;
+    }
+}
+
+
+console.log(Josh.demo('whatevs')); 
+console.log(Josh.grade(Katie, 'project'));
+console.log(Katie.listsSubjects(this.favSubjects));
+console.log(Katie.PRAssignment('LESS'));
+console.log(Katie.sprintChallenge('JavaScript'));
+console.log(Holly.standUp('CS11_Holly'));
+console.log(Holly.debugsCode(Katie, 'CSS'));
