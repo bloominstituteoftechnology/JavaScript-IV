141ed86887c0b12fcf4114b3c805459d8f7ddabb
Kevin Tena <kevten09@gmail.com>
2018-05-24 15:48:43 -0500

    Completed Person, Instructor, and Student classes for lambda school

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,46 @@
 // CODE here for your Lambda Classes
+class Person{
+    constructor(properties){
+        this.name = properties.name;
+        this.age = properties.age;
+        this.location = properties.location;
+        this.gender = properties.gender;
+    }
+    speak() {
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}.`);
+    }
+
+}
+
+class Instructor extends Person{
+    constructor(instructorProps){
+        super(instructorProps);
+        this.specialty = instructorProps.specialty;
+        this.favLanguage = instructorProps.favLanguage;
+        this.catchPhrase = instructorProps.catchPhrase;
+    }
+    demo(subject) {
+        console.log(`Today we are learning about ${subject}.`);
+    }
+    grade(student,subject){
+        console.log(`${student.name} receives a perfect score on ${subject}.`);
+    }
+}
+
+class Student extends Person{
+    constructor(studentProps){
+        super(studentProps);
+        this.previousBackground = studentProps.previousBackground;
+        this.className = studentProps.className;
+        this.favSubjects = studentProps.favSubjects;
+    }
+    listsSubjects(){
+        this.favSubjects.forEach(item => {console.log(item)});
+        }
+    PRAssignment(subject){
+        console.log(`${this.name} has submitted a PR for ${subject}.`);
+    }
+    sprintChallenge(subject){
+        console.log(`${this.name} has begun sprint challenge on ${subject}.`);
+    }
+}
