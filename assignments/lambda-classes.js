5fb0ac3f49733cad307210c8ba050b250b6e7fb5
Mark Hong <m4rkh0ng@gmail.com>
2018-05-24 17:19:43 -0400

    done with prototype refactor

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,19 @@
 // CODE here for your Lambda Classes
+
+class Person {
+  constructor(traits) {
+    this.name = traits.name;
+    this.age = traits.age;
+    this.location = traits.location;
+    this.gender = traits.gender;    
+  }
+  speak() {
+    return `Hello my name is ${this.name}, I am from ${this.location}.`
+  }
+}
+
+class Instructor extends Person {
+  constructor() {
+    
+  }   
+}
