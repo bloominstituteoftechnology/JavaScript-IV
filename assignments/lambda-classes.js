939576d38a16f3752188ac3a85c3f84ed20fe1d6
Alec <afroenygam@gmail.com>
2018-05-24 16:49:43 -0400

    main tasks completed

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,70 @@
 // CODE here for your Lambda Classes
+class Person {
+  constructor(props) {
+    this.name = props.name;
+    this.age = props.age;
+    this.gender = props.gender;
+    this.location = props.location;
+  }
+
+  speak() {
+    return `Hello my name is ${this.name} and I am from ${this.location}`;
+  }
+}
+
+class Instructor extends Person {
+  constructor(props) {
+    super(props);
+    this.specialty = props.specialty;
+    this.favLanguage = props.favLanguage;
+    this.catchPhrase = props.catchPhrase;
+  }
+
+  demo(subject) {
+    return `Today we are learning about ${subject}`;
+  }
+
+  grade(student, subject) {
+    return `${student.name} receives a perfect grade on ${subject}`;
+  }
+}
+
+class Student extends Person {
+  constructor(props) {
+    super(props);
+    this.previousBackground = props.previousBackground;
+    this.className = props.className;
+    this.favSubjects = props.favSubjects;
+    this.grade = Math.random() * 100;
+  }
+
+  listsSubjects(subjects) {
+    subjects.forEach(element => {return element});
+  }
+
+  PRAssignment(subject) {
+    return `${this.name} has submitted a PR for ${subject}`;
+  }
+
+  sprintChallenge(subject) {
+    return `${this.name} has begun sprint challenge on ${subject}`
+  }
+
+
+}
+
+class ProjectManagers extends Instructor {
+  constructor(props) {
+    super(props);
+    this.gradClassName = props.gradClassName;
+    this.favInstructor = props.favInstructor;
+  }
+
+  standUp(channel) {
+    return `${this.name} announces to ${channel}, @channel standy times!`
+  }
+
+  debugsCode(student, subject) {
+    return `${student.name} debugs ${subject}`;
+  }
+}
