12844fda7f3e2034a61646a0e8f35b73e64494bc
chainchompa <chainchompa@gmail.com>
2018-05-24 15:09:35 -0400

    removed age property, not about to offend people

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,7 +1,8 @@
+//Classes
+
 class Person {
   constructor(attributes) {
     this.name = attributes.name;
-    this.age = attributes.age;
     this.location = attributes.location;
     this.gender = attributes.gender;
   }
@@ -12,7 +13,7 @@ class Person {
 
 class Instructor extends Person {
   constructor(instructAttributes) {
-    super(attributes);
+    super(instructAttributes);
     this.specialty = instructAttributes.specialty;
     this.favLanguage = instructAttributes.favLanguage;
     this.catchPhrase = instructAttributes.catchPhrase;
@@ -25,9 +26,9 @@ class Instructor extends Person {
     }
 }
 
-class ProjectManagers extends Instructor {
+class ProjectManager extends Instructor {
   constructor(pmAttributes) {
-    super(instructAttributes);
+    super(pmAttributes);
     this.gradClassName = pmAttributes.gradClassName;
     this.favInstructor = pmAttributes.favInstructor;
   }
@@ -41,7 +42,7 @@ class ProjectManagers extends Instructor {
 
 class Student extends Person {
   constructor(studentAttributes) {
-    super(attributes);
+    super(studentAttributes);
     this.previousBackground = studentAttributes.previousBackground;
     this.className = studentAttributes.className;
     this.favSubjects = studentAttributes.favSubjects;
@@ -52,6 +53,68 @@ class Student extends Person {
     PRAssignment(subject) {
       console.log(`${this.name} has submitted a PR for ${subject}`);
     }
-}   sprintChallenge(subject) {
+    sprintChallenge(subject) {
       console.log(`${this.name} has begun the sprint challenge on ${subject}`);
 }
+}
+
+//Objects
+
+//Instructors
+const josh = new Instructor ({
+  "name": "Josh";
+  "location": ;
+  "gender": ;
+  "specialty": "Front End",
+  "favLanguage": "Javascript",
+  "catchPhrase": "Yabba Dabba Doo!"
+});
+
+const patrick = new Instructor ({
+  "specialty": "Back End",
+  "favLanguage": "C",
+  "catchPhrase": "In the name of the moon, I'll punish you"
+});
+
+const johnathan = new Instructor ({
+  "specialty": "Full Stack",
+  "favLanguage": "Python",
+  "catchPhrase": "Here is your guided-demo..."
+});
+
+//Project Managers
+const jason = new ProjectManager ({
+  "specialty": "Back End",
+  "favLanguage": "Javascript",
+  "catchPhrase": "Wubba lubba dub dub!",
+  "gradClassName": "cs6",
+  "favInstructor": "Josh"
+});
+
+const rick = new ProjectManager ({
+  "specialty": "Back End",
+  "favLanguage": "Rust",
+  "catchPhrase": "And that's the wayyyyyy the news goes!",
+  "gradClassName": "cs8",
+  "favInstructor": "Himself"
+});
+
+const morty = new ProjectManager ({
+  "specialty": "None",
+  "favLanguage": "Java",
+  "catchPhrase": "Ohhh Morty, I dunno",
+  "gradClassName": "cs9",
+  "favInstructor": "Rick"
+});
+
+//Students
+const jennifer = new Student ({
+  "specialty": "Unsure",
+  "favLanguage": "Javascript",
+  "catchPhrase": "I have no idea what I'm doing",
+  "previousBackground": "Cosmetology",
+  "className": "Axolotls",
+  "favSubjects": ["Javascript", "Node.js", "React"]
+});
+
+console.log(jennifer)
