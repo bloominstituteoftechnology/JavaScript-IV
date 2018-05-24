d00d472fe8343654ea7f7cb9cfa7ade18dea8656
Mark Hong <m4rkh0ng@gmail.com>
2018-05-24 18:48:38 -0400

    messing around with math and grades

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -27,6 +27,15 @@ class Instructor extends Person {
   grade(student, subject) {
     return `${student.name} receives a perfect score on ${subject}!`;
   }
+  tutor(student) {
+    let grade = student.grade;
+    let randomNum = Math.floor(Math.random()*24); 
+    let upOrDown = Math.random() < 0.5 ? -1 : 1;
+    let scoreChange = randomNum * upOrDown;
+    if (grade) {
+      return grade + scoreChange;
+    }
+  }
 }
 
 class Student extends Person {
@@ -35,6 +44,7 @@ class Student extends Person {
     this.previousBackground = neoTraits.previousBackground;
     this.className = neoTraits.className;
     this.favSubjects = neoTraits.favSubjects;
+    this.grade = neoTraits.grade;
   }
   listsSubjects() {
     let list = this.favSubjects.join(', '); 
@@ -45,6 +55,13 @@ class Student extends Person {
   }
   sprintChallenge(subject) {
     return `${this.name} has begun a sprint challenge on ${subject}.`     
+  }
+  graduate() {
+    if (this.grade > 70) {
+      let finalGrade = this.grade;
+      return `Congratulations! You've passed our examinations with a score of ${finalGrade}. This means that you're ready to graduate! Go forth and prosper with code.` 
+    }
+    return `${this.name}'s score of ${this.grade} is not enough to graduate yet. Keep studying!`
   }
 }
 
@@ -86,7 +103,8 @@ const mark = new Student({
   gender: 'Male',
   previousBackground: 'Jack-of-all-trades',
   className: 'CS11',
-  favSubjects: ['Digital Art', 'JavaScript', 'UI Design', 'UX Heuristics & Theorycrafting', 'Game Design', 'Music']
+  favSubjects: ['Digital Art', 'JavaScript', 'UI Design', 'UX Heuristics & Theorycrafting', 'Game Design', 'Music'],
+  grade: Math.floor(Math.random() * 10) + 65
 });
 
 const hiawatha = new ProjectManager({
@@ -106,4 +124,14 @@ console.log(mark.PRAssignment(mark.favSubjects[2]));
 console.log(mark.sprintChallenge(mark.favSubjects[4]));
 console.log(hiawatha.standUp(`cs11`));
 console.log(hiawatha.debugsCode(mark,mark.favSubjects[1]));
+
+console.log(mark.grade);
+console.log(hiawatha.tutor(mark));
+console.log(mark.graduate());
+
+
+
+
+
+
 
