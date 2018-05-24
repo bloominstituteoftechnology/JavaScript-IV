52db62f4a8224bd98b905b3677a499fa085dec76
Joe Stanfield <jlstan06@hotmail.com>
2018-05-24 15:17:59 -0400

    starting on lambda classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,20 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(attribute) {
+        this.name = attribute.name;
+        this.age = attribute.age;
+        this.location = attribute.location;
+        this.gender = attribute.gender;
+    }
+    speak() {
+        return `Hello my name is ${this.name}, I am from ${this.location}`;
+    }
+
+}
+
+class Instructor extends Person {
+    constructor(insattribute){
+        super(insattribute);
+        
+    }
+}
