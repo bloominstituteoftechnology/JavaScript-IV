6277b3f5d923030d5998bac8a377cb8ea93296ea
Libby Hart <libbjo@gmail.com>
2018-05-24 11:29:51 -0600

    initial commit - refactored prototypes

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -1,20 +1,24 @@
-// Here we have a functioning solutoin to your challenge from yesterday.
+// Here we have a functioning solution to your challenge from yesterday.
 // Today your goal is to refactor all of this code to use ES6 Classes.
 // The console.log() statements should still return what is expected of them.
 
-function GameObject(options) {
-  this.createdAt = options.createdAt;
-  this.dimensions = options.dimensions;
+class GameObject {
+  constructor(options) {
+    this.createdAt = options.createdAt;
+    this.dimensions = options.dimensions;
+  }
 }
 
 GameObject.prototype.destroy = function() {
   return `Object was removed from the game.`;
 };
 
-function CharacterStats(characterStatsOptions) {
-  GameObject.call(this, characterStatsOptions);
-  this.hp = characterStatsOptions.hp;
-  this.name = characterStatsOptions.name;
+class CharacterStats extends GameObject {
+  constructor(characterStatsOptions) {
+     super(characterStatsOptions);
+     this.hp = characterStatsOptions.hp;
+     this.name = characterStatsOptions.name;
+  }
 }
 
 CharacterStats.prototype = Object.create(GameObject.prototype);
@@ -23,11 +27,13 @@ CharacterStats.prototype.takeDamage = function() {
   return `${this.name} took damage.`;
 };
 
-function Humanoid(humanoidOptions) {
-  CharacterStats.call(this, humanoidOptions);
-  this.faction = humanoidOptions.faction;
-  this.weapons = humanoidOptions.weapons;
-  this.language = humanoidOptions.language;
+class Humanoid extends CharacterStats {
+  constructor(humanoidOptions) {
+    super(humanoidOptions);
+    this.faction = humanoidOptions.faction;
+    this.weapons = humanoidOptions.weapons;
+    this.language = humanoidOptions.language;
+  }
 }
 
 Humanoid.prototype = Object.create(CharacterStats.prototype);
