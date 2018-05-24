5fdeae5d0d502f5d9e702f16481f28f3c1b349c3
Mark Hong <m4rkh0ng@gmail.com>
2018-05-24 18:12:21 -0400

    done with lambda-classes prior to stretching

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -8,12 +8,102 @@ class Person {
     this.gender = traits.gender;    
   }
   speak() {
-    return `Hello my name is ${this.name}, I am from ${this.location}.`
+    return `Hello, my name is ${this.name}, and I am from ${this.location}.`
   }
 }
 
 class Instructor extends Person {
-  constructor() {
-    
+  constructor(teachTraits) {
+    super(teachTraits);
+    this.specialty = teachTraits.specialty;
+    this.favLanguage = teachTraits.favLanguage;
+    this.catchPhrase = teachTraits.catchPhrase;
   }   
+  demo(subject) {
+    if (subject > '') {
+      return `Today, we are learning about ${subject}.`;
+    }
+  }
+  grade(student, subject) {
+    return `${student.name} receives a perfect score on ${subject}!`;
+  }
 }
+
+class Student extends Person {
+  constructor(neoTraits) {
+    super(neoTraits);
+    this.previousBackground = neoTraits.previousBackground;
+    this.className = neoTraits.className;
+    this.favSubjects = neoTraits.favSubjects;
+  }
+  listsSubjects() {
+    let list = this.favSubjects.join(', '); 
+    return `${this.name}'s favorite subjects are: ${list}.`
+  }
+  PRAssignment(subject) {
+    return `${this.name} has submitted a PR for ${subject}.`
+  }
+  sprintChallenge(subject) {
+    return `${this.name} has begun a sprint challenge on ${subject}.`     
+  }
+}
+
+class ProjectManager extends Instructor {
+  constructor(pmTraits) {
+    super(pmTraits);
+    this.gradClassName = pmTraits.gradClassName;
+    this.favInstructor = pmTraits.favInstructor;
+  }
+  standUp(channel) {
+    return `${this.name} announces to ${channel}: Hey @${channel}, it's standup time!`;
+  }
+  debugsCode(student, subject) {
+    return `${this.name} debugs ${student.name}'s code on ${subject}.`;
+  }
+}
+
+const beatrix = new Person({
+  name: 'Beatrix',
+  age: 28,
+  location: 'Alexandria',
+  gender: 'Female',
+});
+
+const bigBoss = new Instructor({
+  name: 'John',
+  age: 83,
+  location: 'Unknown, USA',
+  gender: 'Male',
+  specialty: 'Covert Operations (and also redux)',
+  favLanguage: 'CQC',
+  catchPhrase: 'We are soldiers without borders, our purpose defined by the era we live in.'
+});
+
+const mark = new Student({
+  name: 'Mark',
+  age: 28,
+  location: 'Florida',
+  gender: 'Male',
+  previousBackground: 'Jack-of-all-trades',
+  className: 'CS11',
+  favSubjects: ['Digital Art', 'JavaScript', 'UI Design', 'UX Heuristics & Theorycrafting', 'Game Design', 'Music']
+});
+
+const hiawatha = new ProjectManager({
+  name: 'Hiawatha',
+  age: 99,
+  location: 'The Land Before Time',
+  gender: 'Male',
+  gradClassName: 'CS00: Reloaded',
+  favInstructor: 'Merlin the Legendary Wizard'
+})
+
+console.log(beatrix.speak());
+console.log(bigBoss.demo(bigBoss.favLanguage));
+console.log(bigBoss.grade(beatrix,bigBoss.favLanguage));
+console.log(mark.listsSubjects());
+console.log(mark.PRAssignment(mark.favSubjects[2]));
+console.log(mark.sprintChallenge(mark.favSubjects[4]));
+console.log(hiawatha.standUp(`cs11`));
+console.log(hiawatha.debugsCode(mark,mark.favSubjects[1]));
+
