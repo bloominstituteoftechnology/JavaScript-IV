624524aedb18d6b513bb970efad4642955e34784
Brandon White <bwhite06@stac.edu>
2018-06-28 18:51:40 -0400

    got through task
    must fix code and add objects

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -5,7 +5,7 @@ class Person {
      this.location =  personAttributes.location;
      this.gender = personAttributes.gender;
   }
-  speak()= {
+  speak() {
  return `Hello my name is ${this.name}, I am from ${this.location}`;
   }
 }
@@ -21,7 +21,7 @@ class Instructor extends Person {
       demo(subject){
     return `Today we are learning about ${subject}`
       }
-    grade (){
+    grade (subject){
       return `${student.name} receives a perfect score on ${subject}`
     }
 }
@@ -34,9 +34,9 @@ class Student extends Person {
     this.className=studentAttributes.className
     this.favSubjects= studentAttributes.favSubjects
   }
-  listsSubjects(this.favSubjects){
-    for (let i=0;i<this.favSubjects.length;i++){
-    return this.favSubjects[i];
+  listsSubjects(listfavSubjects){
+    for (let i=0;i<listfavSubjects.length;i++){
+    return listfavSubjects[i];
     }
   }
   PRAssignment(subject){
@@ -46,3 +46,30 @@ class Student extends Person {
     return  `${student.name} has begun spring challenge on ${subject}`
   }
 }
+
+class ProjectManager extends Instructor {
+  constructor(projectManagerAttributes) {
+    super(projectManagerAttributes);
+    this.gradClassName=projectManagerAttributes.gradClassName
+  this.favInstructor= projectManagerAttributes.favInstructor
+  }
+  standup(channel){
+    return`${this.name} announces to ${channel}, @channel standy times!​​​​​`
+  }
+debugsCode(student,subject){
+  `${this.name} debugs ${student.name}'s code on ${subject}`
+}
+}
+
+
+
+const fred = new Instructor({
+  name: 'Fred',
+  location: 'Bedrock',
+  age: 37,
+  gender: 'male',
+  favLanguage: 'JavaScript',
+  specialty: 'Front-end',
+  catchPhrase: `Don't forget the homies`
+});
+console.log(fred.age);
