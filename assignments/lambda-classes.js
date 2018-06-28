97fa48d05f22f849df0d0ac671cbef2e5f6e1e52
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-06-28 15:02:01 -0600

    Finished stretch and refactored some previous code

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -8,7 +8,7 @@ class Person {
         this.gender = props.gender;
     }
     speak(){
-        console.log(`Hello my name is ${this.name}, and I am from ${this.location}`)
+        return `Hello my name is ${this.name}, and I am from ${this.location}`
     }
 }
 
@@ -20,10 +20,19 @@ class Instructor extends Person {
         this.catchPhrase = props.CatchPhrase;
     }
     demo(subject){
-        console.log(`Today we are learning about ${subject}`)
+        return `Today we are learning about ${subject}`
     }
     grade(student, subject){
-        console.log(`${student.name} recieves a perfect score on ${subject}`)
+        return `${student.name} recieves a perfect score on ${subject}`
+    }
+    gradeChange(student){
+        let randomNumber = Math.random();
+        if (randomNumber > 0.5){
+        student.grade += Math.ceil(Math.random() * 10 );
+        }else if(randomNumber < 0.5){
+        student.grade = student.grade - Math.ceil(Math.random() * 10);
+        }
+        return `Grade Change! ${student.name} grade now is ${student.grade}`
     }
 }
 
@@ -33,18 +42,24 @@ class Student extends Person {
         this.previousBackground = props.previousBackground;
         this.className = props.className; 
         this.favSubjects = props.favSubjects;
+        this.grade = props.grade;
     }
     listsSubjects(){
-        console.log("My favorite subjects are:")
-        for(let i = 0; i < this.favSubjects.length; i++){
-            console.log(this.favSubjects[i]);
-        }
+        // console.log("My favorite subjects are:")
+        this.favSubjects.forEach(function(subject){console.log(subject)});
     }
     PRAssignments(subject){
-        console.log(`${this.name} has submitted a PR for ${subject}`)
+        return `${this.name} has submitted a PR for ${subject}`
     }
     sprintChallenge(subject){
-        console.log(`${this.name} has begun sprint challenge on ${subject}`)
+        return `${this.name} has begun sprint challenge on ${subject}`
+    }
+    graduate(instructor){
+        if(this.grade > 70){
+            console.log(`Congratulations ${this.name}! You have successfully finished Lambda School. Good Luck on your job search!!!!`)
+        }else {
+            instructor.gradeChange(this);
+        }
     }
 }
 
@@ -55,10 +70,10 @@ class ProjectManager extends Instructor {
         this.favInstructor = props.favInstructor;
     }
     standUp(channel){
-        console.log(`${this.name} announces to ${channel}, "@channel Stand up time!"`)
+        return `${this.name} announces to ${channel}, "@channel Stand up time!"`
     }
     debugsCode(student, subject){
-        console.log(`${this.name} debugs ${student.name}'s code on ${subject}`)
+        return `${this.name} debugs ${student.name}'s code on ${subject}`
     }
 }
 
@@ -89,6 +104,7 @@ const katia = new Student({
     age: 20, 
     location: "Fort Collins Colorado", 
     gender: "F",
+    grade: 65,
     className: "CS12", 
     previousBackground: "FreeCodeCamp",
     favSubjects: ["Javascript", "React", "Node"]
@@ -98,6 +114,14 @@ const katia = new Student({
 // katia.listsSubjects();
 // katia.PRAssignments("Javascript IV");
 // katia.sprintChallenge("Javascrtipt Fundamentals")
+// console.log(katia.grade);
+// josh.gradeChange(katia);
+// josh.gradeChange(katia);
+// josh.gradeChange(katia);
+// josh.gradeChange(katia);
+// josh.gradeChange(katia);
+// josh.gradeChange(katia);
+// katia.graduate(josh);
 
 const dani = new ProjectManager({
     name: "Dani", 
@@ -111,3 +135,5 @@ const dani = new ProjectManager({
 // console.log(dani);
 // dani.standUp("CS12");
 // dani.debugsCode(katia, "Javascript");
+
+katia.listsSubjects();
