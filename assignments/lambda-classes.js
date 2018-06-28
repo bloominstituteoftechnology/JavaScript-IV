d74a11d673d11f3156f66a1d56e19203ac4b51e3
jordan-massingill <jordan_massingill@hotmail.com>
2018-06-28 17:45:04 -0500

    Lambda-classes.js Changes
    Completed objectives for lambda-classes.js
    Corrected prototype-refactor.js and got code to work
    Got lambda-classes.js code to work

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,4 +1,3 @@
-// CODE here for your Lambda Classes
 class Person {
   constructor(attributes) {
     this.name = attributes.name;
@@ -21,27 +20,77 @@ class Instructor extends Person {
     }
 
   grade (subject) {
-    return `${this.name} receives a perfect score on ${subject}`;
+    return `${this.name} receives a perfect score on ${subject}.`;
     }
   }
 
 class Student extends Person {
-  constructor(StuAttr) {
-    super(StuAttr);
-    this.previousBackground = StuAttr.previousBackground;
-    this.className = StuAttr.className;
-    this.favSubjects = StuAttr.favSubjects;
+  constructor(stuAttr) {
+    super(stuAttr);
+    this.previousBackground = stuAttr.previousBackground;
+    this.className = stuAttr.className;
+    this.favSubjects = stuAttr.favSubjects;
     }
 
-  listsSubjects (this.favSubjects) {
-    this.favSubjects.forEach(x => console.log(x));
+  listsSubjects () {
+    this.favSubjects.forEach(function (x) {console.log(x);});
     }
 
   PRAssignment (subject) {
-    return `${this.name} receives a perfect score on ${subject}`;
+    return `${this.name} receives a perfect score on ${subject}.`;
     }
 
   sprintChallenge (subject) {
-    return `${this.name} has begun their sprint challenge on ${subject}`
+    return `${this.name} has begun their sprint challenge on ${subject}.`;
     }
   }
+
+class ProjectManager extends Instructor {
+  constructor(PMAttr) {
+    super(PMAttr);
+    this.gradClassName = PMAttr.gradClassName;
+    this.favInstructor = PMAttr.favInstructor;
+  }
+
+  standUp (channel) {
+    return `${this.name} announces to {channel}, @channel standy times!`;
+  }
+
+  debugsCode (studentName, subject) {
+    return `${this.name} debugs ${studentName}s code on ${subject}.`;
+  }
+}
+
+const jordan = new Student({
+  name: "Jordan",
+  age: 28,
+  location: "Texas",
+  gender: "F",
+  previousBackground: "Biology",
+  className: "CS12",
+  favSubjects: ["biology", "physics", "psychology", "philosophy"]
+});
+
+const jane = new Instructor ({
+  name: "Jane",
+  age: 47,
+  location: "UK",
+  gender: "F",
+  specialty: "Front End Dev",
+  favLanguage: "JavaScript",
+  catchPhrase: "Where there is code, there will be bugs."
+});
+
+const philippe = new ProjectManager ({
+  name: "Philippe",
+  age: 32,
+  location: "Nevada",
+  gender: "fluid",
+  specialty: "Databases",
+  favLanguage: "JavaScript",
+  catchPhrase: "I came here to eat cheetos and debug code, and I'm all out of cheetos.",
+  gradClassName: "CS 3",
+  favInstructor: "Jane"
+});
+
+console.log(jane);
