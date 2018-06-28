9ae10ae116535fc130aa0d60f29c0890a71b183f
szincone <sawyerzincone@gmail.com>
2018-06-28 13:46:25 -0600

    Finished instructor class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -12,4 +12,21 @@ class Person {
     speak () {
         console.log(`Hello my name is ${this.name}, I am from ${this.location}`)
     }
-}
+}// End of Parent
+
+class Instructor extends Person {
+    constructor(instructorAttributes) {
+        super(instructorAttributes);
+        this.specialty = instructorAttributes.specialty;
+        this.favLanguage = instructorAttributes.favLanguage;
+        this.catchPhrase = instructorAttributes.catchPhrase;
+    }
+    demo (subject) {
+        console.log(`Today we are learning about ${subject}`);
+    }
+    grade (student, subject) {
+        console.log(`${student.name} receives a perfect score on ${subject}`);
+    } 
+}// End of Instructor
+
+
