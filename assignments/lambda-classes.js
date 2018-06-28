c3437b1955873da1927c4148f1f96476a1991d17
adamcglee <adamcglee@gmail.com>
2018-06-28 13:59:09 -0700

    updated code to complete lambda classes project

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -4,11 +4,10 @@ class Person {
         this.age = attributes.age;
         this.location = attributes.location;
         this.gender = attributes.gender;
-      }
-      speak () {
-        return `Hello my name is ${this.name}, I'm from ${this.location}.`;
-      } 
     }
+    speak () {
+        return `Hello my name is ${this.name}, I'm from ${this.location}.`;
+    } 
 }
 
 class Instructor extends Person {
@@ -18,13 +17,12 @@ class Instructor extends Person {
         this.favLanguage = instAttributes.favLanguage;
         this.catchPhrase = instAttributes.catchPhrase;
       }
-      demo (subject) {
+    demo (subject) {
         return `Today we are learning about ${subject}`;
-      } 
-      grade (studentObj, subject) {
+    } 
+    grade (studentObj, subject) {
         return `${studentObj.name} receives a perfect score on ${subject}`;
-      } 
-    }
+    } 
 }
 
 class Student extends Person {
@@ -38,23 +36,55 @@ class Student extends Person {
         return this.favSubjects;
     } 
     PRAssignment (subject) {
-        return `${student.name} has submitted a PR for ${subject}`;
+        return `${this.name} has submitted a PR for ${subject}`;
     } 
     sprintChallenge (subject) {
-        return `${student.name} has begun sprint challenge on ${subject}`;
+        return `${this.name} has begun sprint challenge on ${subject}`;
     } 
 }
 
-class ProjectManagers extends Instructor {
+class ProjectManager extends Instructor {
     constructor (pmAttributes) { 
         super(pmAttributes);
         this.gradClassName = pmAttributes.gradClassName;
         this.favInstructor = pmAttributes.favInstructor;
       }
-    standUp (name, channel) {
-        return `${name} announces to ${channel}, @channel standy times!​​​​​`;
+    standUp (channel) {
+        return `${this.name} announces to ${channel}, @channel standy times!​​​​​`;
     } 
     debugsCode (studentObj, subject) {
         return `${this.name} debugs ${studentObj.name}'s code on ${subject}`;
     } 
 }
+
+const terrance = new Instructor({
+    name: 'Terrance',
+    location: 'Canada',
+    age: 32,
+    gender: 'male',
+    favLanguage: 'HTML',
+    specialty: 'Back-end',
+    catchPhrase: `I'm not your buddy friend.`
+  });
+
+  const eric = new Student({
+    name: 'Eric',
+    location: 'South Park',
+    age: 8,
+    gender: 'male',
+    previousBackground: 'Just graduated 3rd grade',
+    className: 'Full-Stack Web Dev',
+    favSubjects: ['Recess', 'Video Games', 'A-hole 101']
+  });
+
+  const al = new ProjectManager({
+    name: 'Al Gore',
+    location: 'Washington DC',
+    age: 70,
+    gender: 'male',
+    favLanguage: 'CSS',
+    specialty: 'Full-Stack',
+    catchPhrase: `ManBearPig is half man, half bear, and half pig.`,
+    gradClassName: 'CS12',
+    favInstructor: 'Josh (of course)'
+  });
