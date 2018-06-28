f4ed8359a901dddf0959a4c4c0ad9e096cd60d4e
David White <dw@Davids-iMac.local>
2018-06-28 14:06:05 -0600

    lambda classes finished, working on student constructor

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,49 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(details){
+        this.name = details.name;
+        this.age = details.age;
+        this.location = details.location;
+        this.gender = details.gender;
+    }
+
+    speak () {
+        console.log(`Hello my name is {this.name} I am from {this.loacation}`);
+    }
+}
+
+class Instructor extends Person{
+    constructor(details) {
+        super(details);
+        this.specialty = details.specialty;
+        this.favLanguage = details.favLanguage;
+        this.catchPharae = details.catchPharae;
+    }
+
+    demo (subject) {
+        console.log(`Today we are learning about ${subject}`);
+    }
+
+    grade (student, subject) {
+        console.log(`${student} receives a perfect score on ${subject}`);
+    }
+}
+
+class Students extends Instructor {
+    constructor(details) {
+        super(details);
+        this.previousBacground = details.previousBacground;
+        this.className = details.className;
+        this.favSubjects = details.favSubjects;
+    }
+ 
+    listSubjects () {
+        console.log(this.favSubjects.forEach())
+    }
+
+    
+
+
+
+}
+
