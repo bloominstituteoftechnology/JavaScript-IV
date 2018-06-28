afd3d3b6e04b1e698fc850b45291423a964c97d5
jaymzsocool <james.mcraejr@gmail.com>
2018-06-28 17:36:48 -0400

    Added objects and tested. Works correctly!

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -3,10 +3,11 @@ class Person {
     constructor(attributes) {
         this.name = attributes.name;
         this.age = attributes.age;
+        this.location = attributes.location;
         this.gender = attributes.gender;
     }
     speak(){
-        return `Hello my name is ${this.name}, I am from ${this.location}`
+        return `Hello my name is ${this.name}, I am from ${this.location}.`
     }
 }
 
@@ -18,10 +19,10 @@ class Instructor extends Person{
         this.catchPhrase = attributes.catchPhrase;
     }
     demo(subject){
-        return `Today we are learning about ${subject}`
+        return `Today we are learning about ${subject}.`
     }
     grade(student, subject) {
-        return `${student.name} receives a perfect score on ${subject}`
+        return `${student.name} receives a perfect score on ${subject}.`
     }
 }
 
@@ -34,7 +35,7 @@ class Student extends Person {
     }
     listSubjects(favSubjects1, favSubjects2, favSubjects3){
         return `My favorite subjects are ${favSubjects1}, ${favSubjects2}, ${favSubjects3}.`;
-        listSubjects.call(...favSubjects); 
+        
     };
     PRAssingment(subject){
         return `${this.name} has submitted a PR for ${subject}.`
@@ -43,7 +44,7 @@ class Student extends Person {
         return `${this.name} has begun spring challenge on ${subject}.`
     }
 }
-
+ 
 class ProjectManager extends Instructor{
     constructor(attributes){
         super(attributes);
@@ -54,6 +55,48 @@ class ProjectManager extends Instructor{
         return `${this.name} announces to ${channel}, @channel standy times!`
     }
     debugsCode(student, subject){
-        return `${this.name} debugs ${student.name}'s code on ${subject}`
+        return `${this.name} debugs ${student}'s code on ${subject}.`
     }
 }
+
+const josh = new Instructor({
+    name: `Josh`,
+    age: 35,
+    location: `Michigan`,
+    gender: `M`,
+    specialty: `Web Devlopement`,
+    favLanguage: `Javascript`,
+    catchPhrase: `I invoke thine function!`
+})
+
+const james = new Student({
+    name: `James`,
+    age: 30,
+    location: `Florida`,
+    gender: `M`,    
+    previousBackground: `None`,
+    className: `CS12`,
+    favSubjects: [`HTML`, `CSS`, `Javascript`]
+})
+
+const mike = new ProjectManager({
+    name: `Michael`,
+    age: 28,
+    location: `Central US`,
+    gender: `M`,
+    specialty: `Game Developement`,
+    favLanguage: `React`,
+    catchPhrase: `It happened to me too.`,
+    gradClassName: `CS7`,
+    favInstructor: `Josh`
+})
+
+console.log(josh.catchPhrase)
+console.log(mike.speak())
+console.log(james.listSubjects(...james.favSubjects))
+console.log(josh.demo(`CSS`))
+console.log(mike.grade(james, `Javascript`))
+console.log(james.PRAssingment(`Javascript-III`))
+console.log(james.sprintChallenge(`Javascript`))
+console.log(mike.standUp(`CS12-Michael`))
+console.log(mike.debugsCode(`Alex`, `Javascript`))
