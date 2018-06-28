a8d27d58e206b89a6ef6b75bcfe7f89e110c9ff1
Brandon White <bwhite06@stac.edu>
2018-06-28 17:07:59 -0400

    working on classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,11 @@
-// CODE here for your Lambda Classes
+class Person {
+  constructor(personAttributes) {
+    this.name = personAttributes.name
+     this.age= personAttributes.age
+     this.location =  personAttributes.location
+     this.gender = personAttributes.gender
+  }
+  speak()= {
+console.console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
+  }
+}
