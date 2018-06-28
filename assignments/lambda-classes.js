6b93740519f038c3652170e61e3870eb9ef82d8d
Arthur Bates <aabatesj@gmail.com>
2018-06-28 16:58:45 -0400

    Arthur Bates
    First PR
    
    attempt lambda-classes.js and stretch goals.
    
    moving on to prototype-refactor.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,71 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(attrs) {
+        this.name = attrs.name;
+        this.age = attrs.age;
+        this.gender = attrs.gender;
+        this.location = attrs.location;
+    }
+    speak() {
+        return `Hello, my name is ${this.name}, I am from ${this.location}`;
+    }
+}
+
+class Instructor extends Person {
+    constructor(instrAttrs) {
+        super(attrs);
+        this.specialty = instrAttrs.specialty;
+        this.favLanguage = instrAttrs.favLanguage;
+        this.catchPhrase = instrAttrs.catchPhrase;
+    }
+    demo(subject) {
+        console.log(`Today we are learning about ${subject}`);
+    }
+
+    grade(Student, subject) {
+        console.log(`${Student.name} receives a perfect score on ${subject}`);
+    }
+    curve(grade) {
+        return Math.random() + grade;
+    }
+}
+
+class Student extends Person {
+    constructor(studentAttrs) {
+        super(attrs);
+        this.previousBackground = instrAttrs.previousBackground;
+        this.className = StudentAttrs.className;
+        this.favSubjects = StudentAttrs.favSubjects;
+        grade = 90;
+    }
+    listsSubjects() {
+        console.log(this.favSubjects);
+    }
+
+    PRAssignment(subject) {
+        console.log(`${Student.name} has submitted a PR for ${subject}`);
+    }
+    sprintChallenge(subject) {
+        console.log(`${Student.name} has begun sprint challenge on ${subject}`);
+    }
+    graduate() {
+        if (grade > 70) {
+            console.log(`${Student.name} has graduated!`);
+        }
+    }
+}
+
+class ProjectManager extends Instructor {
+    constructor(PMAttrs) {
+        super(InstrAttrs);
+        this.gradClassName = PMAttrs.gradClassName;
+        this.favInstructor = PMAttrs.favInstructor;
+    }
+    standUp(slack) {
+        console.log(`${this.name} announces to ${slack}, @channel standy times!`);
+    }
+
+    debugsCode(Student, subject) {
+        console.log(`${this.name} debugs ${Student.name}'s code on {subject}`);
+    }
+}
