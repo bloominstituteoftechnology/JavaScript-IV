447f9ca973b264a07ca1fc6c3c8bd96f6d402c00
Rachel DiCesare <racheldicesare@missrachelthedeveloper.com>
2018-06-28 15:07:58 -0400

    first commit

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -2,7 +2,7 @@
 // Today your goal is to refactor all of this code to use ES6 Classes.
 // The console.log() statements should still return what is expected of them.
 
-function GameObject(options) {
+function  GameObject(options) {
   this.createdAt = options.createdAt;
   this.dimensions = options.dimensions;
 }
