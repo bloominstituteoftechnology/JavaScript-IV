aa7828c20c8d2c1f5ae8f28c3fee08c0c8cd699d
Alberto-Funk <laptopreneur@gmail.com>
2018-06-28 12:13:44 -0700

    Project Start

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -1,3 +1,5 @@
+/* :) */
+
 // Here we have a functioning solutoin to your challenge from yesterday.
 // Today your goal is to refactor all of this code to use ES6 Classes.
 // The console.log() statements should still return what is expected of them.
