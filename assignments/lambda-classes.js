db9abde89a766a4d34ae2e4ad4460dacf6011734
Charlie Winslow <cwinslow22@yahoo.com>
2018-06-28 14:48:58 -0500

    created person class with speak method

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,15 @@
 // CODE here for your Lambda Classes
+
+class Person {
+    constructor(innate) {
+        this.name = innate.name;
+        this.age = innate.age;
+        this.location = innate.location;
+        this.gender = innate.gender;
+    }
+    speak () {
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}`)
+    }
+}
+
+
