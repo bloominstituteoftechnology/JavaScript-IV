def735b458e341f33f60d841833fcaab9599e677
Charlie Winslow <cwinslow22@yahoo.com>
2018-06-28 15:10:21 -0500

    created instructor class and methods
    Created student class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -12,4 +12,27 @@ class Person {
     }
 }
 
+class Instructor extends Person {
+    constructor(teach) {
+        super(teach);
+        this.specialty = teach.specialty;
+        this.favLanguage = teach.favLanguage;
+        this.catchPhrase = teach.catchPhrase;
+    }
+    demo (subject) {
+        console.log(`Today we are learning about ${subject}`);
+    }
+    grade (student, subject) {
+        console.log(`${student.name} receives a perfect score on ${subject}`);
+    }
+}
 
+class student extends Person {
+    constructor(learn) {
+        super(learn);
+        this.previousBackground = learn.previousBackground;
+        this.className = learn.className;
+        this.favSubjects = learn.favSubjects;
+    }
+    
+}
