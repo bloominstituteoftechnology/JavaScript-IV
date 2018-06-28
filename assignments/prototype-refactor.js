44e5a997f9b239fba46242ba464b30eceb2ad875
Grant Reighard <grantreighard@Grants-iMac.fios-router.home>
2018-06-28 15:27:25 -0400

    Finished prototype-refactor.js.
    - Now moving to lambda-classes.js.

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -4,39 +4,40 @@
 
 // comment
 
-function GameObject(options) {
+class GameObject {
+  constructor (options) {
   this.createdAt = options.createdAt;
   this.dimensions = options.dimensions;
+  }
+  destroy () {
+    return `${this.name} was removed from the game.`;
+  }
+  }
+
+
+class CharacterStats extends GameObject {
+  constructor(characterStatsOptions) {
+    super(characterStatsOptions);
+    this.hp = characterStatsOptions.hp;
+    this.name = characterStatsOptions.name;
+  }
+  takeDamage () {
+    return `${this.name} took damage.`;
+  }
 }
 
-GameObject.prototype.destroy = function() {
-  return `Object was removed from the game.`;
-};
 
-function CharacterStats(characterStatsOptions) {
-  GameObject.call(this, characterStatsOptions);
-  this.hp = characterStatsOptions.hp;
-  this.name = characterStatsOptions.name;
+class Humanoid extends CharacterStats {
+  constructor(humanoidOptions) {
+    super(humanoidOptions);
+    this.faction = humanoidOptions.faction;
+    this.weapons = humanoidOptions.weapons;
+    this.language = humanoidOptions.language;
+  }
+  greet () {
+    return `${this.name} offers a greeting in ${this.language}.`;
+  }
 }
-
-CharacterStats.prototype = Object.create(GameObject.prototype);
-
-CharacterStats.prototype.takeDamage = function() {
-  return `${this.name} took damage.`;
-};
-
-function Humanoid(humanoidOptions) {
-  CharacterStats.call(this, humanoidOptions);
-  this.faction = humanoidOptions.faction;
-  this.weapons = humanoidOptions.weapons;
-  this.language = humanoidOptions.language;
-}
-
-Humanoid.prototype = Object.create(CharacterStats.prototype);
-
-Humanoid.prototype.greet = function() {
-  return `${this.name} offers a greeting in ${this.language}.`;
-};
 
 const mage = new Humanoid({
   createdAt: new Date(),
