bce6ed201d77011d02e27804fb4aba352a7b74b7
Charlie Winslow <cwinslow22@yahoo.com>
2018-06-28 16:24:06 -0500

    added PM class and methods
    added new objects of each class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -27,7 +27,7 @@ class Instructor extends Person {
     }
 }
 
-class student extends Person {
+class Student extends Person {
     constructor(learn) {
         super(learn);
         this.previousBackground = learn.previousBackground;
@@ -47,4 +47,81 @@ class student extends Person {
     }
 }
 
+class ProjectManager extends Instructor {
+    constructor(manage) {
+        super(manage);
+        this.gradClassName = manage.gradClassName;
+        this.favInstructor = manage.favInstructor;
+    }
+    standUp (channel) {
+        console.log(`${this.name} announces to ${channel}, @channel standup time!​​​​​`);
+    }
+    debugsCode (student, subject) {
+        console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
+    }
+}
+
+const fred = new Instructor({
+    name: 'Fred',
+    location: 'Bedrock',
+    age: 37,
+    gender: 'male',
+    favLanguage: 'JavaScript',
+    specialty: 'Front-end',
+    catchPhrase: `Don't forget the homies`
+  });
+
+const sarah = new Instructor({
+    name: 'Sarah',
+    location: 'Rock Island',
+    age: 34,
+    gender: 'female',
+    favLanguage: 'React',
+    specialty: 'Front-end',
+    catchPhrase: `Don't panic!`
+  });
+
+const marco = new Student({
+    name: 'Marco',
+    location: 'Rockford',
+    age: 39,
+    gender: 'male',
+    previousBackground: 'psychology',
+    className: 'CS19',
+    favSubjects: ['Html', 'CSS', 'JavaScript']
+  });
+
+const natalia = new Student({
+    name: 'Natalia',
+    location: 'Moscow',
+    age: 28,
+    gender: 'female',
+    previousBackground: 'ballerina',
+    className: 'CS29',
+    favSubjects: ['Ruby', 'Python', 'JavaScript']
+  });
+
+const jake = new ProjectManager({
+    name: 'Jake',
+    location: 'York',
+    age: 31,
+    gender: 'male',
+    favLanguage: 'Python',
+    specialty: 'Back-end',
+    catchPhrase: `Back in my day ...`,
+    gradClassName: 'CS7',
+    favInstructor: 'Sean'
+  });
+
+const sasha = new ProjectManager({
+    name: 'Sasha',
+    location: 'Philadelphia',
+    age: 32,
+    gender: 'female',
+    favLanguage: 'JavaScript',
+    specialty: 'Front-end',
+    catchPhrase: `Computer says no`,
+    gradClassName: 'CS5',
+    favInstructor: 'Royce'
+  });
 
