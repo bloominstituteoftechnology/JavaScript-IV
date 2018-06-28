6d2c0c6682196f5d30223b5ab002928860247b39
keith Kennedy <keithkennedy@keiths-MacBook-Air.local>
2018-06-28 15:17:40 -0500

    Started building objects
    I built the person class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,2 +1,17 @@
 // CODE here for your Lambda Classes
-class
+class Person {
+  constructor(attributes){
+    this.name = attributes.name;
+    this.age = attributes.age;
+    this.location = attributes.location;
+    this.gender = attributes.gender;
+  }
+  speak(){
+    console.log(`Hello, my name is ${this.name},
+    I am from ${this.location}.`)
+  }
+}
+
+class Instructor {
+  
+}
