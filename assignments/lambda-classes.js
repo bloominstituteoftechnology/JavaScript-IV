b1b3ddaf1694423ce3a219313f3a308ce3e71ce6
bri79319 <bri79319@gmail.com>
2018-06-28 15:59:58 -0400

    classes
    modification

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,24 @@
-// CODE here for your Lambda Classes
+//Person Class Starts
+class Person {
+    constructor(attributes) {
+      this.name = attributes.name;
+      this.age = attributes.age;
+      this.location = attributes.location;
+      this.gender = attributes.gender;
+    }
+    speak() {
+      console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
+    }
+  }
+  //Person Class Ends
+  
+  //Instructor Clas Starts
+  class Instructor extends Person {
+    constructor(instructorAttributes) {
+      super(instructorAttributes);
+      this.speciality = instructorAttributes.speciality;
+      this.favLanguage = instructorAttributes.favLanguage;
+      this.catchPhrase = 
+    }
+  }
+  //Instructor Class Ends
