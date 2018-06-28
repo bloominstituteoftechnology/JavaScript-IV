7e30ce0488eab9aaecb2536b8bb85bf3cde2be2a
Alberto-Funk <laptopreneur@gmail.com>
2018-06-28 14:59:09 -0700

    Completed Stretch
    - Added new method to instructor to grade them
    
    - added grade property to student and a graduation method

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -26,7 +26,16 @@ class Instructor extends Person {
     return `Today we are learning about ${subject}`;
   }
   grade (student, subject) {
-    return `${student} receives a perfect score on ${subject}`;
+    return `${student.name} receives a perfect score on ${subject}`;
+  }
+  gradeStudent (student) {
+    const addOrSubtract = Math.floor(Math.random() * Math.floor(10));
+    const addOrSubtractAmount = Math.floor(Math.random() * Math.floor(21));
+    if (addOrSubtract > 5) {
+      student.classGrade += addOrSubtractAmount;
+    } else if (addOrSubtract <= 5) {
+      student.classGrade -= addOrSubtractAmount;
+    }
   }
 }
 
@@ -38,15 +47,25 @@ class Student extends Person {
     this.previousBackground = studentProps.previousBackground;
     this.className = studentProps.className;
     this.favSubjects = studentProps.favSubjects;
+    this.classGrade = studentProps.classGrade;
   }
   listsSubjects () {
-    this.favSubjects.forEach(element => console.log(element));
+    return this.favSubjects.forEach(element => console.log(element));
   }
   PRAssignment (subject) {
     return `${this.name} has submitted a PR for ${subject}`;
   }
   sprintChallenge (subject) {
     return `${this.name} has begun spring challenge on ${subject}`
+  }
+  graduateChecker () {
+    if (this.classGrade > 70) {
+      return `Congrats ${this.name}, you have graduated!`
+    } else if (this.classGrade > 100) {
+      return `WOW! ${this.name} you have graduated with honers, CONGRATS!!`
+    } else if (this.classGrade < 70) {
+      return `Still need some work, ${this.name}, contact your PM!`
+    }
   }
 }
 
@@ -63,6 +82,55 @@ class ProjectManager extends Instructor {
     return `${this.name} debugs ${student.name}'s code on ${subject}`
   }
 }
+
+
+const instructor1 = new Instructor ({
+  name: 'Mike',
+  location: 'LA',
+  specialty: 'JS',
+  className: 'test',
+  otherTest: 'hello',
+})
+
+const student1 = new Student ({
+  name: 'James',
+  location: 'LA',
+  favLanguage: 'Greek',
+  favSubjects: ['stuff', 'other stuff', 'more stuff'],
+  classGrade: 90,
+})
+
+const pm1 = new ProjectManager ({
+  name: 'Neal',
+  location: 'LA',
+  favLanguage: 'Chinese',
+  favInstructor: 'Mike',
+  favSubjects: ['some stuff', 'more stuff', 'more more stuff'],
+})
+
+
+console.log(instructor1.demo('Math'));
+console.log(instructor1.grade(student1, 'Math'));
+
+
+console.log(student1.classGrade);
+instructor1.gradeStudent(student1);
+pm1.gradeStudent(student1);
+console.log(student1.classGrade);
+
+console.log(student1.graduateChecker());
+
+
+console.log(student1.speak());
+console.log(student1.listsSubjects());
+console.log(student1.PRAssignment('Math'));
+console.log(student1.sprintChallenge('Math'));
+
+console.log(pm1.debugsCode(student1, 'Math'));
+
+// Sibling lost connection example
+// console.log(pm1.PRAssignment('Subject'));
+
 
 
 
