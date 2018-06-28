83afbcb2724ef915c20a7f5906e3ed53379c0c85
mkerbleski <mkerbles@gmail.com>
2018-06-28 14:29:32 -0600

    Created 3 objects and completed all classes.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,5 +1,5 @@
 // CODE here for your Lambda Classes
-class people {
+class People {
   constructor (peopleAttributes){
     this.name = peopleAttributes.name;
     this.name = peopleAttributes.name;
@@ -12,7 +12,7 @@ class people {
     console.log(`Hello, my name is ${this.name}, I am from ${this.location}`);
   }
 }
-class instructor {
+class Instructor {
   constructor (instructorAttributes){
     super(instructorAttributes);
     this.specialty = instructorAttributes.specialty;
@@ -26,7 +26,7 @@ class instructor {
     console.log(`${this.student} receives a perfect score on ${this.subject}.`)
   }
 }
-class student {
+class Student {
   constructor (studentAttributes){
     super(studentAttributes);
     this.previousBackground = instructorAttributes.previousBackground;
@@ -40,4 +40,49 @@ class student {
     console.lot(`${this.name} has submitted a PR for ${this.subject}.`)
   }
 }
-class Project 
+class ProjectMananger extends Instructor {
+  constructor (projectManagerAttributes){
+    super(projectManagerAttributes);
+    this.gradClassName = instructorAttributes.gradClassName;
+    this.favInstructor = instructorAttributes.favInstructor;
+  }
+  standUp (slackChannel) {
+    console.log(`${this.name} announces to ${this.slackChannel}: @channel Time for standup!`)
+  }
+  debugsCode (student.name, subject ) {
+    console.log(`${this.name} debugs ${student.name}'s code on ${this.subject}.`);
+  }
+}
+
+const Josh = new Instructor({
+  name: 'Josh',
+  location: 'Utah',
+  age: 37,
+  gender: 'male',
+  favLanguage: 'JavaScript',
+  specialty: 'Front-end',
+  catchPhrase: `Don't forget the homies`
+});
+
+const Mike = new Student ({
+  name: 'Mike',
+  location: 'New Mexico',
+  age: 94,
+  gender: 'male',
+  favLanguage: 'JavaScript',
+  specialty: 'Front-end',
+  catchPhrase: `I'm here to code.`
+})
+
+
+const Dani = new ProjectMananger ({
+  name: 'Dani',
+  location: 'Minnesota',
+  age: 22,
+  gender: 'female',
+  favLanguage: 'All the languages',
+  specialty: 'Front-end',
+  catchPhrase: `Hi`
+})
+
+ dani.standUp()
