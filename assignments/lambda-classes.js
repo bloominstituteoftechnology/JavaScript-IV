bd47c1d19e92e3499adb214166e95b6f10773de7
bri79319 <bri79319@gmail.com>
2018-06-28 16:25:49 -0400

    classes
    building 4th class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -6,6 +6,8 @@ class Person {
       this.location = attributes.location;
       this.gender = attributes.gender;
     }
+
+    //Method
     speak() {
       console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
     }
@@ -20,8 +22,53 @@ class Person {
       this.favLanguage = instructorAttributes.favLanguage;
       this.catchPhrase = instructorAttributes.catchPhrase;
     }
+
+    //Methods
     demo(subject) {
         console.log(`Today we are learning about ${subject}`);
     }
+    
+    grade(student, subject) {
+        console.log(`${student.name} receives a perfect score on ${subject}`);
+    }
   }
   //Instructor Class Ends
+
+  //Student Class Starts
+  class Student extends Person {
+      constructor(studentAttributes){
+          super(studentAttributes);
+          this.previousBackground = studentAttributes.previousBackground;
+          this.className = studentAttributes.className;
+          this.favSubjects = studentAttributes.favSubjects;
+      }
+
+      //Methods
+      listSubjects() {
+        console.log(`${favSubjects}`);
+      }
+
+      PRAssignments(subject) {
+        console.log(`${student.name} has submitted a PR for ${subject}`);
+      }
+
+      sprintChallenge(subject) {
+        console.log(`${student.name} has begun spring challenge on ${subject}`);
+      }
+  }
+  //Student Class Ends
+
+  //Project Manager Starts
+  class ProjectManagers extends Instructor {
+      constructor(pmAttributes) {
+          super(pmAttributes);
+          this.gradClassName = pmAttributes.gradClassName;
+          this.favInstructor = pmAttributes.favInstructor;
+      }
+
+      //Methods
+      standUP() {
+          
+      }
+  }
+  //Project Manager Ends
