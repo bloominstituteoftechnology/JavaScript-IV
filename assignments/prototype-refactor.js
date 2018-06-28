f1a1ee1f86a96a80b5ec8a2528a3efcc42fea36d
Naaz Shaikh <Naaz@Naazs-MacBook-Pro.local>
2018-06-28 12:22:26 -0700

    initial commit for PR

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -2,14 +2,27 @@
 // Today your goal is to refactor all of this code to use ES6 Classes.
 // The console.log() statements should still return what is expected of them.
 
-function GameObject(options) {
-  this.createdAt = options.createdAt;
-  this.dimensions = options.dimensions;
+class GameObject{
+    constructor(options){
+      this.createdAt = options.createdAt;
+      this.dimensions = options.dimensions;
+    }
+    destroy() {
+      return`Object was removed from the game.`;
+    }
 }
 
-GameObject.prototype.destroy = function() {
-  return `Object was removed from the game.`;
-};
+
+// function GameObject(options) {
+//   this.createdAt = options.createdAt;
+//   this.dimensions = options.dimensions;
+// }
+
+// GameObject.prototype.destroy = function() {
+//   return `Object was removed from the game.`;
+// };
+
+
 
 function CharacterStats(characterStatsOptions) {
   GameObject.call(this, characterStatsOptions);
