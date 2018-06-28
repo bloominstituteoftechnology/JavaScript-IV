a1be189f3f2ef198b3be099206ec23ddb1a3d145
keith Kennedy <keithkennedy@keiths-MacBook-Air.local>
2018-06-28 16:01:17 -0500

    lambd-class.js on project manager
    starting on PM object

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -12,6 +12,35 @@ class Person {
   }
 }
 
-class Instructor {
-  
+class Instructor extends Person {
+    constructor(instructorAttributes){
+      super(instructorAttributes);
+      this.specialty = instructorAttributes.specialty;
+      this.favLanguage = instructorAttributes.favLanguage;
+      this.catchPhrase = instructorAttributes.catchPhrase;
+    }
+    demo(){
+      console.log(`Today we are learning about ${subject}`)
+    }
+    grade(){
+      console.log(`${this[student].name} recieves a perfect score on ${subject}`)
+    }
 }
+
+  class Student extends Person {
+    constructor(studentAttributes){
+      super(studentAttributes);
+      this.previousBackground = studentAttributes.previousBackground;
+      this.className = studentAttributes.className;
+      this.favSubjects = studentAttributes.favSubjects;
+      this.listsSubjects = studentAttributes.listsSubject;
+    }
+    PRAssignment(){
+      console.log(`${this.name} has submitted a PR for ${subject}`);
+    }
+    sprintChallenge(){
+      console.log(`${this.name} has begun sprint challenge on ${subject}`);
+    }
+  }
+
+  
