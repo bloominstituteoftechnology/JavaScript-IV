bfecce5a761639b8ce050c21773e4dc981e9f155
Jurgen Kela <jurgenkela@Jurgens-MacBook-Pro.local>
2018-06-28 17:40:14 -0400

    lamba classes 1/2 done

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,31 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(options) {
+        this.name = options.name;
+        this.age = options.age;
+        this.location = options.location;
+        this.gender = options.gender;
+    }
+    speak() {
+        console.log(`Hello name name is ${this.name}, I am from ${this.location}.`);
+    }
+}
+
+class Instructor extends Person {
+    constructor(instructorOptions) {
+        super(instructorOptions);
+        this.specialty = instructorOptions.specialty;
+        this.favLanguage = instructorOptions.favLanguagel;
+        this.catchPhrase = instructorOptions.catchPhrase;
+    }
+    demo(subject) {
+        console.log(`Today we are learning about ${subject} `);
+    }
+    grade(student, subject) {
+        console.log(`${student} received a perfect score on ${subject}.`);
+    }
+}
+
+class Student extends Instructor {
+
+}
