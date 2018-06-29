140869cabe6d7b7da7a556abed95b08634ac17d6
Stephanie Banks <thisgirlcancode@gmail.com>
2018-06-28 19:34:33 -0500

    still working on this! not quite understanding this....need to read up more

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,139 +1,129 @@
-/*
-  Object oriented design is commonly used in video games.  For this part of the assignment you will be implementing several constructor functions with their correct inheritance heirarchy.
+// CODE here for your Lambda Classes
+class Person {
+    constructor(info) {
+        this.name= info.name;
+        this.age= info.age;
+        this.location=info.location;
+        this.gender=info.gender;
+    }
+    greet () {
+        return `Hello my name is ${this.name} , I am from ${this.location}`
+    }
+}
+class Instructor extends Person{
+    constructor(instructorInfo) {
+        super(instructorInfo);
+        this.specialty=instructorInfo.specialty;
+        this.favLanguage= instructorInfo.favLanguage;
+        this.catchPhrase= instructorInfo.catchPhrase;
+    }
+    demo (this.subject) {
+        return `Today we are learning about ${subject[1]} where the subject is the param passed in`;
+    }
+    grade () {
 
-  In this file you will be creating three constructor functions: GameObject, CharacterStats, Humanoid.  
+    }
+}
+class Student extends Person {
+    constructor(studentInfo) {
+        super(studentInfo);
+        this.previousBackground = studentInfo.previousBackground;
+        this.className = studentInfo.className;
+        this.favSubjects = studentInfo.favSubjects;
+    }
+    listsSubjects () {
+        console.log(this.favSubjects);
 
-  At the bottom of this file are 3 objects that all inherit from Humanoid.  Use the objects at the bottom of the page to test your constructor functions.
-  
-  Each constructor function has unique properites and methods that are defined in their block comments below:
-*/
- 
-/*
-  === GameObject ===
-  * createdAt
-  * dimensions
-  * destroy() // prototype method -> returns the string 'Object was removed from the game.'
-*/
-function GameObject(attributes) {
-    this.createdAt=attributes.createdAt;
-    this.dimensions=attributes.dimensions;
-  
-  }
-  GameObject.prototype.destroy= function() {
-     return (`${this.name} was removed from the game`);
-  }
-  /*
-    === CharacterStats ===
-    * hp
-    * name
-    * takeDamage() // prototype method -> returns the string '<object name> took damage.'
-    * should inherit destroy() from GameObject's prototype
-  */
-  function CharacterStats(childAttributes) {
-    GameObject.call(this, childAttributes);
-    this.hp = childAttributes.hp;
-    this.name = childAttributes.name;
-  }
-  
-  CharacterStats.prototype = Object.create(GameObject.prototype);
-  
-  CharacterStats.prototype.takeDamage = function() {
-   return (`${this.name} took damage`);
-  }
-  
-  /*
-    === Humanoid ===
-    * faction
-    * weapons
-    * language
-    * greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
-    * should inherit destroy() from GameObject through CharacterStats
-    * should inherit takeDamage() from CharacterStats
-  */
-  function Humanoid(grandchildAttributes) {
-    CharacterStats.call(this,grandchildAttributes);
-    this.faction = grandchildAttributes.faction;
-    this.weapons = grandchildAttributes.weapons;
-    this.language = grandchildAttributes.language;
-  }
-  Humanoid.prototype =Object.create(CharacterStats.prototype);
-  
-  Humanoid.prototype.greet = function() {
-   return (`${this.name} offers a greeting in ${this.language}`);
+    }
+    PRAssignment (subject) {
+       console.log( `${student.name} has begun spring challenge on ${subject}`)
+    }
+    sprintChallenge
+}
+class projectManager extends Instructor {
+    constructor(projectmanagerInfo) {
+        super(projectmanagerInfo);
+        this.gradClassName = projectmanagerInfo.gradClassName;
+        this.favInstructor= projectmanagerInfo.favInstructor;
+    }
+    standUp() {
+        `${this.name} announces to ${this.channel} , @channel standy times!`
+    }
+    debugCode() {
+        `${this.name} debugs ${student.name}'s code on ${favSubjects[2]}`
+    }
+}
+
+
+
+const John = new Instructor({
+    name: 'Fred',
+    location: 'SF',
+    age: 37,
+    gender: 'male',
+    favLanguage: 'JavaScript, Python, Java, C++',
+    specialty: 'Front-end',
+    catchPhrase: `Say it like you mean it!`
+});
+
+const Sally = new Instructor({
+    name: 'Sally',
+    location: 'Boston',
+    age: 27,
+    gender: 'Female',
+    favLanguage: 'Go, Ruby, Java,',
+    specialty: 'Full-Stack',
+    catchPhrase: `Full Stack Rocks my socks!`
+});
+
+
+
+const Steph = new Student ({
+    name: 'Steph',
+    location: 'DFW',
+    age: 29,
+    gender: 'female',
+    favSubjects: ['Javascript, Ruby on Rails, Webdesign, UI/UX'],
+    specialty: 'Front-End',
+    previousBackground: 'The Arts & Sciences'
+    className: 'CS12'
     
-  }
-  /*
-    * Inheritance chain: Humanoid -> CharacterStats -> GameObject
-    * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
-    * Instances of CharacterStats should have all of the same properties as GameObject.
-  */
-  
-  //Test you work by uncommenting these 3 objects and the list of console logs below:
-  
-  
-    const mage = new Humanoid({
-      createdAt: new Date(),
-      dimensions: {
-        length: 2,
-        width: 1,
-        height: 1,
-      },
-      hp: 5,
-      name: 'Bruce',
-      faction: 'Mage Guild',
-      weapons: [
-        'Staff of Shamalama',
-      ],
-      language: 'Common Toungue',
-    });
-  
-    const swordsman = new Humanoid({
-      createdAt: new Date(),
-      dimensions: {
-        length: 2,
-        width: 2,
-        height: 2,
-      },
-      hp: 15,
-      name: 'Sir Mustachio',
-      faction: 'The Round Table',
-      weapons: [
-        'Giant Sword',
-        'Shield',
-      ],
-      language: 'Common Toungue',
-    });
-  
-    const archer = new Humanoid({
-      createdAt: new Date(),
-      dimensions: {
-        length: 1,
-        width: 2,
-        height: 4,
-      },
-      hp: 10,
-      name: 'Lilith',
-      faction: 'Forest Kingdom',
-      weapons: [
-        'Bow',
-        'Dagger',
-      ],
-      language: 'Elvish',
-    });
-  
-    console.log(mage.createdAt); // Today's date
-    console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
-    console.log(swordsman.hp); // 15
-    console.log(mage.name); // Bruce
-    console.log(swordsman.faction); // The Round Table
-    console.log(mage.weapons); // Staff of Shamalama
-    console.log(archer.language); // Elvish
-    console.log(archer.greet()); // Lilith offers a greeting in Elvish.
-    console.log(mage.takeDamage()); // Bruce took damage.
-    console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-  
-  
-    // Stretch task: 
-    // * Create Villian and Hero constructor functions that inherit from the Humanoid constructor function.  
-    // * Give the Hero and Villians different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
-    // * Create two new objects, one a villian and one a hero and fight it out with methods!// CODE here for your Lambda Classes
+});
+
+const Ian = new Student ({
+    name: 'Ian',
+    location: 'Miami',
+    age: 23,
+    gender: 'male',
+    favSubjects:[ 'Java', 'Python', 'Angular'],
+    specialty: 'Back-end',
+    previousBackground: 'Math'
+    className: 'CS12'
+    
+});
+
+const Joanne = new projectManager ({
+    name: 'Joanne',
+    location: 'Austin',
+    age: 30,
+    gender: 'female',
+    favSubjects: 'Javascript',
+    gradClassName: 'CS3',
+    favInstructor: 'John',
+    
+});
+
+const Jeff = new projectManager ({
+    name: 'Jeff',
+    location: 'NY',
+    age: 35,
+    gender: 'male',
+    favSubjects: 'Java',
+    gradClassName: 'CS5',
+    favInstructor: 'Andy',
+    
+});
+
+const subject=['HTML', 'CSS','React', 'DOM'];
+
+Joanne.standUp();
