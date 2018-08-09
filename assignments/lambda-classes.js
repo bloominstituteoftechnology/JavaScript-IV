c0578d9032f54b174d19d72b400b587b2a80a524
m-j-hacker <mjhacker@gmail.com>
2018-08-09 14:54:49 -0500

    Began building lambda-classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,20 @@
 // CODE here for your Lambda Classes
+// Creating Person class
+class Person {
+  constructor(attr) {
+    this.name = attr.name;
+    this.age = attr.age;
+    this.location = attr.location;
+    this.gender = attr.gender;
+  }
+  speak() {
+    console.log(`Hello, my name is ${this.name}, I am from ${this.location}`);
+  }
+}
+
+class Instructor extends Person {
+  constructor(attr) {
+    super(attr);
+    
+  }
+}
