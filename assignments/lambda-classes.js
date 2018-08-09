5f8439522ea58156e1dbf5e087eb1785d032c68d
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2018-08-09 17:43:03 -0400

    created the class Person and working on the Instructor, will commit again once student is complete

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,20 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(props){
+        this.name = props.name;
+        this.age = props.age;
+        this.location = props.location;
+        this.gender = props.gender;
+    }
+    speak() {
+        return `Hello my name is ${name}, I am from ${location}.`
+    }
+}
+
+class Instructor extends Person {
+    constructor(instructorProps){
+        this.speciality = instructorProps.speciality;
+        this.favLanguage = insctructorProps
+
+    }
+}
