20b5a60c8e1a892678734deabd28187da21c6a58
Timothy Hoang <timh1203@gmail.com>
2018-08-09 15:08:48 -0500

    Add person class to roster

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,12 @@
 // CODE here for your Lambda Classes
+class Person {
+  constructor(personAttr){
+    this.name = personAttr.name;
+    this.age = personAttr.age;
+    this.location = personAttr.locaiton;
+    this.gender = personAttr.name;
+  }
+  speak() {
+    return `Hello my name is ${this.name}, I am from ${this.location}`
+  }
+}
