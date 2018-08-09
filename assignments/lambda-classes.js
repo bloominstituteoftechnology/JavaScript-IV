c52ea88e9f3091c8c2838dba1730c62b03d5b48e
unknown <cornielleandres@gmail.com>
2018-08-09 15:05:50 -0400

    Student class done.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -50,7 +50,7 @@ class Instructor extends Person {
 
 const cindy = new Instructor({
     "name": "Cindy",
-    "age": 28,
+    "age": 38,
     "location": "California",
     "gender": "Female",
     "specialty": "Redux",
@@ -60,7 +60,7 @@ const cindy = new Instructor({
 
 const david = new Instructor({
     "name": "David",
-    "age": 30,
+    "age": 40,
     "location": "Delaware",
     "gender": "Male",
     "specialty": "React",
@@ -75,3 +75,67 @@ console.log(cindy.demo("Classes"));
 console.log(david.grade({
     "name": "Dave",
 }, "Geography"));
+
+
+// Now we need some students!
+// Student uses the same attributes that have been set up by Person
+// Student has the following unique props:
+// previousBackground i.e. what the Student used to do before Lambda School
+// className i.e. CS132
+// favSubjects. i.e. an array of the student's favorite subjects ['Html', 'CSS', 'JavaScript']
+// Student has the following methods:
+// listsSubjects a method that logs out all of the student's favoriteSubjects one by one.
+// PRAssignment a method that receives a subject as an argument and logs out that the student.name has submitted a PR for {subject}
+// sprintChallenge similar to PRAssignment but logs out student.name has begun sprint challenge on {subject}
+class Student extends Person {
+    constructor(studentProps) {
+        super(studentProps);
+        this.previousBackground = studentProps.previousBackground;
+        this.className = studentProps.className;
+        this.favSubjects = studentProps.favSubjects;
+    }
+    listsSubjects() {
+        this.favSubjects.forEach(subject => {
+            return console.log(subject);
+        });
+    }
+    PRAssignment(subject) {
+        return `${this.name} has submitted a PR for ${subject}`;
+    }
+    sprintChallenge(subject) {
+        return `${this.name} has begun sprint challenge on ${subject}`;
+    }
+}
+
+const erwin = new Student({
+    "name": "Erwin",
+    "age": 28,
+    "location": "Estonia",
+    "gender": "Male",
+    "specialty": "React",
+    "favLanguage": "Assembly",
+    "catchPhrase": "Don't have a cow, man.",
+    "previousBackground": "Secret Agent",
+    "className": "CS13",
+    "favSubjects": ["Html", "CSS", "JavaScript"],
+});
+
+const fred = new Student({
+    "name": "Fred",
+    "age": 30,
+    "location": "France",
+    "gender": "Male",
+    "specialty": "SQL",
+    "favLanguage": "C++",
+    "catchPhrase": "Ruh roh!",
+    "previousBackground": "Astronaut",
+    "className": "CS132",
+    "favSubjects": ["LESS", "React", "SQL"],
+});
+
+console.log("\nTESTING STUDENT CLASS.")
+console.log(erwin);
+console.log(fred);
+erwin.listsSubjects();
+console.log(fred.PRAssignment("HTML"));
+console.log(erwin.sprintChallenge("CSS"));
