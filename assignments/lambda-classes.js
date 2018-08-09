4cbb129a12ef7b2ef691ec6ff9837af1360851bb
travis-russell <35874527+travis-russell@users.noreply.github.com>
2018-08-09 16:46:27 -0400

    JS IV Complete

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -2,7 +2,7 @@
 class Person {
     constructor(atts) {
         this.name = atts.name;
-        this.age = atts. age;
+        this.age = atts.age;
         this.location = atts.location;
         this.gender = atts.gender;
     }
@@ -11,9 +11,9 @@ class Person {
     }
 }
 
-class Instructor {
+class Instructor extends Person {
   constructor(instrAtts) {
-      super(Person);
+      super(instrAtts);
       this.specialty = instrAtts.specialty;
       this.favLanguage = instrAtts.favLanguage;
       this.catchPhrase = instrAtts.catchPhrase;
@@ -24,14 +24,28 @@ class Instructor {
   grade(student, subject) {
       console.log(`${student.name} receives a perfect score on ${subject}`);
   }
+  changeGrade(student) {
+      let ranNum = Math.floor(Math.random() * 51 - 25);
+      if (ranNum === 0) {
+        console.log(`${student.name}'s grade remains the same`)
+      } else if (ranNum > 0) {
+          console.log(`${student.name} did great! Their grade increased by ${ranNum}%!`);
+          return student.grade = student.grade + ranNum;
+      }
+      else {
+          console.log(`${student.name} did poorly and their grade decreased by ${ranNum}%!`)
+          return student.grade = student.grade + ranNum;
+      }
+  }
 }
 
-class Student {
+class Student extends Person {
     constructor(studAtts) {
-        super(Person);
+      super(studAtts);
         this.previousBackground = studAtts.previousBackground;
         this.className = studAtts.className;
-        this.favSubjects = studAtts.favSubjects
+        this.favSubjects = studAtts.favSubjects;
+        this.grade = studAtts.grade;
     }
     listSubjects() {
         console.log(this.favSubjects);
@@ -42,11 +56,18 @@ class Student {
     sprintChallenge() {
         console.log(`${this.name} has begun sprint challenge on ${subject}`);
     }
+    readyToGrad() {
+        if (this.grade > 70) {
+            console.log(`Congratulations! You are ready to graduate!`);
+        } else {
+            console.log(`Chirp, chirp`)
+        }
+    }
 }
 
-class ProjectManagers {
+class ProjectManager extends Instructor {
     constructor(PRAtts) {
-        super(Instructor);
+        super(PRAtts);
         this.gradClassName = PRAtts.gradClassName;
         this.favInstructor = PRAtts.favInstructor;
     }
@@ -57,3 +78,42 @@ class ProjectManagers {
         console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
     }
 }
+
+const travis = new Student ({
+    name: 'Travis',
+    age: 29,
+    location: 'Washington, D.C.',
+    gender: 'M',
+    previousBackground: `Marketing/Business`,
+    className: 'CS13',
+    favSubjects: 'JavaScript',
+    grade: 95
+});
+
+const melanie = new Instructor ({
+    name: 'Melanie',
+    age: 'Not telling',
+    location: 'Classroom....always',
+    gender: 'F',
+    speciality: 'JS',
+    favLanguage: 'JS',
+    catchPhrase: 'GO GET \'EM'
+})
+
+const jack = new ProjectManager ({
+    name: 'Jack',
+    age: 35,
+    location: 'Kentucky',
+    gender: 'M',
+    gradClassName: 'CS1',
+    favInstructor: 'Josh'
+})
+
+console.log(travis.name);
+console.log(travis.PRAssignments('JavaScript'));
+console.log(melanie.catchPhrase);
+console.log(melanie.demo('JS'));
+console.log(jack.name);
+console.log(jack.debugsCode(travis, 'JavaScript'))
+jack.changeGrade(travis);
+travis.readyToGrad();
