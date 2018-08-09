0d902d12a4e181b3d2847e8461590cc8a1c67c46
m-j-hacker <mjhacker@gmail.com>
2018-08-09 15:03:47 -0500

    Fleshing out Student class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -15,6 +15,26 @@ class Person {
 class Instructor extends Person {
   constructor(attr) {
     super(attr);
-    
+    this.specialty = attr.specialty;
+    this.favLanguage = attr.favLanguage;
+    this.catchPhrase = attr.catchPhrase;
   }
+  demo(subject) {
+    console.log(`Today we are learning about ${subject}`);
+  }
+  grade(student, subject) {
+    console.log(`${student.name} receives a perfect score on ${subject}`);
+  }
+}
+
+class Student extends Person {
+  constructor(attr) {
+    super(attr);
+    this.previousBackground = attr.previousBackground;
+    this.className = attr.className;
+    this.favSubjects = attr.favSubjects;
+    }
+    listsSubjects() {
+      console.log(favSubjects);
+    }
 }
