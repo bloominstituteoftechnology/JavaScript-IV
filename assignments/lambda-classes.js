54355e593d8d64c2b491b993664bde31ef6d3d07
Ray Lomeli <raylom96@gmail.com>
2018-08-09 15:00:26 -0700

    assignment complete

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -13,42 +13,102 @@ class Person {
 
 class Instructor extends Person {
   constructor(instructorAttrs) {
-    super(attrs);
+    super(instructorAttrs);
+    this.gender = instructorAttrs.gender;
+    this.age = instructorAttrs.age;
+    this.name = instructorAttrs.name;
+    this.location = instructorAttrs.location;
     this.isInstructor = instructorAttrs.isInstructor;
     this.specialty = instructorAttrs.specialty;
     this.favLanguage = instructorAttrs.favLanguage;
     this.catchPhrase = instructorAttrs.catchPhrase;
   }
   demo(subject) {
-    console.log(`Today we are learning about ${subject}`);
+    return `Today we are learning about ${subject}`;
   }
-  grade() {
-    console.log(`{student.name} receives a perfect score on {subject}`);
+  grade(subject) {
+    return `${this.name} receives a perfect score on ${subject}`;
   }
 }
 
 class Student extends Person {
   constructor(studentAttrs) {
-    super(attrs);
+    super(studentAttrs);
+    this.gender = studentAttrs.gender;
+    this.age = studentAttrs.age;
+    this.name = studentAttrs.name;
+    this.location = studentAttrs.location;
     this.previousBackground = studentAttrs.previousBackground;
     this.className = studentAttrs.className;
     this.favSubjects = studentAttrs.favSubjects;
   }
-  listSubjects() {}
-  PRAssignmnet() {}
-  sprintChallenge() {}
+  listSubjects() {
+    return this.favSubjects;
+  }
+  PRAssignmnet(subject) {
+    return `${this.name} has submitted a PR for ${subject}`;
+  }
+  sprintChallenge(subject) {
+    return `${this.name} has begun sprint challenge on ${subject}`;
+  }
 }
 
 class ProjectManagers extends Instructor {
   constructor(pmAttrs) {
-    super(attrs);
+    super(pmAttrs);
+    this.gender = pmAttrs.gender;
+    this.age = pmAttrs.age;
+    this.name = pmAttrs.name;
+    this.location = pmAttrs.location;
     this.gradClassName = pmAttrs.gradClassName;
     this.favInstructor = pmAttrs.favInstructor;
   }
-  standUp() {
-    console.log(`{name} announces to {channel}, @channel standy times!​​​​​`);
+  standUp(channel) {
+    return `${this.name} announces to ${channel}, @channel standy times!​​​​​`;
   }
-  debugsCode() {
-    console.log(`{name} debugs {student.name}'s code on {subject}`);
+  debugsCode(student, subject) {
+    return `${this.name} debugs ${student.name}'s code on ${subject}`;
   }
 }
+
+const fred = new Instructor({
+  name: "Fred",
+  location: "Bedrock",
+  age: 37,
+  gender: "male",
+  favLanguage: "JavaScript",
+  specialty: "Front-end",
+  catchPhrase: `Don't forget the homies`
+});
+
+const ray = new Student({
+  name: "Ray",
+  location: "Austin",
+  age: 31,
+  gender: "male",
+  previousBackground: "military",
+  className: "CS13",
+  favSubjects: ["javascript"]
+});
+
+const kait = new ProjectManagers({
+  name: "Kait",
+  location: "Austin",
+  age: 30,
+  gender: "female",
+  previousBackground: "student",
+  className: "CS5",
+  favSubjects: ["python"],
+  gradClassName: "CS1",
+  favInstructor: "Josh"
+});
+
+console.log(fred.speak());
+console.log(fred.demo("chess"));
+console.log(ray.speak());
+console.log(ray.listSubjects());
+console.log(ray.PRAssignmnet("chess"));
+console.log(ray.sprintChallenge("chess"));
+console.log(kait.speak());
+console.log(kait.standUp("cs13"));
+console.log(kait.debugsCode("ray", "chess"));
