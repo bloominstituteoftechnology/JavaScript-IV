58f5a90e8a98969cd22818081faa158381d3eec8
Tom Tarpey <decagondevelopment@gmail.com>
2018-08-09 19:44:06 +0100

    created a People class and added the initial test object

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,28 @@
-// CODE here for your Lambda Classes
+class Person {
+    constructor(attr) {
+        this.name - attr.name;
+        this.age = attr.age;
+        this.location = attr.location;
+        this.gender = attr.gender;
+    }
+
+    speak() {
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
+    }
+}
+
+
+class Instructor {}
+
+
+// starting the tests 
+
+const fred = new Instructor({
+    name: 'Fred',
+    location: 'Bedrock',
+    age: 37,
+    gender: 'male',
+    favLanguage: 'JavaScript',
+    specialty: 'Front-end',
+    catchPhrase: `Don't forget the homies`
+  });
