2c1eb6a177e9bdf8731b5d60a55cc36b0656e47a
Timothy Hoang <timh1203@gmail.com>
2018-08-09 15:13:08 -0500

    Add instructor class to roster

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -10,3 +10,18 @@ class Person {
     return `Hello my name is ${this.name}, I am from ${this.location}`
   }
 }
+
+class Instructor extends Person {
+  constructor(instructorAttr){
+    super(instructorAttr);
+    this.specialty = instructorAttr.specialty;
+    this.favLanguage = instructorAttr.specialty;
+    this.catchPhrase = instructorAttr.catchPhrase;
+  }
+  demo(subject) {
+    return `Today we are learning about ${subject}`
+  }
+  grade(student, subject) {
+    return `${student.name} receives a perfect score on ${subject}`
+  }
+}
