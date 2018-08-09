1c62b0a677b03ebdc8f2536851794aa944787611
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2018-08-09 18:08:04 -0400

    set up student and instructor, going to set up pm as well as a few objects then try to watch the lesson and get the methods to work

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -7,14 +7,54 @@ class Person {
         this.gender = props.gender;
     }
     speak() {
-        return `Hello my name is ${name}, I am from ${location}.`
+        return `Hello my name is ${this.name}, I am from ${this.location}.`
     }
 }
 
 class Instructor extends Person {
     constructor(instructorProps){
+        super(instructorProps);
         this.speciality = instructorProps.speciality;
-        this.favLanguage = insctructorProps
-
+        this.favLanguage = instructorProps.favLanguage;
+        this.catchPhrase = instructorProps.catchPhrase;
+    }
+    demo(){
+        return `Today we are learning about ${this.subject}.`
+    }
+    grade(){
+        return `${student.name} receives a perfect score on {subject}.`
     }
 }
+
+class Student extends Person {
+    constructor(studentProps){
+        super(studentprops);
+        this.previousBackground = studentProps.previousBackground;
+        this.className = studentProps.className;
+        this.favsubject = studentProps.favSubject;
+    }
+    listsSubjects(){
+        return ``
+    }
+    PRAssignments(){
+        return ``
+    }
+    sprintChallenge(){
+        return ``
+    }
+}
+
+
+
+
+const fred = new Instructor({
+    name: 'Fred',
+    location: 'Bedrock',
+    age: 37,
+    gender: 'male',
+    favLanguage: 'JavaScript',
+    specialty: 'Front-end',
+    catchPhrase: `Don't forget the homies`
+  });
+
+  fred.speak();
