a4fe0a33e3fcd753cf1d868dae66aafbc5c3f305
Dewayne87 <dewaynelindsay87@gmail.com>
2018-08-22 21:23:35 -0700

    finished refactoring

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -1,40 +1,40 @@
 // Here we have a functioning solutoin to your challenge from yesterday.
 // Today your goal is to refactor all of this code to use ES6 Classes.
 // The console.log() statements should still return what is expected of them.
-
-function GameObject(options) {
-  this.createdAt = options.createdAt;
-  this.dimensions = options.dimensions;
+class GameObject {
+  constructor(obj) {
+    this.createdAt = obj.createdAt;
+    this.dimensions = obj.dimensions;
+  }
+  destroy() {
+    return `${this.name} was removed from the game.`;
+  };
 }
 
-GameObject.prototype.destroy = function() {
-  return `Object was removed from the game.`;
-};
-
-function CharacterStats(characterStatsOptions) {
-  GameObject.call(this, characterStatsOptions);
-  this.hp = characterStatsOptions.hp;
-  this.name = characterStatsOptions.name;
+class CharacterStats extends GameObject {
+  constructor(obj) {
+    super(obj)
+    this.hp = obj.hp;
+    this.name = obj.name;
+  }
+  takeDamage() {
+    return `${this.name} took damage.`;
+  };
 }
 
-CharacterStats.prototype = Object.create(GameObject.prototype);
-
-CharacterStats.prototype.takeDamage = function() {
-  return `${this.name} took damage.`;
-};
-
-function Humanoid(humanoidOptions) {
-  CharacterStats.call(this, humanoidOptions);
-  this.faction = humanoidOptions.faction;
-  this.weapons = humanoidOptions.weapons;
-  this.language = humanoidOptions.language;
+class Humanoid extends CharacterStats {
+  constructor(obj) {
+    super(obj)
+    this.faction = obj.faction;
+    this.weapons = obj.weapons;
+    this.language = obj.language;
+  }
+  greet() {
+    return `${this.name} offers a greeting in ${this.language}.`;
+  };
 }
 
-Humanoid.prototype = Object.create(CharacterStats.prototype);
 
-Humanoid.prototype.greet = function() {
-  return `${this.name} offers a greeting in ${this.language}.`;
-};
 
 const mage = new Humanoid({
   createdAt: new Date(),
