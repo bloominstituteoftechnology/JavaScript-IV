9d8ed8f29dc148186a2cbcf515dc3da813a8bc25
iepoch <staclem@live.com>
2018-08-22 20:19:50 -0700

    Updated lambda-classes, added  4 constructors > Gave methods to 2 constructors.> Building out Students constructor next.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,42 @@
 // CODE here for your Lambda Classes
+
+class Person {
+
+    constructor(allPeps){
+       this.name =allPeps.name;
+       this.age = allPeps.age;
+       this.location = allPeps.location;
+       this.gender = allPeps.gender;
+    }
+    speak() {
+
+        return `Hello  my name is ${this.name}, I am from ${this.location}`
+    }
+}
+
+class Instructor extends Person {
+
+    constructor(instructors){
+        super(instructors);
+        this.speciality = instructors.speciality;
+        this.favLanguage = instructors.favLanguage;
+        this.catchPhrase = instructors.catchPhrase;
+    }
+
+    demo(subject) {
+        return `Today we are learning about ${this.subject}`
+
+    }
+    grade(student, subject)
+    {
+        return `${students.name} recieves a perfect score on ${this.subject}`
+    }
+
+}
+class Students extends Person {
+
+}
+
+class ProjectManagers extends Instructors {
+
+}
