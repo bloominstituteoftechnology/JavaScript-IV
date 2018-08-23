1d211e0655223b680e319f79dc7025fd3b65b89c
stevekutz <stkutz@gmail.com>
2018-08-23 18:19:14 -0500

    completed lambda-classes assignment

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -21,8 +21,8 @@ class Instructor extends Person{
   demo (subject) {
     return `Today we are learning about ${subject}`;
   }
-  grade(studentObj, subject) {
-    `${studentObj.name} receives a perfect score on ${subject}`;
+  grade (studentObj, subject) {
+    return `${studentObj.name} receives a perfect score on ${subject}`;
   }
 }
 
@@ -35,8 +35,7 @@ class Student extends Person {
   }
   listSubjects() {
     this.favSubjects.forEach(function(element){
-      console.log(element + '\n');
-      console.log(` Template literal says: ${element}\n`);
+      console.log(`${element}\n`);
     });
   }
   PRAssignment(subject) {
@@ -55,10 +54,88 @@ class ProjectManager extends Instructor {
     this.favInstructor = projectManager_attributes.favInstructor;
   }
   standUp (slackChannel){
-    return `${this.name} announces to ${slackChannel}, at @channel standy times`;
+    return `${this.name} announces to ${slackChannel}, at @channel standy times!`;
   }
   debugsCode (studentObj, subject) {
     return `${this.name} debugs ${studentObj.name}'s code on ${subject}`;
   }
 }
 
+const joeI = new Instructor({
+  name: 'Joe',
+  age: 35,
+  location: 'Austin',
+  gender: 'male',
+  specialty: 'algorithms',
+  favLanguage: 'JavaScript',
+  catchPhrase: 'just Google it'
+});
+
+const maryI = new Instructor({
+  name: 'Mary',
+  age: 32,
+  location: 'Seattle',
+  gender: 'female',
+  specialty: 'data science',
+  favLanguage: 'Python',
+  catchPhrase: 'look in the data'
+});
+
+const samS = new Student({
+  name: 'Sam',
+  age: 25,
+  location: 'Denver',
+  gender: 'female',
+  previousBackground: 'cashier',
+  classname: 'iOS programming',
+  favSubjects: ['math', 'science', 'art']
+});
+
+const mikeS = new Student({
+  name: 'Mike',
+  age: 34,
+  location: 'Cupertino',
+  gender: 'male',
+  previousBackground: 'bouncer',
+  classname: 'Python programming',
+  favSubjects: ['history', 'politics', 'cage fighting']
+});
+
+const kenPM = new ProjectManager({
+  name: 'Ken',
+  age: 38,
+  location: 'Denver',
+  gender: 'male',
+  specialty: 'UX',
+  favLanguage: 'JavaScript',
+  catchPhrase: 'I love StackOverflow',
+  gradClassName: 'CS1',
+  favInstructor: 'Mary'
+});
+
+const cathyPM = new ProjectManager({
+  name: 'Cathy',
+  age: 34,
+  location: 'Chicago',
+  gender: 'female',
+  specialty: 'Full Stack Development',
+  favLanguage: 'JavaScript',
+  catchPhrase: 'Lambda Rules',
+  gradClassName: 'CS101',
+  favInstructor: 'Mike'
+});
+
+console.log(joeI.name);
+console.log(maryI.specialty);
+console.log(joeI.speak());
+console.log(joeI.demo('Front-End Development'));
+console.log(maryI.grade(mikeS, 'Web Security'));
+console.log(samS.favSubjects);
+console.log(samS.listSubjects());
+console.log(mikeS.PRAssignment('history of objects'));
+console.log(samS.sprintChallenge('JavaScript IV'));
+console.log(kenPM.name);
+console.log(kenPM.speak());
+console.log(cathyPM.catchPhrase);
+console.log(cathyPM.standUp('hired'));
+console.log(cathyPM.debugsCode(samS, 'recursion'));
