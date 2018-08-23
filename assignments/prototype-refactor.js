ac508748da791591516bed25f59dcb9c4e82e315
Jess <jessdev83@gmail.com>
2018-08-22 22:52:38 -0400

    Initial Commit, Refactor Complete
    Completed prototype-refactor.js assignment.

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -2,80 +2,80 @@
 // Today your goal is to refactor all of this code to use ES6 Classes.
 // The console.log() statements should still return what is expected of them.
 
-function GameObject(options) {
-  this.createdAt = options.createdAt;
-  this.dimensions = options.dimensions;
-}
-
-GameObject.prototype.destroy = function() {
-  return `Object was removed from the game.`;
+class GameObject {
+    constructor(options) {
+        this.createdAt = options.createdAt;
+        this.dimensions = options.dimensions;
+    }
+    destroy() {
+        return `${this.name} was removed from the game.`;
+    };
 };
 
-function CharacterStats(characterStatsOptions) {
-  GameObject.call(this, characterStatsOptions);
-  this.hp = characterStatsOptions.hp;
-  this.name = characterStatsOptions.name;
-}
 
-CharacterStats.prototype = Object.create(GameObject.prototype);
-
-CharacterStats.prototype.takeDamage = function() {
-  return `${this.name} took damage.`;
+class CharacterStats extends GameObject {
+    constructor(characterStatsOptions) {
+        super(characterStatsOptions)
+        this.hp = characterStatsOptions.hp;
+        this.name = characterStatsOptions.name;
+    }
+    takeDamage() {
+        return `${this.name} took damage.`;
+    };
 };
 
-function Humanoid(humanoidOptions) {
-  CharacterStats.call(this, humanoidOptions);
-  this.faction = humanoidOptions.faction;
-  this.weapons = humanoidOptions.weapons;
-  this.language = humanoidOptions.language;
-}
-
-Humanoid.prototype = Object.create(CharacterStats.prototype);
-
-Humanoid.prototype.greet = function() {
-  return `${this.name} offers a greeting in ${this.language}.`;
+class Humanoid extends CharacterStats {
+    constructor(humanoidOptions) {
+        super(humanoidOptions);
+        this.faction = humanoidOptions.faction;
+        this.weapons = humanoidOptions.weapons;
+        this.language = humanoidOptions.language;
+    }
+    greet() {
+        return `${this.name} offers a greeting in ${this.language}.`;
+    };
 };
 
 const mage = new Humanoid({
-  createdAt: new Date(),
-  dimensions: {
-    length: 2,
-    width: 1,
-    height: 1
-  },
-  hp: 5,
-  name: 'Bruce',
-  faction: 'Mage Guild',
-  weapons: ['Staff of Shamalama'],
-  language: 'Common Toungue'
+    createdAt: new Date(),
+    dimensions: {
+        length: 2,
+        width: 1,
+        height: 1
+    },
+    hp: 5,
+    name: 'Bruce',
+    faction: 'Mage Guild',
+    weapons: ['Staff of Shamalama'],
+    language: 'Common Toungue'
 });
 
 const swordsman = new Humanoid({
-  createdAt: new Date(),
-  dimensions: {
-    length: 2,
-    width: 2,
-    height: 2
-  },
-  hp: 15,
-  name: 'Sir Mustachio',
-  faction: 'The Round Table',
-  weapons: ['Giant Sword', 'Shield'],
-  language: 'Common Toungue'
+    createdAt: new Date(),
+    dimensions: {
+        length: 2,
+        width: 2,
+        height: 2
+    },
+    hp: 15,
+    name: 'Sir Mustachio',
+    faction: 'The Round Table',
+    weapons: ['Giant Sword', 'Shield'],
+    language: 'Common Toungue'
 });
 
 const archer = new Humanoid({
-  createdAt: new Date(),
-  dimensions: {
-    length: 1,
-    width: 2,
-    height: 4
-  },
-  hp: 10,
-  name: 'Lilith',
-  faction: 'Forest Kingdom',
-  weapons: ['Bow', 'Dagger'],
-  language: 'Elvish'
+    createdAt: new Date(),
+    dimensions: {
+        length: 1,
+        width: 2,
+        height: 4
+    },
+    hp: 10,
+    name: 'Lilith',
+    faction: 'Forest Kingdom',
+    weapons: ['Bow', 'Dagger'],
+    language: 'Elvish'
 });
 
 console.log(mage.createdAt); // Today's date
