0667ed1a9cded9f94ccfc95f82841a5fa1e9b242
Rick Fleming <elfeming111@yahoo.com>
2018-08-22 20:56:01 -0700

    Added classes and test objects to lambda-classes.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,109 @@
 // CODE here for your Lambda Classes
+class Person{
+    constructor(obj){
+        this.name = obj.name;
+        this.age = obj.age;
+        this.location = obj.location;
+        this.gender = obj.gender;
+    }
+
+    speak(){
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}.`);
+    }
+}
+
+class Instructor extends Person{
+    constructor(obj){
+        super(obj);
+        this.specialty = obj.specialty;
+        this.favLanguage = obj.favLanguage;
+        this.catchPhrase = obj.catchPhrase;
+    }
+
+    demo(subject){
+        console.log(`Today we are learning about ${subject}.`);
+    }
+
+    grade(student, subject){
+        console.log(`${student.name} receives a perfect score on ${subject}.`)
+    }
+}
+
+class Student extends Person{
+    constructor(obj){
+        super(obj);
+        this.previousBackground = obj.previousBackground;
+        this.className = obj.className;
+        this.favSubjects = obj.favSubjects;
+    }
+
+    listsSubjects(){
+        console.log(`${this.name}'s favorite subjects:`);
+        this.favSubjects.forEach((element, index) => {
+            console.log(`${index + 1}. ${element}`);
+        });
+    }
+
+    PRAssignment(subject){
+        console.log(`${this.name} has submitted a PR for ${subject}.`);
+    }
+
+    sprintChallenge(subject){
+        console.log(`${this.name} has begun sprint challenge on ${subject}.`);
+    }
+}
+
+class ProjectManager extends Instructor{
+    constructor(obj){
+        super(obj);
+        this.gradClassName = obj.gradClassName;
+        this.favInstructor = obj.favInstructor;
+    }
+
+    standUp(channel){
+        console.log(`${this.name} announces to ${channel}, @channel standy times!`);
+    }
+
+    debugsCode(student, subject){
+        console.log(`${this.name} debugs ${student.name}'s code on ${subject}.`);
+    }
+}
+
+const genericPerson = new Person({
+    name: 'Liam',
+    age: 14,
+    location: 'In Bed',
+    gender: 'Male'
+});
+
+const genericPerson = new Instructor({
+    name: 'Bob',
+    age: 36,
+    location: 'Mount Everest',
+    gender: 'Male',
+    specialty: 'JavaScript',
+    favLanguage: 'JavaScript',
+    catchPhrase: 'JavaScript is at the top of the World'
+});
+
+const genericPerson = new Student({
+    name: 'Eric',
+    age: 41,
+    location: 'Maricopa, AZ',
+    gender: 'Male',
+    previousBackground: 'Some C++ programming',
+    className: 'CSPT2',
+    favSubjects: ['JavaScript', 'HTML', 'React']
+});
+
+const genericPerson = new ProjectManager({
+    name: 'Charlie',
+    age: 24,
+    location: 'A beach somewhere',
+    gender: 'Female',
+    specialty: 'Teaching React',
+    favLanguage: 'Javascript',
+    catchPhrase: 'When you stay classy, \'this\' happens',
+    gradClassName: 'CS1',
+    favInstructor: 'Dan \'Lambda Swap\' Frehner'
+});
