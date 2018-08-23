a80e5b273184d4fba8d0914000a37d0bc7290216
BrandonMoll <brandonmoll85@gmail.com>
2018-08-22 22:27:15 -0500

    created student and projectmanager classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -29,3 +29,40 @@ class Instructor extends Person {
     };
 }
 
+class Student extends Person {
+    constructor(obj) {
+        super(obj);
+        this.previousBackground = obj.previousBackground;
+        this.className = obj.className;
+        this.favSubjects = obj.favSubjects;
+    };
+
+    listsSubjects() {
+        console.log(this.favSubjects);  //needs to split up an array?
+    };
+
+    PRAssignment(subject) {
+        console.log(`${this.name} has submitted a PR for ${subject}`)
+    };
+
+    sprintChallenge(subject) {
+        console.log(`${this.name} has begun sprint challenge on ${subject}`)
+    };
+}
+
+class ProjectManager extends Instructor {
+    constructor(obj) {
+        super(obj);
+        this.gradClassName = obj.gradClassName;
+        this.favInstructor = obj.favInstructor;
+    };
+
+    standUp() {
+        console.log(`${this.name} announces to {channel}, @channel standy times!`)
+    };
+
+    debugsCode(student, subject) {
+        console.log(`${this.name} debugs ${student.name}'s code on ${subject}`)
+    };
+}
+
