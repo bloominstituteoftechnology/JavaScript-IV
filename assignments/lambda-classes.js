f9497faed4327d6522e534c8cd26a5090402c278
rlmclaughlin <rlmclaughlin@yahoo.com>
2018-08-22 22:38:31 -0500

    created a person and instructor class for Lambda-classes.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,21 @@
 // CODE here for your Lambda Classes
+class Person{
+    constructor(personInfo){
+        this.name = personInfo.name; 
+        this.age = personInfo.age; 
+        this.location = personInfo.location;
+        this.gender = personInfo.gender; 
+    }
+    speak(){
+        return `Hello my name is ${this.name} and I am from ${this.location}`;
+    }
+}
+
+class Instructor extends Person{
+  constructor(instructorInfo){
+    super(instructorInfo);
+    this.specialty = instructorInfo.specialty; 
+    this.favLanguage = instructorInfo.favLanguage; 
+    this.catchPhrase = instructorInfo.catchPhrase; 
+  }
+}
