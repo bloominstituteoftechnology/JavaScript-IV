e591d72edf26dbc305f542e5f747b593bff46863
Jennifer King <jenbellpgh@gmail.com>
2018-08-23 00:27:22 -0400

    Stretch goals completed. Added grade to Student, added gradeStudent() to Instructor, added graduate to Student.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -27,6 +27,12 @@ class Instructor extends Person {
     grade(student, subject) {
         console.log(`${student.name} receives a perfect score on ${subject}.`);
     }
+
+    gradeStudent(student) {
+        let score = Math.round(Math.random() * (10 - -10) + -10);
+        student.grade += score;
+        console.log(`${this.name} gives ${student.name} a score of ${score}. ${student.name}'s grade is now ${student.grade}.`);
+    }
 }
 
 class Student extends Person {
@@ -35,6 +41,7 @@ class Student extends Person {
         this.previousBackground = attrs.previousBackground;
         this.className = attrs.className;
         this.favSubjects = attrs.favSubjects;
+        this.grade = attrs.grade;
     }
 
     listsSubjects() {
@@ -48,6 +55,13 @@ class Student extends Person {
     sprintChallenge(subject) {
         console.log(`${this.name} has begun the sprint challenge on ${subject}.`);
     }
+
+    graduate() {
+        if( this.grade > 70 )
+            console.log(`You did it!`);
+        console.log(`Keep working on those assignments!`);
+    }
+        
 }
 
 class ProjectManager extends Instructor {
@@ -75,6 +89,7 @@ var studentAttrs = {
     previousBackground: 'SysAdmin',
     className: 'CSPT2',
     favSubjects: ['PHP', 'MySQL', 'LESS', 'HTML'],
+    grade: 50,
 };
 const firstStudent = new Student(studentAttrs);
 
@@ -106,3 +121,10 @@ var pmAttrs = {
 const firstPM = new ProjectManager(pmAttrs);
 
 console.log(firstPM.debugsCode(firstStudent,'JavaScript IV'));
+firstPM.gradeStudent(firstStudent);
+firstPM.gradeStudent(firstStudent);
+firstPM.gradeStudent(firstStudent);
+firstPM.gradeStudent(firstStudent);
+firstPM.gradeStudent(firstStudent);
+
+firstStudent.graduate();
