834af374216d116be69aba2e3ec410b39950bd3f
Courtney Buratto <you@example.com>
2018-08-22 23:25:46 -0600

    objects built, lambda-classes.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -44,16 +44,51 @@ class Student extends Person {
     };
 }
 
-class ProjectManagers  extends Instructor {
+class ProjectManager  extends Instructor {
     constructor(obj) {
         super(obj);
         this.gradClassName = obj.gradClassName;
         this.favInstructor = obj.favInstructor;
-    };
+    }
     standUp(channel) {
         console.log(`${this.name} announces to ${channel}, @channel standy times!​​​​​`)
-    };
+    }
     debugsCode({student}, subject) {
         console.logs(`${this.name} debugs ${student.name}'s code on ${subject}`);
-    };
+    }
 }
+
+
+
+const lulu = new Instructor({
+    name: 'Lulu',
+    location: 'Chicago',
+    age: 21,
+    gender: 'non-binary',
+    favLanguage: 'Ruby',
+    specialty: 'Front-end',
+    catchPhrase: `A real bobby-dazzler`,
+  });
+
+const delphine = new Student ({
+    name: 'Delphine',
+    location: 'Perpignan',
+    age: 34,
+    gender: 'Female',
+    previousBackground: 'Barista',
+    className: 'CSPT3',
+    favSubjects: 'Math',
+  });
+
+  const Joey = new ProjectManager ({
+      name: 'Joey',
+      location: 'Honolulu',
+      age: 41,
+      gender: 'Female',
+      favLanguage: 'C++',
+      specialty: 'Back-End',
+      catchPhrase: 'Cheap as Chips',
+      gradClassName: 'CS3',
+      favInstructor: 'Dan',
+ });
+
