a247c663fb182cf0a2ef9fd1f4d0405e7088c9e3
Obed Lorisson <Obed.lorisson@gmail.com>
2018-08-23 19:24:45 -0400

    fixed indentation issue

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,98 +1,99 @@
 class Person{
     
-    constructor(personAttributes){
-        this.name = personAttributes.name;
-        this.age = personAttributes.age;
-        this.location = personAttributes.location;
-        this.gender = personAttributes.gender;
-    }
-    speak(){
-        console.log(`Hello my name is ${this.name}, I am from ${this.location}`)
-    }
+	constructor(personAttributes){
+		this.name = personAttributes.name;
+		this.age = personAttributes.age;
+		this.location = personAttributes.location;
+		this.gender = personAttributes.gender;
+	}
+	speak(){
+		console.log(`Hello my name is ${this.name}, I am from ${this.location}`)
+	}
 }
 
 class Instructor extends Person {
 
-    constructor(attributes){
-        super(attributes)
-        this.speciality = attributes.specialty;
-        this.favLanguage = attributes.favLanguage;
-        this.catchPhrase ="Don't forget the homies";
-    }
+	constructor(attributes){
+		super(attributes)
+		this.speciality = attributes.specialty;
+		this.favLanguage = attributes.favLanguage;
+		this.catchPhrase ="Don't forget the homies";
+	}
 
-    demo(subject){
-        console.log(`Today we are learning about ${subject}`)
-    }
+	demo(subject){
+		console.log(`Today we are learning about ${subject}`)
+	}
 
-    grade(student, subject){
-        console.log(`${student.name} receives a perfect score on ${subject}`)
-    }
+	grade(student, subject){
+		console.log(`${student.name} receives a perfect score on ${subject}`)
+	}
 
-    studentGrade(student){
-      let grade  = student.grade - Math.round(Math.random(100) * 100);
-      console.log(`${student.name} score ${grade} `)
-      return grade;
-    }        
+	studentGrade(student){
+		let grade  = student.grade - Math.round(Math.random(100) * 100);
+		console.log(`${student.name} score ${grade} `)
+		return grade;
+	}        
 }
+
 
 class Student extends Person{
 
-    constructor(attributes){
-        super(attributes)
-        this.previousBackground = attributes.previousBackground;
-        this.className = attributes.className;
-        this.favSubjects = attributes.favSubjects;
-        this.grade = Math.round(Math.random(100)* 100);
-    }
+	constructor(attributes){
+		super(attributes)
+		this.previousBackground = attributes.previousBackground;
+		this.className = attributes.className;
+		this.favSubjects = attributes.favSubjects;
+		this.grade = Math.round(Math.random(100)* 100);
+	}
 
-    listSubjects(){
-      let subjects = this.favSubjects;
-      return subjects.forEach(element => console.log(element));
-    }
+	listSubjects(){
+		let subjects = this.favSubjects;
+		return subjects.forEach(element => console.log(element));
+	}
 
-    PRAssignment(subject){
-        console.log(`${this.name} has submitted a PR for ${subject}`)
-    }
+	PRAssignment(subject){
+		console.log(`${this.name} has submitted a PR for ${subject}`)
+	}
 
-    sprintChallenge(subject){
-        console.log(`${this.name} has begun a sprint challenge on ${subject}`)
-    }
+	sprintChallenge(subject){
+		console.log(`${this.name} has begun a sprint challenge on ${subject}`)
+	}
 
-    graduate(){
-      if(this.grade > 70 ){
-       console.log (`${this.name } is ready to graduate`)
-      }else{
-        console.log(`${this.name} need more study time`)
-      }
-    }
+	graduate(){
+		if(this.grade > 70 ){
+			console.log (`${this.name } is ready to graduate`)
+		}else{
+			console.log(`${this.name} need more study time`)
+		}
+	}
 }
 
 
 class ProjectManagers extends Instructor{
     constructor(attributes){
-        super(attributes)
-        this.gradClassName = attributes.gradClassName;
-        this.favInstructor = attributes.favInstructor;
-    }
-    standUp(channel = this.gradClassName){
-        console.log(`${this.name} announces to ${channel}, @channel standy times!`)
-    }
+			super(attributes)
+			this.gradClassName = attributes.gradClassName;
+			this.favInstructor = attributes.favInstructor;
+	}
+	standUp(channel = this.gradClassName){
+			console.log(`${this.name} announces to ${channel}, @channel standy times!`)
+	}
 
-    debugsCode(student,subject){
-        console.log(`${this.name} debugs ${student.name} code on ${subject}`)
-    }
+	debugsCode(student,subject){
+			console.log(`${this.name} debugs ${student.name} code on ${subject}`)
+	}
 }
 
 
 const jerry = new Student({
-    name: 'Jerry',
-    location: 'Bedrock',
-    age: 25,
-    gender: 'male',
-    previousBackground:'accounting',
-    className:'CSPT2',
-    favSubjects:['html', 'Less', 'JS']
-  });
+	name: 'Jerry',
+	location: 'Bedrock',
+	age: 25,
+	gender: 'male',
+	previousBackground:'accounting',
+	className:'CSPT2',
+	favSubjects:['html', 'Less', 'JS']
+});
 
 const fred = new Instructor({
   name: 'Fred',
@@ -105,12 +106,12 @@ const fred = new Instructor({
 });
 
 const luis = new ProjectManagers({
-    name: 'luis',
-    location: 'Bedrock',
-    age: 28,
-    gender: 'male',
-    gradClassName:'CSPT2',
-    favInstructor:'John'
+	name: 'luis',
+	location: 'Bedrock',
+	age: 28,
+	gender: 'male',
+	gradClassName:'CSPT2',
+	favInstructor:'John'
 });
 
 
