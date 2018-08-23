c8603568cfd95876dfed218916ff4e9bba79bc32
Marc de la Torre <mdelatorre1314@gmail.com>
2018-08-22 21:06:21 -0700

    prototype-refactor.js completed. lambda-classes.js 50% complete

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,27 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(stats){
+        this.name = stats.name,
+        this.age = stats.age,
+        this. location = stats.location,
+        this. gender = stats.gender
+    }
+    speak (){
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}.`)
+    }
+}
+
+class Instructor extends Person {
+    constructor(details) {
+    super (details);
+    this.specialty = details.specialty,
+    this.favLanguage = details.favLanguage,
+    this.catchPhrase = details.catchPhrase
+    }
+    demo(subject) {
+        console.log(`Today we are learning about ${subject}.`);
+    }
+    grade(student) {
+        console.log(`${student.name} receives a perfect score on ${subject}.`);
+    }
+}
