3d50bb58a95836aee0a1134a75e1723250557176
David Reichardt <davereichardt@gmail.com>
2018-08-23 09:46:05 -0400

    started working on lambda-class.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,20 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(info){
+        this.name = info.name;
+        this.city = info.city;
+        this.age = info.age;
+        this.gender = info.gender;
+    }
+    speak(){ return `Hello, my name is ${this.name}, and I am from ${this.city}`; }
+}
+
+class Instructor extends Person {
+    constructor(instructorInfo){
+    super(instructorInfo);
+    this.specialty = instructorInfo.specialty;
+    this.favLanguage = instructorInfo.favLanguage;
+    this.catchPhrase = instructorInfo.catchPhrase;
+    }
+    demo(subject){ return `Today we are learning about ${subject}!`; }
+}
