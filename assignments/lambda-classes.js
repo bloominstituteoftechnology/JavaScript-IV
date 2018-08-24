308fd48bc6ce8aef5daf2e8fe12f74f1c38d06b7
Pascale Pierre <pascaleee@gmail.com>
2018-08-23 22:29:12 -0500

    waited too long to commit

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,82 @@
 // CODE here for your Lambda Classes
+class Person {
+  constructor(name, age, gender, location) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.location = location;
+  }
+
+  get speak() {
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
+  }
+
+}
+
+class Instructor extends Person {
+  constructor(name, age, gender, location, favLanguage, catchPhrase) {
+    super(name, age, gender, location);
+    this.favLanguage = favLanguage;
+    this.catchPhrase = catchPhrase;
+  }
+  demo(subject) {
+    return `Today we are learning about ${subject}`;
+  }
+
+  grade(student, subject) {
+    return `${student} receives a perfect score on ${subject}`;
+  }
+}
+
+class Student extends Person {
+  constructor(name, age, gender, location, previousBackground, className, favSubjects) {
+    super(name, age, gender, location);
+    this.previousBackground = previousBackground;
+    this.className = className;
+    this.favSubjects = favSubjects;
+  }
+  listsSubjects() {
+    // for (let i = 0; i < favSubjects.length; i++) {
+      return this.favSubjects;
+    // }
+  }
+
+  PRAssignment(subject) {
+    return `${student.name} has submitted a PR for ${this.subject}`;
+  }
+
+  sprintChallenge(subject) {
+    return `${student.name} has begun sprint challenge on ${this.subject}`;
+  }
+}
+
+class ProjectManager extends Instructor {
+  constructor(favLanguage, catchPhrase, gradClassName, favInstructor) {
+    super(favLanguage, catchPhrase);
+    this.fgradClassName = gradClassName;
+    this.favInstructor = favInstructor;
+  }
+  standUp(channel) {
+    return `{$this.name} announces to ${channel}, @channel standy times!​​​​​`;
+  }
+
+  debugsCode(student, subject) {
+    return `${this.name} debugs ${student.name}'s code on ${subject}`;
+  }
+}
+
+const fred = new Instructor(
+'Fred',
+'Bedrock',
+37,
+  'male',
+  'JavaScript',
+'Front-end',
+`Don't forget the homies`
+);
+
+const pascale = new Student('Pascale', 30, 'female', 'New York', 'artist', 'CSPT2', [
+  'math', 'science', 'english'
+]);
+
+console.log(pascale);
--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -1,40 +1,70 @@
-// Here we have a functioning solutoin to your challenge from yesterday.
+// Here we have a functioning solution to your challenge from yesterday.
 // Today your goal is to refactor all of this code to use ES6 Classes.
 // The console.log() statements should still return what is expected of them.
 
-function GameObject(options) {
+class GameObject {
+  constructor(options){
   this.createdAt = options.createdAt;
   this.dimensions = options.dimensions;
 }
 
-GameObject.prototype.destroy = function() {
-  return `Object was removed from the game.`;
-};
 
-function CharacterStats(characterStatsOptions) {
-  GameObject.call(this, characterStatsOptions);
+destroy() {
+    return `Object was removed from the game.`;
+  }
+}
+
+class CharacterStats extends GameObject {
+  constructor(characterStatsOptions){
+  super(characterStatsOptions);
   this.hp = characterStatsOptions.hp;
   this.name = characterStatsOptions.name;
 }
 
-CharacterStats.prototype = Object.create(GameObject.prototype);
-
-CharacterStats.prototype.takeDamage = function() {
+takeDamage() {
   return `${this.name} took damage.`;
 };
+}
 
-function Humanoid(humanoidOptions) {
-  CharacterStats.call(this, humanoidOptions);
+
+// CharacterStats(characterStatsOptions) {
+//   GameObject.call(this, characterStatsOptions);
+//   this.hp = characterStatsOptions.hp;
+//   this.name = characterStatsOptions.name;
+// }
+//
+// CharacterStats.prototype = Object.create(GameObject.prototype);
+//
+// CharacterStats.prototype.takeDamage = function() {
+//   return `${this.name} took damage.`;
+// };
+
+class Humanoid extends CharacterStats {
+  constructor(name, humanoidOptions){
+  super(humanoidOptions);
   this.faction = humanoidOptions.faction;
   this.weapons = humanoidOptions.weapons;
   this.language = humanoidOptions.language;
 }
 
-Humanoid.prototype = Object.create(CharacterStats.prototype);
-
-Humanoid.prototype.greet = function() {
+greet() {
   return `${this.name} offers a greeting in ${this.language}.`;
 };
+}
+//
+// function Humanoid(humanoidOptions) {
+//   CharacterStats.call(this, humanoidOptions);
+//   this.faction = humanoidOptions.faction;
+//   this.weapons = humanoidOptions.weapons;
+//   this.language = humanoidOptions.language;
+// }
+
+
+// Humanoid.prototype = Object.create(CharacterStats.prototype);
+//
+// Humanoid.prototype.greet = function() {
+//   return `${this.name} offers a greeting in ${this.language}.`;
+// };
 
 const mage = new Humanoid({
   createdAt: new Date(),
