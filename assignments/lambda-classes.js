9831b4fb7986d6c6219c410d55ef0399485cc036
Willie Yturralde <willieino@hotmail.com>
2018-08-24 00:17:09 -0700

    modified js file to complete the stretch goals

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,6 +1,9 @@
 // CODE here for your Lambda Classes
 function school() {
-    let ourThis = "";
+    let instructorThis = "";            //kind of messy way of calling sibling functions
+    let studentThis = "";               //kind of messy way of calling sibling functions
+    let isReady = false;
+
 class Person {
     constructor(stats) {
         this.name = stats.name;
@@ -20,7 +23,8 @@ class Instructor extends Person {
         this.specialty = isGoodAt.specialty;
         this.favLanguage = isGoodAt.favLanguage;
         this.catchPhrase = isGoodAt.catchPhrase;
-        ourThis = this;
+        this.student = isGoodAt.student;
+        instructorThis = this;                      //save the 'this' value for the instructor
     }
     demo() {
         return `Today we are learning about ${this.specialty}`;
@@ -28,11 +32,18 @@ class Instructor extends Person {
     grades() {
         return `${this.name} receives a perfect score on ${this.specialty}`;
     }
-    addPoints(orig) {
+    addPoints(orig) {                                                       //instructor adds new random scores to current student grade
         let newPoints = Math.floor(Math.random() * (100 - 1)) + 1;
-        //let newPoints = 0
-        console.log(`Students grade was ${orig} and now after new grade is added, it is ${orig += newPoints} `);
-        return orig;
+        console.log(`${studentThis.name}'s grade was ${orig} and now after new grade is added, it is ${orig += newPoints}% `);
+        return orig;                                                        //returns the new student grade to graduate function in student
+    }
+    checkGrades() {                                                         //uses the 'this' from the student to run the graduate function
+        let gradFlag = studentThis.graduate(studentThis.grade);             //checks the graduation flag
+        if (gradFlag) {
+            return `It's time to graduate, your grade is ${studentThis.grade}%.`;
+        }
+      
+       return `Student isnt ready yet`;     
     }
 };
 
@@ -42,6 +53,9 @@ class Student extends Person {
         this.previousBackground = details.previousBackground;
         this.className = details.className;
         this.favSubject = details.favSubject;
+        this.instructor = details.instructor;
+        this.pm = details.pm;
+        studentThis = this;                                 //save the 'this' value for the student
             
     }
 
@@ -55,15 +69,18 @@ class Student extends Person {
     sprintChallenge() {
         return `${this.name} has begun sprint challenge on ${this.favSubject}`;
     } 
-    graduate() {
-      if (this.grade > 70) {
-        return `It's time to graduate, your grade is ${this.grade}%.`;
+    graduate(orig) {
+      if (orig > 70) {
+          isReady = true;                               //checks the grade and returns true if ready for graduation
+        return isReady;    
       } else {
-        let modPoints = ourThis.addPoints(this.grade);
+        let modPoints = instructorThis.addPoints(orig); //not ready to graduate so instrctor checks grades again
+        this.grade = modPoints;
         if (modPoints > 70) {
-           return `It's time to graduate, your grade is ${modPoints}%.`;
+            isReady = true;
+            return isReady; 
         }
-        return modPoints;
+        return false;
        }
     }
 };
@@ -89,28 +106,32 @@ const burt = new Student({
     gender: 'male',
     location: 'California',
     grade: 60,
+    instructor: 'beulah',
+    pm: 'raymond',
     previousBackground: 'electrician',
     className: 'CS2',
     favSubject: ['JS', 'Science']
 });
 
-const beualah = new Instructor({
-    name: 'InstructorBeulah',
+const beulah = new Instructor({
+    name: 'beulah',
     age: 42,
     gender: 'female',
     location: 'California',
     grade: 80,
+    student: 'burt',
     specialty: 'physical education',
     favLanguage: 'english',
     catchPhrase: 'Whats up' 
 });
 
 const raymond = new ProjectManagers({
-    name: 'Raymond',
+    name: 'raymond',
     age: 23,
     gender: 'male',
     location: 'Notsureville',
     grade: 90,
+    student: 'burt',
     specialty: 'JavaScript',
     favLanguage: 'JS',
     catchPhrase: 'PM me if you need help',
@@ -119,13 +140,13 @@ const raymond = new ProjectManagers({
 });
 
 console.log(burt.speak());  
-console.log(burt.graduate());  
+//console.log(burt.graduate());  
 console.log(burt.listSubject()); //21
 console.log(raymond.specialty);
-console.log(beualah.demo());
-console.log(beualah.grades());
+console.log(beulah.demo());
 console.log(raymond.standUp());
 console.log(raymond.debugsCode());
+console.log(beulah.checkGrades()); //checks the grades and is initiated by the instructor
 
 };
 
