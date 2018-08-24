8d5bcea870a487cf847a914e8ef4c47099be6526
Ryntak94 <ryan.matthews.professional@gmail.com>
2018-08-23 22:02:04 -0400

    create sub-class Instructor

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -10,3 +10,18 @@ class Person    {
         return `Hello my name is ${this.name}, I am from ${this.location}`;
     }
 }
+
+class Instructor extends Person {
+    constructor(instructorObject)   {
+        super(instructorObject)
+        this.specialty = instructorObject.specialty;
+        this.favLanguage = instructorObject.favLanguage;
+        this.catchPhrase = instructorObject.catchPhrase;
+    }
+    demo(subject)  {
+        return `Today we are learning about ${subject}`;
+    }
+    grade(student, subject) {
+        return `${student.name} receives a perfect score on ${subject}`
+    }
+}
