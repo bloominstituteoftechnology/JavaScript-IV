a0f1508876a63a3e48f844c0a3d0fcdb652a26c2
Marguel Gutierrez <marguel.gtz@gmail.com>
2018-08-23 22:03:26 -0500

    Person class created

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,12 @@
 // CODE here for your Lambda Classes
+class Person {
+  constructor(personAttr) {
+    this.name = personAttr.name;
+    this.age = personAttr.age;
+    this.location = personAttr.location;
+    this.gender = personAttr.gender;
+  }
+  speak() {
+    return `Hello my name is ${this.name}, I am from ${this.location} `;
+  }
+}
