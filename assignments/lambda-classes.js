8bc441630cede561b8d0ead649aad701ec37062c
Griffin <griffinshuth@gmail.com>
2018-08-23 21:31:08 -0600

    3rd commit i redit the classes for practice!

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,132 +1,122 @@
 // CODE here for your Lambda Classes
-class person {
-    constructor(personObj){
-      this.name = personObj.name;
-      this.age = personObj.age;
-      this.location = personObj.location;
-      this.gender = personObj.gender;
-    }
-    speak() {
-      return `Hello my name is ${this.name} , i am from ${this.location}`
-    }
+class Person{
+  constructor(obj){
+  this.name = obj.name;
+  this.age = obj.age;
+  this.location = obj.location;
+  this.gender = obj.gender;
   }
-  class Instructor extends person {
-    constructor(inst){
-      super(inst);
-      this.specialty = inst.specialty;
-      this.favLanguage = inst.favLanguage;
-      this.catchPhrase = inst.catchPhrase;
-    }
-    demo(subject){
-      return `Today we are learning about ${subject}`;
-    }
-    grade(student, subject){
-      return `'${student.name} receives a perfect score on ${subject}'`
-    };
+  speak (){
+    return `Hello my name is ${this.name } , I am from ${this.location}.`
   }
-  class Student extends Instructor{
-    constructor(stdnt){
-      super(stdnt);
-      this.previousBackground = stdnt.previousBackground;
-      this.className = stdnt.className;
-      this.favSubjects = stdnt.favSubjects;
-    }
-    listsSubjects(){
-      return `${this.name}'s subjects are ${this.favSubjects.join(', ')}`;
-    }
-    PRAssignment (subject) {
-      return `${this.name} has submitted a PR for ${subject}`;
-    }
-    sprintChallenge (subject){
-      return `${this.name} has begun sprint challenge on ${subject}`;
-    }
+}
+class Instructor extends Person{
+  constructor(teacher){
+  super(teacher)
+  this.specialty = teacher.specialty;
+  this.favLanguage = teacher.favLanguage;
+  this.catchPhrase = teacher.catchPhrase;
   }
-  class projectManager extends Student{
-    constructor(pm){
-      super(pm);
-      this.gradClassName = pm.gradClassName;
-      this.favInstructor = pm.favInstructor;
-    }
-    standUp(channel){
-     return `${this.name} announces to {channel}, @channel standy times!​​​​​`;
-    }
-    debugsCode(student, subject){
-      return `${this.name} debugs ${student.name}'s code on ${subject}`;
-    }
+  demo(subject){
+    return `Today we are learning about ${subject}`
   }
-  //Instructors
-  const Dan = new Instructor({
+  grade(student, subject){
+    return `${student} receives a perfect score on ${subject}`
+  }
+}
+class Student extends Instructor{
+  constructor(studentobj){
+  super(studentobj)
+  this.previousBackground = studentobj.previousBackground;
+  this.className = studentobj.className;
+  this.favSubjects = studentobj.favSubjects;
+  }
+  listsSubjects(subject){
+    return `${this.name}'s subjects are ${this.favSubjects.join(', ')}`
+  }
+  PRAssignment(subject){
+    return `${this.name} has submitted a PR for ${subject}`
+  }
+  sprintChallenge(subject){
+    return `${this.name} has submitted a PR for ${subject}`
+  }
+}
+class projectManager extends Instructor{
+  constructor(pm){
+    super(pm)
+    this.gradClassName = pm.gradClassName;
+    this.favInstructor = pm.favInstructor;
+  }
+  standUp(channel){
+  return `${this.name} announces to {channel}, @channel standy times!`
+  }
+  debugsCode(student, subject){
+    return `${this.name} debugs ${student}'s code on ${subject}`
+  }
+}
+
+
+//Instructor
+const Dan = new Instructor({
   name: 'Dan',
-  location:'Salt Lake City',
-  age: 33,
-  gender: 'male',
+  location: 'SLC',
+  age: 32,
+  gender:'Male',
   favLanguage: 'javaScript',
-  specialty: 'web design',
-  catchPhrase: 'Stay Classy!',
-  });
-  
-  const Joe = new Instructor({
-    name: 'Joe',
-    location: 'Herriman',
-    age: 27,
-    gender: 'male',
-    favLanguage: 'Swift',
-    specialty: 'App Development',
-    catchPhrase: 'You are over thinking it',
-  });
-  //students
-    const Griffin = new Student({
-      name: 'Griffin',
-      location: 'Tooele',
-      age: 27,
-      gender: 'male',
-      previousBackground: 'sales',
-      className: 'CSPT2',
-      favSubjects: ['javaScript', 'IOS', 'HTML'],
-    });
-  
-    const Austin = new Student({
-      name:'Austin',
-      location: 'Kaysville',
-      age: 28,
-      gender: 'male',
-      previousBackground: 'stock boy',
-      className: 'CSPT2',
-      favSubjects: ['CSS', 'HTML', 'javaScript'],
-    });
-  
-  // Project Managers
-    
-  const Raymond = new projectManager({
-      name: 'Raymond',
-      location: 'Houston',
-      age: 26,
-      gender: 'Male',
-      favLanguage: 'Python',
-      specialty: 'web design',
-      catchPhrase: 'lets do this!',
-      gradClassName: 'CS16',
-      favInstructor: 'Dan'
-  });
-  
-  const Moises = new projectManager({
-      name: 'Moises',
-      location: 'Orlando',
-      age: 26,
-      gender: 'Male',
-      favLanguage: 'Javascript',
-      specialty: 'Design',
-      catchPhrase: 'You can do it!',
-      gradClassName: 'CS7',
-      favInstructor: 'Joe'
-  });
-  
-  
-  console.log(Griffin.demo('IOS'));
-  console.log(Joe.grade(Griffin, 'javaScript'));
-  console.log(Griffin.listsSubjects());
-  console.log(Austin.PRAssignment('HTMl'));
-  console.log(Austin.sprintChallenge('CSS'));
-  console.log(Raymond.standUp('cs16'));
-  console.log(Moises.debugsCode(Austin, 'IOS'));
-  
+  specialty: 'web development',
+  catchPhrase: 'You can do this!'
+});
+const Joe = new Instructor({
+  name: 'Joe',
+  location: 'Herriman',
+  age: 27,
+  gender: 'Male',
+  favLanguage: 'Swift',
+  specialty: 'App development',
+  catchPhrase: 'I think you are over thinking it',
+});
+// Student
+const Griffin = new Student ({
+  name: 'Griffin',
+  location: 'Tooele',
+  age: 27,
+  gender: 'Male',
+  previousBackground: 'sales',
+  className: 'CSPT2',
+  favSubjects: ['javaScript', 'IOS', 'React']
+});
+const Austin = new Student ({
+  name: 'Austin',
+  location: 'Kaysville',
+  age: 28,
+  gender: 'Male',
+  previousBackground: 'sales',
+  className: 'CSPT5',
+  favSubjects: ['java', 'CSS', 'HTML']
+});
+// Project Managers
+const Raymond = new projectManager ({
+  name: 'Raymond',
+  location: 'Houston',
+  age: 26,
+  gender: 'Male',
+  gradClassName: 'CS10',
+  favInstructor: 'Dan'
+});
+const Moises = new projectManager ({
+  name: 'Moises',
+  location: 'Orlando',
+  age: 25,
+  gender: 'Male',
+  gradClassName: 'CS8',
+  favInstructor: 'Joe',
+});
+
+console.log(Griffin.speak('Griffin','Tooele'));
+console.log(Joe.demo('IOS'));
+console.log(Dan.grade('Austin', 'HTML'));
+console.log(Austin.listsSubjects(', '));
+console.log(Griffin.PRAssignment( 'javaScript'));
+console.log(Austin.sprintChallenge('CSS'));
+console.log(Raymond.standUp('Raymond'));
+console.log(Moises.debugsCode('Griffin', 'IOS'));
