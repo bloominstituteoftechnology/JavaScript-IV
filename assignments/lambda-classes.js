461e5f3e886c1aefccd8101893468648b6e17feb
BrendonKootz <BrendonKootz@gmail.com>
2018-08-23 19:12:01 -0500

    Set up classes in lambda-class.js | About to write test objects

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -3,36 +3,68 @@
 // Set up LambdaAssociate as head class
 // Need Instructor, student & PM classes
 
-class LambdaAssociate{
-	constructor(Name,Age,Location,Gender,Speak){
-		this.Name = Name;
-		this.Age = Age;
-		this.Location = Location;
-		this.Gender = Gender;
-		this.Speak = function(){
-			return `Hello my name is ${this.name} I am from ${this.Location}.`;};
-		}
+class Person{
+	constructor(PersonProp){
+		this.name = PersonProp.name;
+		this.age = PersonProp.age;
+		this.Location = PersonProp.Location;
+		this.gender = PersonProp.gender;
 	}
-
-class Instructor extends LambdaAssociate{
-	constructor(specialty,favLanguage,catchPhrase,demo,grade){
-		super(Name,Age,Location,Gender,Speak);
-		this.specialy = specialy;
-		this.favLanguage = favLanguage;
-		this.catchPhrase = catchPhrase;
-		this.demo = demo;	
-		this.grade = grade;
-	}
+	speak(){return `Hello my name is ${this.name}, I am from ${this.Location}`;};
 }
 
-const TestBoi = new Instructor;
-TestBoi.grade = "F";
+class Instructor extends Person{
+	constructor(InstructorProp){
+		super(InstructorProp);
+		this.specialty = InstructorProp.specialty;
+		this.favLanguage = InstructorProp.favLanguage;
+		this.catchPhrase = InstructorProp.catchPhrase;
+	}
+	demo(subject){return `Today we are learning about ${subject}`;};
+	grade(student,subject){return
+		`${student.name} receives a perfect score on ${subject}`;
+	};
+}
+
+class Student extends Person{
+	constructor(StudentProp){
+		super(StudentProp);
+		this.previousBackground = StudentProp.previousBackground;
+		this.className = StudentProp.className;
+		this.favSubjects = StudentProp.favSubjects;
+	}
+	listsSubjects(){return this.favSubjects;};
+	
+	PRAssignment(subject){
+	return `${this.name} has submitted a PR for ${subject}`;};
+	
+	sprintChallenge(subject){
+	return `${this.name} has begun sprint challenge on ${subject}`;};
+
+}
+
+class PM extends Instructor{
+	constructor(PMprop){
+		super(PMprop);
+		this.gradClassName = PMprop.gradClassName;
+		this.favInstructor = PMprop.favInstructor;
+	}
+	standUp(name,channel){return `${name} announces to ${channel}, @channel standy times!`;};
+	debugsCode(name,Student,subject){return 
+		`${name} debugs ${Student.name}s code on ${subject}`;
+	};
+}
 
 
-console.log(LambdaAssociate);		// Test that parent class works
-console.log(LambdaAssociate.Name); 	// Should be "undefined"
+// Person Test
 
-console.log(TestBoi.grade);
+// Instructor Test
+
+// Student Test
+
+// PM test
+
+
 
 
 
