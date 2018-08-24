20dd70c10f1be3d21f568f7ee48adef47778d918
Emmanium <pradoe.ep@gmail.com>
2018-08-23 19:38:39 -0600

    revised randomGrade method

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -24,14 +24,23 @@ class Instructor extends Person {
     grade(student, subject){
         return `${student.name} receives a perfect score on ${subject}`;
     };
-    randomGrade(student, min, max){
+    /* randomGrade(student, min, max){
         const randomInt = Math.floor(Math.random() * (max - min + 1)) + min;
         if (student.grade + randomInt >= 100){
             return student.grade = 100;
         } else {
             return student.grade = student.grade + randomInt;
         }
-    };
+    }; */
+    //revised method
+    randomGrade(student){
+        const randomNum = Math.floor(Math.random() * 21) - 10;
+        if (student.grade + randomNum >= 100) {
+            return student.grade = 100;
+        } else {
+            return student.grade = student.grade + randomNum;
+        }
+    }
 }
 //Student Class
 class Student extends Person {
@@ -126,12 +135,7 @@ const Mykael = new ProjectManager({
 //testing methods on Project Manager class
 console.log(Mykael.standUp('cspt2_myke'));
 console.log(Mykael.debugsCode(Emmanuel, 'Classes'));
-console.log(Mykael.randomGrade(Emmanuel, -10, 10));
-console.log(Mykael.randomGrade(Emmanuel, -10, -5));
-console.log(Mykael.randomGrade(Emmanuel, -10, -5));
-console.log(Mykael.randomGrade(Emmanuel, -10, -5));
-console.log(Mykael.randomGrade(Emmanuel, -10, -5));
-console.log(Mykael.randomGrade(Emmanuel, -10, -5));
+console.log(Mykael.randomGrade(Emmanuel));
 
 //testing message when student.grade is below 70%
 console.log(Emmanuel.graduate());
