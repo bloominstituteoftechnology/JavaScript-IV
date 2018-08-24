f26f6a819f31da96b554a77a2ab8c791af6388a3
rlmclaughlin <rlmclaughlin@yahoo.com>
2018-08-23 19:15:32 -0500

    created the ProjectManager class for assignment

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -26,5 +26,58 @@ class Instructor extends Person{
   }
 }
 
-let james = new Instructor("jumping", "english", "hey man"); 
-console.log(james.grade("james", 23)); 
+ 
+class Student extends Person{
+    constructor(studentInfo){
+      super(studentInfo);
+      this.previousBackground = studentInfo.previousBackground; 
+      this.className = studentInfo.className; 
+      this.favSubjects = [studentInfo.favSubjects]; 
+    }
+    listsSubjects(){
+        return `${this.favSubjects}`;
+    }
+    PRAssignment(subject){
+        return `${this.name} has submitted a PR for ${subject}`; 
+    }
+    sprintChallenge(sprintSub){
+        return `${this.name} has begun sprint challenge on ${sprintSub}`;
+    }
+}
+
+class projectManager extends Instructor{
+    constructor(projectManagerInfo){
+        super(projectManagerInfo); 
+        this.gradClassName = projectManagerInfo.gradClassName; 
+        this.favInstructor = projectManagerInfo.favInstructor; 
+    }
+    standUp(channel){
+        return `${this.name} announces to ${channel}, @channel standy times!​`; 
+    }
+    debugsCode(student, subject){
+        return `${this.name} debugs ${student.name}'s code on ${subject}`
+    }
+}
+
+const fred = new Instructor({
+    name: 'Fred',
+    location: 'Bedrock',
+    age: 37,
+    gender: 'male',
+    favLanguage: 'JavaScript',
+    specialty: 'Front-end',
+    catchPhrase: `Don't forget the homies`,
+  });
+
+  const Ryan = new Student({
+    name: 'Ryan',
+    location: 'Dallas',
+    age: 31,
+    gender: 'male',
+    previousBackground: 'music', 
+    className: "CSPT2",
+    favSubjects: `Javascript, Less/Css `
+  });
+
+
+console.log(Ryan.listsSubjects()); //undefined
