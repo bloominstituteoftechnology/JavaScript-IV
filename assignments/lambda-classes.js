9e010fcf6d7ce86b16055be6b193b520a5b04e63
Ryntak94 <ryan.matthews.professional@gmail.com>
2018-08-23 21:58:25 -0400

    create base-class 'Person'

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,12 @@
 // CODE here for your Lambda Classes
+class Person    {
+    constructor(personObject)   {
+        this.name = personObject.name;
+        this.age = personObject.age;
+        this.location = personObject.location;
+        this.gender = personObject.gender;
+    }
+    speak() {
+        return `Hello my name is ${this.name}, I am from ${this.location}`;
+    }
+}
