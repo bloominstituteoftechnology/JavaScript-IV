92ea5353ce14f634e2062ffc16c85bc2759de815
Lorenzo Evans <lorenzo.evans94@gmail.com>
2018-08-25 23:56:00 -0400

    Working on the base classes for lambda school & co.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,28 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(obj) {
+        this.name = obj.name ;
+        this.age = obj.age ;
+        this.location = obj.location ;
+        this.gender = obj.gender ;
+
+    }
+    speak(){
+        return `Hello, my name is ${this.name}, I am from ${this.location}.`
+    }
+}
+
+class Instructor extends Person {
+    constructor(obj) {
+        this.specialty = obj.specialty ;
+        this.favLanguage = obj.favLanguage ;
+        this.catchPhrase = obj.catchPhrase ;
+    }
+
+    demo(subject) {
+        return `Today we are learning about ${subject}.`
+    }
+    grade(studentObj, subjectStr) {
+        return `${studentObj.name0} recieves a perfect score on ${subjectStr}.`
+    }
+}
