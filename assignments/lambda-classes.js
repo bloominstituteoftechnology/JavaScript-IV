f05cb35f979e07bcdb6c91c0d9dcd7b89157767f
Lorenzo Evans <lorenzo.evans94@gmail.com>
2018-08-26 00:09:07 -0400

    Outlined Lambda Classes constructors.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -26,3 +26,48 @@ class Instructor extends Person {
         return `${studentObj.name0} recieves a perfect score on ${subjectStr}.`
     }
 }
+
+class Students extends Person {
+    constructor(obj){
+        this.previousBackround = obj.previousBackround ;
+        this.className = obj.className ;
+        this.favSubjects = obj.favSubjects ;
+
+    }
+
+    listsSubjects(){
+        let index = obj.favSubjects.length - 1 ;
+        for (let i = 0; i <= index ; i++) {
+            console.log(obj.favSubjects[i]) ;
+        }
+    }
+
+    PRAssignment(subjectArg){
+        let pullText = `${student.name} has submitted a PR for ${subjectArg}.` ;
+        console.log(pullText);
+    }
+
+    sprintChallenge(subjectArg){
+        let sprintText = `${student.name} has begun sprint challenge on ${subjectArg}.` ;
+        console.log(sprintText);
+    }
+}
+
+class ProjectManager extends Instructor{
+    constructor(obj){
+        gradClassName = obj.gradClassName ;
+        favInstructor = obj.favInstructor ;
+    }
+
+    standUp(slackArg){
+
+        let standUpText = `${name} announces to ${slackArg}, @Channel stand up time!`
+
+    }
+
+    debugsCode(studentObj, subjectStr){
+        
+        debugTxtt = `${name} debugs ${student.name}${"'s"} code on ${subjectStr}.`
+
+    }
+}
