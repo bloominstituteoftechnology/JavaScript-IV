5168e1a06153ec754487bf8a24e2764c0f61fbf7
unknown <quan.nguyen199712@gmail.com>
2018-09-13 14:58:47 -0400

    created person class for lambda school

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,12 @@
 // CODE here for your Lambda Classes
+class Person{
+    constructor(personAttribute){
+        this.name=personAttribute.name;
+        this.age=personAttribute.name;
+        this.location=personAttribute.name;
+        this.gender= personAttribute.name;
+    }
+    speak(){
+        return `Hello my name is $this.name`
+    }
+}
