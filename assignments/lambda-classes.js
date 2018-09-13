c61d6cc1974874a8562c9135329f66c92ecbd478
TRIF3X <thewizardofcrypto@gmail.com>
2018-09-13 13:23:20 -0500

    lambda-class started

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,11 @@
-// CODE here for your Lambda Classes
+class Person {
+    construction(attributes){
+        this.age = attributes.age;
+        this.name = attributes.name;
+        this.location = attributes.location;
+        this.gender = attributes.gender;
+    }
+    speak() {
+        return `Hello my name is ${this.name}, I am from ${this.location}.`
+    }
+}
