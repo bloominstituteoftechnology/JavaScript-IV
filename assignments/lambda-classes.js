6036c415c207f1d9d04fe719d029df636e766114
Katy Hollobaugh <khollobaugh@gmail.com>
2018-09-13 15:35:13 -0400

    created lambda classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,60 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(attributes) {
+        this.age = attributes.age;
+        this.name = attributes.name;
+        this.location = attributes.location;
+        this.gender = attributes.gender;
+    }
+    speak() {
+        console.log(``)
+    }
+}
+
+class Student extends Person {
+    constructor(studentAttributes) {
+        super(studentAttributes);
+        this.previousbackground = studentAttributes.previousbackground;
+        this.className = studentAttributes.className;
+        this.favSubjects = studentAttributes.favSubjects;
+        this.grade = studentAttributes.grade;
+    }
+    listsSubjects() {
+
+    }
+    PRAssignments() {
+
+    }
+    sprintChallenge() {
+
+    }
+}
+
+class Instructor extends Person {
+    constructor(instructorAttributes) {
+        super(instructorAttributes);
+        this.specialty = instructorAttributes.specialty;
+        this.favLanguage = instructorAttributes.favLanguage;
+        this.catchPhrase = instructorAttributes.catchPhrase;
+    }
+    demo() {
+
+    }
+    grade() {
+
+    }
+}
+
+class PM extends Instructor {
+    constructor(pmAttributes) {
+        super(pmAttributes);
+        this.gradClassName = pmAttributes.gradClassName;
+        this.favInstructor = pmAttributes.favInstructor;
+    }
+    standUp() {
+
+    }
+    debugCode() {
+
+    }
+}
