ff614eab59b371bc9ba6834a19cb047ea993d3ab
Cwright0428 <cwright0428@yahoo.com>
2018-09-13 16:35:55 -0500

    adding PM class to proj

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,84 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(qualities) {
+        this.name = qualities.name;
+        this.age = qualities.age;
+        this.location = qualities.location;
+        this.gender = qualities.gender;
+    }
+    speak() {
+        return ('Hello my name is'
+            `${qualities.name} 'and I'm from' ${qualities.location}`);
+    };
+};
+console.log(speak);
+
+class Instructors extends Person {
+    constructor(instrQualities) {
+        super(qualities);
+        this.specialty = instrQualities.specialty;
+        this.favLanguage = instrQualities.favLanguage;
+        this.catchPhrase = instrQualities.catchPhrase;
+
+    };
+
+    demo() {
+        return ('Today we are learning about'
+            `${.subject}`)
+    };
+
+    grade() {
+        return (`${student.name} 'receives a perfect score on' {.subject}`);
+    };
+};
+
+console.log(demo)
+console.log(grade)
+class PMs extends Instructors {
+    constructor(pmQualities) {
+        super(qualities);
+        this.specialty = instrQualities.specialty;
+        this.favLanguage = instrQualities.favLanguage;
+        this.catchPhrase = instrQualities.catchPhrase;
+
+    };
+
+    demo() {
+        return ('Today we are learning about'
+            `${.subject}`)
+    };
+
+    grade() {
+        return (`${student.name} 'receives a perfect score on' {.subject}`);
+    };
+};
+
+console.log(demo)
+console.log(grade)
+
+class Student extends Person {
+    constructor(stuQualities) {
+        super(stuQualities);
+        this.previousBackground =stuQualities.previousBackground;
+        this.className = stuQualities.className;
+        this.favSubjects = stuQualities.favSubjects;
+    };
+
+    listSubjects() {
+        return
+    };
+
+    PRAssignment() {
+        return (`${student.name} 'has submitted a PR for' {.subject}`);
+    };
+    sprintChallenge() {
+        return (`${student.name} 'has begun sprint challange' {.subject}`);
+    };
+
+
+
+};
+
+console.log(ListSubjects);
+console.log(PRAssignment);
+console.log(sprintChallenge);
