85dc0dd99fc3d023b81f2bcbf03d465b49031083
Mindysueasaurus <mymindyyeager@gmail.com>
2018-09-13 16:34:55 -0500

    object creation finished, finished the listSubjects method, added console.logs

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -10,28 +10,32 @@ class Person {
     }
     //methods
     speak() {
-        console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
+        return `Hello my name is ${this.name}, I am from ${this.location}`;
     }
 }
 
 //instructor class
 class Instructor extends Person{
     constructor(instAttr) {
-        super(InstOption);
+        super(instAttr);
         this.specialty = instAttr.specialty;
         this.favLanguage = instAttr.favLanguage;
         this.catchPhrase = instAttr.catchPhrase;
     }
     //methods
     demo(subject){
-        console.log(`Today we are learning about ${subject}`);
+        return `Today we are learning about ${subject}`;
     }
     grade(student, subject) {
-        console.log(`${student.name} recieves a perfect score on ${subject}`);
+        return `${student.name} recieves a perfect score on ${subject}`;
     }
 }
 
+
+
+
 //student class
+
 class Student extends Person {
     constructor(studOptions) {
         super(studOptions);
@@ -40,33 +44,36 @@ class Student extends Person {
         this.favSubjects = studOptions.favSubjects;
     }
     //methods
-    listSubjects() {
+    listsSubjects() {
         //logs out all of the students fav subjects 1 by 1
+        return this.favSubjects.forEach(subjects => console.log(subjects));
     }
-    PRAssignments(student, subject) {
-        console.log(`${student.name} has submitted a PR for ${subject}`)
+    PRAssignments(subject) {
+        return `${this.name} has submitted a PR for ${subject}`;
     }
     sprintChallenge(subject) {
-        console.log(`${student.name} has begun sprint challenge on ${subject}` );
+        return `${this.name} has begun sprint challenge on ${subject}`;
     }
 }
+
 
 //project manager class
 
 class ProjectManager extends Instructor {
     constructor(pMOptions) {
-        super();
+        super(pMOptions);
         this.gradClassName = pMOptions.gradClassName;
         this.favInstructor = pMOptions.favInstructor;
     }
     //methods
     standUp(channel) {
-        console.log(`${this.name} announces to ${channel}, @channel standy times!`);
+        return `${this.name} announces to ${channel}, @channel standy times!`;
     }
     debugsCode(student, subject) {
-        console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
+        return `${this.name} debugs ${student.name}'s code on ${subject}`;
     }
 }
+
 
 //instructor objects
 
@@ -103,6 +110,9 @@ const picard = new Instructor({
 
 });
 
+
+
+
 //student objects
 
 const mindy = new Student({
@@ -112,7 +122,7 @@ const mindy = new Student({
   gender: "F",
   previousBackground: "self taught",
   className: "FSW-14",
-  favSubjects: "Python"
+  favSubjects: ["Python", "Javascript", "LESS"]
 });
 
 const kevin = new Student({
@@ -122,9 +132,42 @@ const kevin = new Student({
     gender: "M",
     previousBackground: "self taught",
     className: "FSW-16",
-    favSubjects: "Raspberry PI"
+    favSubjects: ["Raspberry PI", "Python", "C"]
+});
+
+
+
+//project manager objects
+
+const dude = new ProjectManager({
+   name: "The Dude",
+   age: 45,
+    location: "California",
+    gender: "M",
+    className: "CS4",
+    favInstructor: "Josh"
+});
+
+const tammy = new ProjectManager({
+ name: "Tammy",
+  age: 25,
+  location: "Seattle",
+  gender: "F",
+  className: "CS6",
+  favInstructor: "Mr Rogers"
 });
 
 
 
 
+console.log(roger.speak());
+console.log(josh.demo("Javascript"));
+console.log(picard.grade(mindy, "Javascript-IV"));
+console.log(kevin.speak());
+console.log(mindy.listsSubjects());
+console.log(kevin.PRAssignments("LESS"));
+console.log(mindy.sprintChallenge("Javascript"));
+console.log(kevin.listsSubjects());
+console.log(dude.speak());
+console.log(tammy.standUp("FSW14"))
+console.log(dude.debugsCode(kevin, "Javascript"));
