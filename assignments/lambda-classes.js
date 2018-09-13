de5aa3337bbee6fcf34aebb58e87f44b24e3a080
Corey Gumbs <cgumbs@Coreys-MBP-3.fios-router.home>
2018-09-13 15:27:06 -0400

    [chore] finished Person class - lambda-class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,29 @@
 // CODE here for your Lambda Classes
+
+//Class Person - Base Class
+class Person{
+    //constructor
+    constructor(personAttributes){
+        this.name = personAttributes.name;
+        this.age = personAttributes.age;
+        this.gender = personAttributes.gender;
+        this.location = personAttributes.location;
+    }
+    //method
+    speak(){
+        return `Hello my name is ${this.name}, I am from ${this.location}.`;
+    }
+}
+
+const testPerson = new Person({
+    "name":"Corey",
+    "age": 41,
+    "gender": 'M',
+    "location" : "New York City"
+})
+
+console.log(testPerson.name);
+console.log(testPerson.age);
+console.log(testPerson.location);
+console.log(testPerson.gender);
+console.log(testPerson.speak());
