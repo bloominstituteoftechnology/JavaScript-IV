55fe42b99aa98491de099284f251492755fae6e7
Gannon <gkleblanc@gmail.com>
2018-09-13 15:42:24 -0400

    finished making my base constructor Person class with speak method

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,14 @@
 // CODE here for your Lambda Classes
+
+class Person {
+    constructor (Pattributes) {
+        this.name = Pattributes.name;
+        this.age = Pattributes.age;
+        this.location = Pattributes.location;
+        this.gender = Pattributes.gender;
+    }
+
+    speak() {
+        return `Hello my name is ${this.name}, I am from ${this.location}`
+    }
+}
