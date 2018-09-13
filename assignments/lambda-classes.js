38c62f0a2918f81346764bcaca4b8971eff19bcb
David247365 <ds051128@gmail.com>
2018-09-13 17:44:12 -0500

    Created Project Manager Class, and Student Class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,22 +1,27 @@
 // CODE here for your Lambda Classes
+
+
+// Person base-class
 class Person {
-    constructor(personAttributes){
-        this.name = personAttributes.name;
-        this.age = personAttributes.age;
-        this.location = personAttributes.location;
-        this.gender = personAttributes.gender;
+    constructor(personInfo){
+        this.name = personInfo.name;
+        this.age = personInfo.age;
+        this.location = personInfo.location;
+        this.gender = personInfo.gender;
     }
     speak(){
-        console.log(`Hello my name is ${this.location}, I am from ${this.location}`);
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
     }
 }
 
+//Instructor Class
+
 class Instructor extends Person {
-    constructor(instructorAttributes){
-        super(instructorAttributes)
-        this.specialty = instructorAttributes.specialty;
-        this.favLanguage = instructorAttributes.favLanguage;
-        this.catchPhrase = instructorAttributes.catchPhrase;
+    constructor(instructorInfo){
+        super(instructorInfo)
+        this.specialty = instructorInfo.specialty;
+        this.favLanguage = instructorInfo.favLanguage;
+        this.catchPhrase = instructorInfo.catchPhrase;
     }
     demo(subject){
         console.log(`Today we are learning about ${subject}`);
@@ -26,4 +31,102 @@ class Instructor extends Person {
     }
 }
 
-class 
+// Student class
+
+class Student extends Person {
+    constructor(studentInfo){
+        super(studentInfo);
+        this.previousBackground = studentInfo.previousBackground;
+        this.className = studentInfo.className;
+        this.favSubjects = studentInfo.favSubjects;
+    }
+    listsSubjects(){
+        console.log(`${this.favSubjects}`);
+    }
+    PRAssignment(subject){
+        console.log(`${student.name} has submitted a PR for ${subject}`);
+    }
+    sprintChallenge(subject){
+        console.log(`${student.name} has begun sprint challenge on ${subject}`);
+    }
+}
+
+// Project Managers Class
+
+class ProjectManagers extends Person{
+    constructor(pmInfo){
+        super(pmInfo);
+        this.gradClassName = pmInfo.gradClassName;
+        this.favInstructor = pmInfo.favInstructor;
+    }
+    standUp(channel){
+        console.log(`${this.name} announces to {channel} @channel standy times!`);
+    }
+    debugsCode(student, subject){
+        console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
+    }
+}
+
+// Instructor Objects
+
+const micheal = new Instructor({
+    'name': 'Micheal',
+    'age': 25,
+    'location': 'Hawaii',
+    'gender': 'M',
+    'specialty': 'Data Analyst',
+    'favLanguage': 'Python',
+    'catchPhrase': "Let's play the pronoun game."
+});
+
+const hyun = new Instructor({
+    'name': 'Hyun',
+    'age': 23,
+    'location': 'Seuol',
+    'gender': 'F',
+    'specialty': 'Software Engineer',
+    'favLanguage': 'C',
+    'catchPhrase': "반갑습니다"
+});
+
+const felix = new Instructor({
+    'name': 'Felix',
+    'age': 37,
+    'location': 'Sweden',
+    'gender': 'M',
+    'specialty': 'Full Stack Developer',
+    'favLanguage': 'JavScript',
+    'catchPhrase': "Jag saknar dig."
+});
+
+//Student Objects
+
+const david = new Student({
+    'name': 'David',
+    'age': 22,
+    'location': 'Texas',
+    'gender': 'M',
+    'previousBackground': 'Customer Service',
+    'className': 'FSW14',
+    'favSubjects': ['HTML', 'CSS', 'Javascript'],
+});
+
+const  = new Student({
+    'name': 'David',
+    'age': 22,
+    'location': 'Texas',
+    'gender': 'M',
+    'previousBackground': 'Customer Service',
+    'className': 'FSW14',
+    'favSubjects': ['HTML', 'CSS', 'Javascript'],
+});
+
+const david = new Student({
+    'name': 'David',
+    'age': 22,
+    'location': 'Texas',
+    'gender': 'M',
+    'previousBackground': 'Customer Service',
+    'className': 'FSW14',
+    'favSubjects': ['HTML', 'CSS', 'Javascript'],
+});
