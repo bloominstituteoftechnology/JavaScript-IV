4f217eaa9d780a5d090b2903495bfc5fb2ba8b0e
Brandon Vilaychith <blvilaychith@outlook.com>
2018-09-13 13:07:30 -0500

    Added Instructor class and mothods

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -5,8 +5,25 @@ class Person {
         this.age = personAttrs.age;
         this.location = personAttrs.location;
         this.gender = personAttrs.gender;
-    }
+    }// End of Person Constructor
+    // Person Methods
     speak() {
         return `Hello my name is ${this.name}, I am from ${this.location}`;
     }
-}
+}// End of Person Class
+
+class Instructor extends Person {
+    constructor(instructorAttrs) {
+        super(instructorAttrs);
+        this.specialty = instructorAttrs.specialty;
+        this.favLanguage = instructorAttrs.favLanguage;
+        this.catchPhrase = instructorAttrs.catchPhrase;
+    }// End of Instructor Constructor
+    // Instructor Methods
+    demo(subject) {
+        return `Today we are learning about ${subject}`;
+    }
+    grade(student, subject) {
+        return `${student.name} receives a perfect score on ${subject}`;
+    }
+}// End of Instructor Class
