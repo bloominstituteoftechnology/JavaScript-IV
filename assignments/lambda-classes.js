bfeb66dee305809bba27688344daa7086be93b84
Carlos Lantigua <clantigua954@gmail.com>
2018-09-13 14:58:01 -0400

    completed student class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,2 +1,71 @@
 // CODE here for your Lambda Classes
 
+class Person {
+  constructor(attr) {
+    this.name = attr.name;
+    this.age = attr.age;
+    this.location = attr.location;
+    this.gender = attr.gender;
+  }
+  // Methods go here:
+  speak() {
+    console.log(`Hello my name is ${this.name} I am from ${this.location}`);
+  }
+};
+
+class Instructor extends Person {
+  constructor(instAttr) {
+    super(instAttributes)
+    this.specialty = instAttr.specialty
+    this.favLanguage = instAttr.favLanguage;
+    this.catchPhrase = instAttr.catchPhrase;
+  }
+  // Methods go here:
+  demo() {
+    console.log(`Today we are learning about ${subject}.`);
+  } // end of demo
+  grade() {
+    console.log(`${student.name} receives a perfect score on ${subject}`);
+  } // end of grade
+}
+
+class Student extends Person {
+  constructor(studAttr) {
+    this.previousBackground = studAttr.previousBackground;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
