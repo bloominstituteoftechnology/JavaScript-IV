9d32e5f6f2ec8764c797a9bf581850d07eedc7be
Shawn Antonucci <shawnantonucci2018@gmail.com>
2018-09-13 11:48:10 -0700

    Working on inplimenting class constructors

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,2 +1,55 @@
 // CODE here for your Lambda Classes
+const subjectsArr = ['HTML', 'CSS', 'JavaScript']
 
+class Person {
+    constructor(attributes) {
+        this.name = attributes.name;
+        this.location = attributes.location;
+        this.age = attributes.age;
+        this.gender = attributes.gender;
+    }
+
+    speak() {
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}.`)
+    }
+}
+
+class Instructor extends Person{
+    constructor(instructorAttributes) {
+    super(instructorAttributes);
+    this.specialty = instructorAttributes.specialty;
+    this.favLanguage = instructorAttributes.favLanguage;
+    this.catchPhrase = instructorAttributes.catchPhrase;
+    }
+
+    demo(subject) {
+        console.log(`Today we are learning about ${subject}`);
+    }
+
+    grade(student, subject) {
+        console.log(`${student} receives a perfect score on ${subject}`);
+    }
+}
+
+class Student extends Person{
+    constructor(studentAttributes) {
+    super(studentAttributes);
+    this.previousBackground = studentAttributes.previousBackground;
+    this.className = studentAttributes.className;
+    this.favSubjects = studentAttributes.favSubjects;
+    }
+
+    listsSubjects() {
+        for (let i = 0; i < subjectsArr.length; i++) {
+            console.log(subjectsArr[i]);
+        }        
+    }
+
+    PRAssignment(subject) {
+        console.log(`${this.name} has submitted a PR for ${subject}.`);
+    }
+
+    sprintChallenge(subject) {
+        console.log(`${this.name} has begun sprint challenge on ${subject}.`);
+    }
+}
