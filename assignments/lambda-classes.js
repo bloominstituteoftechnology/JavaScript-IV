a8d1d7382df92bff03a8e8f0941b0d47d0a4e35c
Mindysueasaurus <mymindyyeager@gmail.com>
2018-09-13 14:43:47 -0500

    base class person set up, instructor class set up, student class started

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,41 @@
 // CODE here for your Lambda Classes
+
+//base/master class
+class Person {
+    constructor(attributes) {
+        this.name = attributes.name;
+        this.age = attributes.age;
+        this.location = attributes.location;
+    }
+    //methods
+    speak() {
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
+    }
+}
+
+//instructor class
+class Instructor extends Person{
+    constructor(instOption) {
+        super(InstOption);
+        this.specialty = instOption.specialty;
+        this.favLanguage = instOption.favLanguage;
+        this.catchPhrase = instOption.catchPhrase;
+    }
+    //methods
+    demo(subject){
+        console.log(`Today we are learning about ${subject}`);
+    }
+    grade(student, subject) {
+        console.log(`${student.name} recieves a perfect score on ${subject}`);
+    }
+}
+
+//student class
+class Student extends Person {
+    constructor(studOptions) {
+        super(studOptions);
+        
+    }
+    
+}
+
