ca7be5686498e480a15485ba2b6fb634b72468c9
zacharyarney <zachary.arney@gmail.com>
2018-09-13 17:47:06 -0500

    Created individuals

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -61,3 +61,69 @@ class ProjectManager extends Instructor {
     console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
   }
 } // end of ProjectManager class
+
+const josh = new Instructor({
+  name: 'Josh',
+  location: 'Salt Lake',
+  age: 'UNKNOWN',
+  gender: 'male',
+  favLanguage: 'Javascript',
+  specialty: 'rad-ness',
+  catchPhrase: "Let's take a 5 minute break",
+});
+
+josh.demo('Javascript');
+
+const evilJosh = new Instructor({
+  name: 'Hsoj',
+  location: 'Saltier Lake',
+  age: 'INFINITE',
+  gender: 'yes',
+  favLanguage: 'LOLCODE',
+  specialty: 'bad-ness',
+  catchPhrase: "Let's not take a 5 minute break",
+});
+
+const zach = new Student({
+  name: 'Zach',
+  location: 'Minneapolis',
+  age: '28',
+  gender: 'male',
+  previousBackground: 'failing',
+  className: 'FSW14',
+  favSubjects: ['JS', 'HTML', 'other'],
+});
+
+const evilZach = new Student({
+  name: 'Hcaz',
+  location: 'Heck',
+  age: 'NONE',
+  gender: 'yes',
+  previousBackground: 'winning',
+  className: 'CS14',
+  favSubjects: ['evil JS', 'HTML', 'other'],
+});
+
+const luis = new ProjectManager({
+  name: 'Luis',
+  age: 24,
+  location: 'Los Angeles',
+  gender: 'male',
+  specialty: 'PMing',
+  favLanguage: 'Javascript',
+  catchPrase: "That's tricky",
+  gradClassName: 'CS10',
+  favInstructor: 'Josh',
+});
+
+const evilLuis = new ProjectManager({
+  name: 'Evil Luis',
+  age: 42,
+  location: 'San Francisco',
+  gender: 'male',
+  specialty: 'Not PMing',
+  favLanguage: 'Evil Javascript',
+  catchPrase: "That's not so tricky",
+  gradClassName: 'CS100',
+  favInstructor: 'Hsoj',
+});
