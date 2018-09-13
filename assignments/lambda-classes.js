3d0c3fdfd464da271302723b25425508e12d78ff
Blake <koenneckerb@gmail.com>
2018-09-13 12:33:03 -0700

    Finished Instructor class in lambda-classes.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -43,3 +43,17 @@ class Student extends Person {
         return `${this.name} has begun sprint challenge on ${subject}`
     }
 }
+
+class ProjectManager extends Instructor {
+    constructor(attributes) {
+        super(attributes);
+        this.gradClassName = attributes.gradClassName;
+        this.favInstructor = attributes.favInstructor;    
+    }
+    standUp(channel) {
+        return `${this.name} announces to ${channel}, @channel standy times!`;
+    }
+    debugsCode(student, subject) {
+        return `${this.name} debugs ${student.name}'s code on ${subject}`;
+    }
+}
