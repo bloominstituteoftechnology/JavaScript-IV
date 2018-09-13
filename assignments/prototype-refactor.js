31b1f4805fe69ff189a00ed398d794ff76e46735
Troy Amlee <troyamlee19@gmail.com>
2018-09-13 13:17:58 -0500

    Troy Amlee [Refactor] Finished refactoring, onward to classes

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -2,39 +2,43 @@
 // Today your goal is to refactor all of this code to use ES6 Classes.
 // The console.log() statements should still return what is expected of them.
 
-function GameObject(options) {
-  this.createdAt = options.createdAt;
-  this.dimensions = options.dimensions;
-}
+class GameObject {
+  constructor (options) {
+      this.createdAt = options.createdAt;
+      this.dimensions = options.dimensions;
+    }
 
-GameObject.prototype.destroy = function() {
-  return `Object was removed from the game.`;
-};
+    destroy() {
+      return `Object was removed from the game.`;
+    }
 
-function CharacterStats(characterStatsOptions) {
-  GameObject.call(this, characterStatsOptions);
-  this.hp = characterStatsOptions.hp;
-  this.name = characterStatsOptions.name;
-}
+}  
 
-CharacterStats.prototype = Object.create(GameObject.prototype);
+class CharacterStats extends GameObject {
+  constructor (characterStatsOptions) {
+      super(characterStatsOptions)
+      this.hp = characterStatsOptions.hp;
+      this.name = characterStatsOptions.name;
+  }
 
-CharacterStats.prototype.takeDamage = function() {
+ takeDamage () {
   return `${this.name} took damage.`;
-};
+}; 
 
-function Humanoid(humanoidOptions) {
-  CharacterStats.call(this, humanoidOptions);
-  this.faction = humanoidOptions.faction;
-  this.weapons = humanoidOptions.weapons;
-  this.language = humanoidOptions.language;
 }
 
-Humanoid.prototype = Object.create(CharacterStats.prototype);
-
-Humanoid.prototype.greet = function() {
-  return `${this.name} offers a greeting in ${this.language}.`;
-};
+class Humanoid extends CharacterStats {
+  constructor (humanoidOptions) {
+      super (humanoidOptions)
+      this.faction = humanoidOptions.faction;
+      this.weapons = humanoidOptions.weapons;
+      this.language = humanoidOptions.language;
+  }
+  greet () {
+      return `${this.name} offers a greeting in ${this.language}.`;
+  }
+ 
+}
 
 const mage = new Humanoid({
   createdAt: new Date(),
@@ -88,3 +92,11 @@ console.log(archer.language); // Elvish
 console.log(archer.greet()); // Lilith offers a greeting in Elvish.
 console.log(mage.takeDamage()); // Bruce took damage.
 console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+
+
+
+
+
+
+
+
