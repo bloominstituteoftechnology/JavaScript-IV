b48c42a50d2f55c04de2dd629f810f4065110f78
2thumbstech <2thumbstech@gmail.com>
2018-09-13 16:59:35 -0600

    assignment

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,13 @@
 // CODE here for your Lambda Classes
+
+class Erica{
+    constructor(attributes){
+        this.name=attributes.name;
+        this.age=attributes.age;
+        this.location=attributes.location;
+        this.gender=attributes.gender;
+    }
+    speak(){
+        return 'Hola, mi nombre es ${this.name},im'
+    }
+} 
