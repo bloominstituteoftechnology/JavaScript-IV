ee60cfd296c4f0ad12322d7b1d5401f99c520d03
Andre Myrick <amyrick.820@gmail.com>
2018-09-13 15:50:43 -0400

    created person class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,11 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(stuff) {
+        this.name = stuff.name;
+        this.age = stuff.age;
+        this.location = stuff.location;
+        this.gender = stuff.gender;
+    }
+    speak = () => `Hello my name is ${this.name}, I am from ${this.location}`
+}
+
