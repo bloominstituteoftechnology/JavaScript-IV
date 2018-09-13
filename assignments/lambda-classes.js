5de367a6a962d27f5ef1b4162f61b14da5ef709a
bummings <sup@fauxkno.com>
2018-09-13 17:44:00 -0400

    almost there

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -19,10 +19,47 @@ class Instructor extends Person {
     this.favLanguage = instructorProps.favLanguage;
     this.catchPhrase = instructorProps.catchPhrase;
   }
+
   demo(subject) {
     console.log(`Today we are learning about ${subject}.`);
   }
   grade({student}, subject){
     console.log(`${student.name} receives a perfect score on ${subject}!`);
+  }
+}
+
+
+class Student extends Instructor {
+  constructor(studentProps) {
+    super(studentProps);
+    this.previousBackground = studentProps.previousBackground;
+    this.className = studentProps.className;
+    this.favSubject = studentProps.favSubject;    
+  }
+  listsSubjects(){
+    forEach.this.favSubject(function(subject){
+      console.log(subject);
+    });
+  }
+  PRAssignment(subject){
+    console.log(`${student.name} has submitted a PR for ${subject}.`);
+  }
+  sprintChallenge() {
+    console.log(`${student.name} has begun spring challenge on {subject}`);
+  }
+}
+
+
+class ProjectManager extends Instructor {
+  constructor(projectManagerProps) {
+    super(projectManagerProps);
+    this.gradClassName = projectManagerProps.gradClassName;
+    this.favInstructor = projectManagerProp.favInstructor;
+  }
+  standUp(channel, subject) {
+    console.log(`${this.name} announces to ${channel}, stand up time!`);
+  }
+  standUp(subject) {
+    console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
   }
 }
