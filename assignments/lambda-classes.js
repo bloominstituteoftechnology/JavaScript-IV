f8c7f2c3f4e4206847db709e1cefe9ed8bcedea2
Adam <ajmboosh@gmail.com>
2018-09-13 14:34:15 -0500

    made Person and Instructor classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,31 @@
 // CODE here for your Lambda Classes
+//base-class declaration
+class Person {
+  constructor(personInfo) {
+    this.name = personInfo.name;
+    this.age = personInfo.age;
+    this.location = personInfo.location;
+    this.gender = personInfo.gender;
+  }
+  //methods
+  speak() {
+    console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
+  }
+}
+
+//extended class from Person
+class Instructor extends Person {
+  constructor(insInfo) {
+    super(insInfo);
+    this.speciality = insInfo.speciality;
+    this.favLanguage = insInfo.favLanguage;
+    this.catchPhrase = insInfo.catchPhrase;
+  }
+  //methods
+  demo(subject) {
+    console.log(`Today we are learning about ${subject}`);
+  }
+  grade(student, subject) {
+    console.log(`${student.name} receives a perfect score on ${subject}`);
+  }
+}
