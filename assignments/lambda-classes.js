6b8a2d2f6fa243b622ccae41d8d940638a2fd704
Katy Hollobaugh <khollobaugh@gmail.com>
2018-09-13 15:53:05 -0400

    added methods

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -7,7 +7,7 @@ class Person {
         this.gender = attributes.gender;
     }
     speak() {
-        console.log(``)
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}`)
     }
 }
 
@@ -20,13 +20,13 @@ class Student extends Person {
         this.grade = studentAttributes.grade;
     }
     listsSubjects() {
-
+        console.log(`${this.favSubjects}`);
     }
-    PRAssignments() {
-
+    PRAssignments(subject) {
+        console.log(`${this.student.name} has submitted a PR for ${this.subject}`);
     }
-    sprintChallenge() {
-
+    sprintChallenge(subject) {
+        console.log(`${this.student.name} has begun sprint challenge on ${this.subject}`);
     }
 }
 
@@ -37,11 +37,11 @@ class Instructor extends Person {
         this.favLanguage = instructorAttributes.favLanguage;
         this.catchPhrase = instructorAttributes.catchPhrase;
     }
-    demo() {
-
+    demo(subject) {
+        console.log(`Today we are learning about ${this.subject}`);
     }
-    grade() {
-
+    grade(student, subject) {
+        console.log(`${this.student.name} receives a perfect score on ${subject}`);
     }
 }
 
@@ -51,10 +51,10 @@ class PM extends Instructor {
         this.gradClassName = pmAttributes.gradClassName;
         this.favInstructor = pmAttributes.favInstructor;
     }
-    standUp() {
-
+    standUp(slackChannel) {
+        console.log(`${this.name} announces to ${this.slackChannel}, @channel standup time!`)
     }
-    debugCode() {
-
+    debugCode(student, subject) {
+        console.log(`${this.name} debugs ${this.student.name}'s code on ${this.subject}`);
     }
 }
