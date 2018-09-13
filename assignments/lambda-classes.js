118a75bd63b53bf25ff07d0f59691d170a066809
ThomasUtsey <tomutsey@yahoo.com>
2018-09-13 18:55:11 -0400

    project completion

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,4 +1,3 @@
-// CODE here for your Lambda Classes
 
 // ------------------------------------------
 // Person Class starts here
@@ -6,6 +5,8 @@ class Person {
   constructor(people){
     this.name = people.name;
     this.location = people.location;
+    this.age = people.age;
+    this.gender = people.gender;
   }
   speak(){
     return(`Hello my name is ${this.name} I am from ${this.location}`)
@@ -25,7 +26,7 @@ class Person {
       this.catchPhrase = instruct.catchPhrase;
     }
     demo(subject){console.log(`Today we are learning about ${subject}`)}
-    grade(student, subject){console.log(`${student.name} receives a perfect score on ${subject}`)}
+    grade(){console.log(`${student.name} receives a perfect score on ${subject}`)}
   }
 //Instructor class ends here
 //--------------------------------------------
@@ -33,16 +34,20 @@ class Person {
 //--------------------------------------------
 //Students class starts here
 
-  class Student extends Instructor{
+  class Student extends Person{
     constructor(stud){
       super(stud);
       this.previousBackground = stud.previousBackground;
       this.className = stud.className;
       this.favSubjects = stud.favSubjects;
    }
-   listsSubjects(){console.log([this.listsSubjects])};
-   PRAssignment(subject){`${student.name} has submitted a PR for ${subject}` }
-   sprintChallenge(subject){`${student.name} has begun sprint challenge on ${subject}`}
+   listsSubjects( ){
+     for (let i = 0; i < this.favSubjects.length; i ++){
+       console.log(this.favSubjects[i])
+     }};
+
+   PRAssignment(subject){console.log(`${student.name} has submitted a PR for ${subject}`) }
+   sprintChallenge(subject){console.log(`${student.name} has begun sprint challenge on ${subject}`)}
   }
 
   //Students class ends here
@@ -56,11 +61,43 @@ class Person {
       super (prom);
       this.gradClassName = prom.gradClassName;
       this.favInstructor = prom.favInstructor;
+      
     }
-    standUp(channel){console.log(`${name} announces to ${channel}, @channel standy times!`​​​​​);}
-    debugsCode(Student){`${name} debugs ${student.name}'s code on ${subject}`}
+    standUp(channel) {
+      console.log(`${this.name} announces to ${channel}, @channel standy times!`);
+    }
+    debugsCode(subject){`${name} debugs ${student.name}'s code on ${subject}`}
+    
+    
 
   }
 
 //PMs class ends here
 //--------------------------------------------
+const tai = new ProjectManagers({
+  name: "Tai",
+  age: 25,
+  gender: 'm',
+  location: "Virginia",
+  specialty: "React",
+  favLanguage: "JavaScript",
+  catchPhrase: "See ya'll tomorrow, have a good day!",
+  gradClassName: "CS11",
+  favInstructor: "Josh"
+});
+
+console.log(tai);
+tai.standUp(1);
+
+const figg = new Student({
+  name: 'Figgy',
+  location: 'Charleston',
+  age: 37,
+  gender: 'male',
+  previousBackground: 'Fork Lift Operator',
+  className: 'CS11',
+  favSubjects: ['Html', 'CSS', 'JavaScript']
+});
+console.log(figg);
+console.log(figg.favSubjects);
+figg.listsSubjects();
