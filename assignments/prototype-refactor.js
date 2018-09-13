1cd8d53746e467d209c060ac9fdb4d3b77e9b99f
Katy Hollobaugh <khollobaugh@gmail.com>
2018-09-13 15:15:18 -0400

    edits to match expected output

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -26,7 +26,7 @@ class GameObject {
   this.dimensions = options.dimensions;
   }
 destroy() {
-  return `Object was removed from the game.`;
+  return `${this.name} was removed from the game.`;
   }
 }
 
