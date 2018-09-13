9596988afd719d1012526334cbe1bdd6f64430ec
Adam <ajmboosh@gmail.com>
2018-09-13 15:36:59 -0500

    created 2 of each child constructor type, wrote console.log tests to make sure all are workign as intended

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -65,3 +65,71 @@ class ProjectManager extends Instructor {
     console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
   }
 }
+
+const josh = new Instructor({
+  name: "Josh",
+  location: "Utah",
+  age: 29,
+  gender: "male",
+  favLanguage: "JavaScript",
+  speciality: "dropping knowledge bombs",
+  catchPhrase: "Let's look in the guts of our Children, guys"
+});
+
+const bizarroJosh = new Instructor({
+  name: "Bizarro Josh",
+  location: "Australia",
+  age: 92,
+  gender: "male",
+  favLanguage: "colonial French",
+  speciality: "being totally different from regular Josh. Also whittling",
+  catchPhrase: "man do I hate coding, guys"
+});
+
+const adam = new Student({
+  name: "Adam",
+  location: "Round Rock",
+  age: 29,
+  gender: "male",
+  previousBackground: "retail management",
+  className: "FSW14",
+  favSubjects: ["JavaScript", "Html", "CSS"]
+});
+
+const feratu = new Student({
+  name: "Coach Feratu",
+  location: "inside your house",
+  age: 327,
+  gender: "male",
+  previousBackground: "vampire management",
+  className: "FSW14",
+  favSubjects: ["RubyRedBlood", "CoffinScript", "SharpPerlyWhites"]
+});
+
+const jeff = new ProjectManager({
+  name: "Jeff",
+  location: "California(I think)",
+  age: 29,
+  gender: "male",
+  gradClassName: "FSW14",
+  favInstructor: "Josh, probably"
+});
+
+const kamry = new ProjectManager({
+  name: "Kamry",
+  location: "somewhere that uses Mountain Time Zone",
+  age: 29,
+  gender: "male",
+  gradClassName: "FSW14",
+  favInstructor: "Josh, probably"
+});
+
+//tests
+console.log(josh.speciality); //dropping knowledge bombs
+console.log(josh.catchPhrase); //Let's look in the guts of our Children, guys
+josh.speak(); //Hello my name is Josh, I am from Utah
+bizarroJosh.demo("preprocessors"); //Today we are learning about preprocessors
+console.log(adam.favSubjects); // ['JavaScript', 'Html, 'CSS']
+feratu.sprintChallenge("Bloodsucking"); // Coach Feratu has begun sprint challenge on Bloodsucking
+jeff.standUp("fsw14_jeff"); //Jeff announces to fsw14_jeff, @channel standy times!
+kamry.debugsCode(adam, "constructors"); //Kamry debugs Adam's code in constructors
