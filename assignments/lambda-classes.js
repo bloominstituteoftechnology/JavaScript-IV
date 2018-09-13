0f411ebd56dbc123b8b47f3d8bb7c009bccc3a9a
Mindysueasaurus <mymindyyeager@gmail.com>
2018-09-13 14:58:50 -0500

    added student class and PM class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -34,8 +34,35 @@ class Instructor extends Person{
 class Student extends Person {
     constructor(studOptions) {
         super(studOptions);
-        
+        this.previousBackground = studOptions.previousBackground;
+        this.className = studOptions.className;
+        this.favSubjects = studOptions.favSubjects;
     }
-    
+    //methods
+    listSubjects() {
+        //logs out all of the students fav subjects 1 by 1
+    }
+    PRAssignments(student, subject) {
+        console.log(`${student.name} has submitted a PR for ${subject}`)
+    }
+    sprintChallenge(subject) {
+        console.log(`${student.name} has begun sprint challenge on ${subject}` );
+    }
 }
 
+//project manager class
+
+class ProjectManager extends Instructor {
+    constructor(pMOptions) {
+        super();
+        this.gradClassName = pMOptions.gradClassName;
+        this.favInstructor = pMOptions.favInstructor;
+    }
+    //methods
+    standUp(channel) {
+        console.log(`${this.name} announces to ${channel}, @channel standy times!`);
+    }
+    debugsCode(student, subject) {
+        console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
+    }
+}
