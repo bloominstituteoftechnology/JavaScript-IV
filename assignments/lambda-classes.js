b7d12288a27a9b38ec83dce50874ece9a01441c6
bummings <sup@fauxkno.com>
2018-09-13 17:18:39 -0400

    working thru lamaba-classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,28 @@
-// CODE here for your Lambda Classes
+//base class
+class Person {
+  constructor(props) {
+    this.name = props.name;
+    this.age = props.age;
+    this.location = props.location;
+    this.gender = props.gender;
+  }
+  
+  speak() {
+    console.log(`Hello, my name is ${this.name}, I am from ${this.location}.`);
+  }
+}
+
+class Instructor extends Person {
+  constructor(instructorProps) {
+    super(instructorProps);
+    this.speciality = instructorProps.speciality;
+    this.favLanguage = instructorProps.favLanguage;
+    this.catchPhrase = instructorProps.catchPhrase;
+  }
+  demo(subject) {
+    console.log(`Today we are learning about ${subject}.`);
+  }
+  grade({student}, subject){
+    console.log(`${student.name} receives a perfect score on ${subject}!`);
+  }
+}
