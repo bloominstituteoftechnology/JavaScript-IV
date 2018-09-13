2b44e94b0dfa203b35b5c532c663effcfd01735d
Corey Gumbs <cgumbs@Coreys-MBP-3.fios-router.home>
2018-09-13 15:39:12 -0400

    [chore] completed instructor class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -14,12 +14,35 @@ class Person{
         return `Hello my name is ${this.name}, I am from ${this.location}.`;
     }
 }
+//end Class Person
 
-const testPerson = new Person({
+//Class Instructor
+class Instructor extends Person{
+    //constructor
+    constructor(instructorAttributes){
+        super(instructorAttributes);
+        this.specialty =  instructorAttributes.specialty;
+        this.favLanguage = instructorAttributes.favLanguage;
+        this.catchPhrase = instructorAttributes.catchPhrase;
+    }
+
+    demo(subject){
+        console.log(`Today we are learning about ${subject}.`);
+    }
+
+    grade(student, subject){
+        console.log(`${student.name} receives a perfect score on ${subject}`);
+    }
+}
+
+const testPerson = new Instructor({
     "name":"Corey",
     "age": 41,
     "gender": 'M',
-    "location" : "New York City"
+    "location" : "New York City",
+    "specialty" : "Django",
+    "favLanguage": "Python",
+    "catchPhrase": "Chile Please!"
 })
 
 console.log(testPerson.name);
@@ -27,3 +50,8 @@ console.log(testPerson.age);
 console.log(testPerson.location);
 console.log(testPerson.gender);
 console.log(testPerson.speak());
+console.log(testPerson.specialty);
+console.log(testPerson.favLanguage);
+console.log(testPerson.catchPhrase);
+//console.log(testPerson.demo("classes"));
+//console.log(testPerson.grade('Corey', 'classes'));
