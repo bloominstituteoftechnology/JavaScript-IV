a4d4df7669f93528bac5d24b41b26709c3a235e5
Katie Cleary <katiecleary@airmaster.lan>
2018-09-13 15:57:00 -0400

    test objects created

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -68,3 +68,114 @@ debugs(student,subject) {
   }
   
 };
+
+/////////// TEST OBJECTS - INSTRUCTOR /////////////
+const lucy= new Instructor({
+//Person props
+  name: 'Lucy',
+  location: 'New York',
+  age: 30,
+  gender: 'female',
+//instructor props
+  favLanguage: 'JavaScript',
+  specialty: 'Front-end',
+  catchPhrase: ' but Ricky!'
+});
+
+const ethel= new Instructor({
+  name: 'Ethel',
+  location: 'New York',
+  age: 37,
+  gender: 'female',
+  favLanguage: 'Python',
+  specialty: 'Back-end',
+  catchPhrase: 'I\'m going to regret this'
+});
+
+const ricky= new Instructor({
+  name: 'Ricky',
+  location: 'New York',
+  age: 35,
+  gender: 'male',
+  favLanguage: 'Ruby',
+  specialty: 'Front-end',
+  catchPhrase: 'Lucy - what did you do??'
+});
+
+///--------- TEST OBJECTS - STUDENT---------/////
+const ross = new Student ({
+    //Person props
+      name: 'Ross',
+      location: 'NY',
+      age: 37,
+      gender: 'male',
+    //student props
+      previousBackground: 'dinosaurs',
+      className: 'FullStack',
+      favSubject: ['dinoScript', 'CSS', 'HTML']
+    });
+    
+    const pheobe = new Student({
+      name: 'Pheobe',
+      location: 'NY',
+      age: 33,
+      gender: 'female',
+      previousBackground: 'massage',
+      className: 'Front-end',
+      favSubject: ['trippyScript', 'Java', 'JS']
+    });
+    
+    const monica = new Student({
+      name: 'Monica',
+      location: 'NY',
+      age: 30,
+      gender: 'female',
+      previousBackground: 'chef',
+      className: 'back-end',
+      favSubject: ['cleanScripting', 'ruby', 'Python']
+    });
+    
+    ///||||||| TEST OBJECTS - PM ||||||||||///
+    const sheridan = new ProjectManager ({
+    //Person props
+      name: 'Sheridan',
+      location: 'B5',
+      age: 37,
+      gender: 'male',
+    //instructor props
+      favLanguage: 'JavaScript',
+      specialty: 'Front-end',
+      catchPhrase: 'lets go win',
+    //PM props
+      gradClassName: 'CSB5',
+      favInstructor: 'Lucy'
+    });
+    
+    const delenn= new ProjectManager({
+    //Person props
+      name: 'Delenn',
+      location: 'B5',
+      age: 37,
+      gender: 'female',
+      favLanguage: 'Python',
+      specialty: 'Front-end',
+      catchPhrase: 'never goodbye',
+      gradClassName: 'CSB55',
+      favInstructor: 'Ricky'
+    });
+    
+    const susan = new ProjectManager({
+    //Person props
+      name: 'Susan',
+      location: 'Bedrock',
+      age: 32,
+      gender: 'female',
+    //instructor props
+      favLanguage: 'C',
+      specialty: 'back-end',
+      catchPhrase: 'Susan is always right',
+    //PM props
+      gradClassName: 'CSB555',
+      favInstructor: 'Ethel'
+      
+    });
