a8eb96b5f331026be0d6435a7e3300b39e34833a
Wanda <doubleuevans@gmail.com>
2018-09-13 14:41:01 -0500

    'completed' classes with errors

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -24,8 +24,8 @@ class Instructor extends Person{
   demo() {
     return `Today we are learning about ${this.specialty}.`
   }
-  grade() {
-    return `${this.name} receives a perfect score on ${this.specialty}`
+  grade(subject) {
+    return `${this.name} receives a perfect score on ${subject}`
   }
 }
 
@@ -44,11 +44,11 @@ class Student extends Person{
   }
 
   PRAssignment() {
-    return `${this.name} has submitted a PR for ${SUBJECT}`
+    return `${this.name} has submitted a PR for SUBJECT`
   }
 
   sprintChallenge() {
-    return `${this.name} has begun sprint challenge on ${SUBJECT}`
+    return `${this.name} has begun sprint challenge on ${this.favLanguage}`
   }
 }
 
@@ -61,11 +61,133 @@ class ProjectManager extends Instructor{
   }
 
   standUp(){
-    return `${this.name} debugs ${student.name}'s code on ${SUBJECT}`
+    return `${this.name} debugs ${Student.name}'s code on SUBJECT`
   }
 
-  debugsCode() {
-    return `${this.name} debugs ${student.name}'s code on ${SUBJECT}`
+  debugsCode(student) {
+    return `${this.name} debugs ${student.name}'s code on SUBJECT`
   }
 }
 
+//TEST OBJECTS
+
+//instructor
+const fred = new Instructor({
+  name: 'Fred',
+  location: 'Bedrock',
+  age: 37,
+  gender: 'male',
+  favLanguage: 'JavaScript',
+  specialty: 'Front-end',
+  catchPhrase: `Don't forget the homies`
+});
+
+const dan = new Instructor({
+  name: 'Dan',
+  location: 'Salt Lake City',
+  age: 29,
+  gender: 'male',
+  favLanguage: 'Python',
+  specialty: 'Data Science',
+  catchPhrase: `To infinity and beyond!` 
+});
+
+const ellen = new Instructor({
+  name: 'Ellen',
+  location: 'Indianapolis',
+  age: 31,
+  gender: 'female',
+  favLanguage: 'Java',
+  specialty: 'Databases',
+  catchPhrase: `Four score and seven years ago` 
+});
+
+console.log(fred.speak())
+console.log(dan.demo())
+console.log(ellen.grade('Javascript-I'))
+
+// //PROJECT MANAGER
+const john = new ProjectManager({
+  name: 'John',
+  location: 'St. Louis',
+  age: 42,
+  gender: 'male',
+  favLanguage: 'Python',
+  specialty: 'Back-end',
+  catchPhrase: `Here goes nothing.`,
+  gradClassName: 'FSW-12',
+  favInstructor: 'Fred' 
+})
+
+const melissa = new ProjectManager({
+  name: 'Melissa',
+  location: 'New Orleans',
+  age: 35,
+  gender: 'female',
+  favLanguage: 'C++',
+  specialty: 'Operating Systems',
+  catchPhrase: `Be there or be square!`,
+  gradClassName: 'FSW-13',
+  favInstructor: 'Dan'  
+})
+
+const robert = new ProjectManager({
+  name: 'Robert',
+  location: 'Miami',
+  age: 20,
+  gender: 'male',
+  favLanguage: 'C#',
+  specialty: 'Gaming',
+  catchPhrase: `To the moon, Alice!`,
+  gradClassName: 'FSW-11',
+  favInstructor: 'Ellen'  
+})
+
+console.log(john.standUp())
+console.log(melissa.debugsCode(john))
+console.log(robert.age)
+
+// //STUDENT Objects
+const wanda = new Student({
+  name: 'Wanda',
+  location: 'St. Louis',
+  age: 35,
+  gender: 'female',
+  favLanguage: 'Python',
+  specialty: 'Back-end',
+  catchPhrase: `From the Earth to the Moon`,
+  previousBackground: 'Video Production',
+  className: 'FSW-14',
+  favSubjects: ['English Literature', 'Web Development'] 
+});
+
+const adam = new Student({
+  name: 'Adam',
+  location: 'Boise',
+  age: 38,
+  gender: 'male',
+  favLanguage: 'Bootstrap',
+  specialty: 'Front-end',
+  catchPhrase: `It's me!`,
+  previousBackground: 'Insurance',
+  className: 'FSW-14',
+  favSubjects: ['Kids', 'Programming', 'Insurance'] 
+});
+
+const sean = new Student({
+  name: 'Sean',
+  location: 'Los Angeles',
+  age: 40,
+  gender: 'male',
+  favLanguage: 'React',
+  specialty: 'Web Apps',
+  catchPhrase: `I build web apps?`,
+  previousBackground: 'Waiter',
+  className: 'iOS-1',
+  favSubjects: ['Apple', 'Acting'] 
+});
+
+console.log(wanda.listsSubjects())
+console.log(wanda.PRAssignment())
+console.log(adam.sprintChallenge())
+console.log(sean.favSubjects)
