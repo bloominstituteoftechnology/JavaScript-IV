3c71bb6b2de52d09781bde9aad3b10c38a69e653
Aman <amsingh714@gmail.com>
2018-10-18 15:23:49 -0400

    finished classes for students, PMs

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -30,5 +30,35 @@ class Instructor extends People {
 }
 
 class Student extends People {
-    
+    constructor(attributes) {
+        super(attributes);
+        this.previousBackground = attributes.previousBackground;
+        this.className = attributes.className;
+        this.favSubjects = attributes.favSubjects;
+    }
+    listsSubjects() {
+        return `${this.favSubjects}`; // may have to come back to this
+    }
+    PRAssignment(subject) {
+        return `${this.name} has submitted a PR for ${this.subject}`;
+    }
+    sprintChallenge() {
+        return `${this.name} has begun sprint challenge on ${this.subject}`;
+    }
 }
+
+class ProjectManagers extends Instructor {
+    constructor(attributes) {
+        super(attributes);
+        this.gradClassName = attributes.gradClassName;
+        this.favInstructor = attributes.favInstructor;
+    }
+    standUp(channel) {
+        return `${this.name} announces to ${this.channel}, @channel standy times!​​​​​`;
+    }
+    debugsCode(student, subject) {
+        return `${this.name} debugs ${student.name}'s code on ${this.subject}`;
+    }
+}
+
+
