60830da7888fdde206c1634b75bc8ca0e194839f
dan2014 <dmen326@gmail.com>
2018-10-18 14:17:38 -0400

    added the class Instructor to lambda-classes.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -10,3 +10,46 @@ class Person{
         return `Hello my name is ${this.name}, I am from ${this.location}`;
     }
 }
+
+
+class Instructor extends Person{
+    constructor(instructorAttr){
+        super(instructorAttr);
+        this.specialty = instructorAttr.specialty;
+        this.favLanguage = instructorAttr.favLanguage;
+        this.catchPhrase = instructorAttr.catchPhrase;
+    }
+    demo(subject) {
+        return `Today we are learning about ${subject}`;
+    }
+    grade(student,subject) {
+        return `${student.name} receives a perfect score on ${subject}`;
+    }
+}
+
+class Student extends Person{
+    constructor(studentAttr){
+        super(studentAttr);
+        this.name = studentAttr.name;
+        this.age = studentAttr.age;
+        this.location = studentAttr.location;
+        this.gender = studentAttr.gender;
+    }
+    speak() {
+        return `Hello my name is ${this.name}, I am from ${this.location}`;
+    }
+}
+
+class ProjectManager extends Person{
+    
+    constructor(pmAttr){
+        super(pmAttr);
+        this.name = pmAttr.name;
+        this.age = pmAttr.age;
+        this.location = pmAttr.location;
+        this.gender = pmAttr.gender;
+    }
+    speak() {
+        return `Hello my name is ${this.name}, I am from ${this.location}`;
+    }
+}
