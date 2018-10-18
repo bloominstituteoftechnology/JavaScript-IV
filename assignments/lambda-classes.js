c5d265247d94dbf513221ee63b70d8c8542e8a6f
Yasirah Krueng <yasirah.krueng@gmail.com>
2018-10-18 15:01:39 -0500

    add instructor extends person class to lambda-classes.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -17,3 +17,27 @@ class Person {
     return `Hello my name is ${this.name}, I am from ${this.location}`;
   }
 }
+
+/*
+  === Instructor ===
+  Inherit from Person
+  * specialty
+  * favLanguage
+  * catchPhrase
+  * demo(subject) // returns the phrase: 'Today we are learning about <subject>'
+  * grade(student, subject) // returns the phrase: '<student.name> receives a perfect score on <subject>'
+*/
+class Instructor extends Person {
+  constructor(attr) {
+    super(attr);
+    this.specialty = attr.specialty;
+    this.favLanguage = attr.favLanguage;
+    this.catchPhrase = attr.catchPhrase;
+  }
+  demo(subject) {
+    return `Today we are learning about ${subject}`;
+  }
+  grade(student, subject) {
+    return `${student.name} receives a perfect score on ${subject}`;
+  }
+}
