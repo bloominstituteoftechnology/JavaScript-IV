e6aed0c78ca364f896686965ffe07c2160573e95
Trai Lynne <trailynne@outlook.com>
2018-10-18 13:50:43 -0600

    Classes: wrote tests for Person class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -8,3 +8,27 @@
 
     * speak() // prototype method -> returns a log of `Hello my name is ${this.name}, I am from ${this.location}`
 */
+
+
+
+// Tests
+const adam = new Person({
+    name: 'adam',
+    age: 28,
+    location: 'North America',
+    gender: 'm'
+});
+
+const eve = new Person({
+    name: 'eve',
+    age: 25,
+    location: 'Africa',
+    gender: 'f'
+});
+
+
+console.log(johnny);
+console.log(johnny.speak());
+
+console.log(janny);
+console.log(janny.speak());
