41f9282879bb9d9e1bdb54c0a8281f0609a56a88
J A <jarata@gmail.com>
2018-10-18 11:06:13 -0700

    clean up on prototype refractor and layout for lambda classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,57 @@
 // CODE here for your Lambda Classes
+
+class Person {
+  constructor(attributes) {
+    this.name = attributes.name;
+    this.age = attributes.age;
+    this.location = attributes.location;
+    this.gender = attributes.gender;
+  }
+  speak () {
+    return `Hello my name is ${this.name}, I am from ${this.location}.`
+  }
+}
+
+class Instructors extends Person {
+  constructor(attributes) {
+    super(attributes);
+    this.specialty = attributes.specialty;
+    this.favLanguage = attributes.favLanguage;
+    this.catchPhrase = attributes.catchPhrase;
+  }
+  demo() {
+    return ``
+  }
+}
+
+class Student extends Instructors {
+  constructor(attributes) {
+    super(attributes);
+    this.previousBackground = attributes.previousBackground;
+    this.className = attributes.className;
+    this.favSubjects = attributes.favSubjects;
+  }
+  listsSubjects() {
+    return `${favSubjects}`;
+  }
+  PRAssignment() {
+
+  }
+  sprintChallenge() {
+
+  }
+}
+
+class ProjectManager extends Student {
+  constructor(attributes) {
+    super(attributes);
+    this.gradClassName = attributes.gradClassName;
+    this.favInstructor = attributes.favInstructor;
+  }
+  standUp () {
+    console.log(`${this.name} announces to ${channel}, @channel standup times!`);
+  }
+  debugsCode () {
+    console.log(`${this.name} debugs ${student.name}'s code on ${subject}.`)
+  }
+}
