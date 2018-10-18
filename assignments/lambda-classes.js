ccf13f6348b89fa03e403e34d18975bebefaef00
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2018-10-18 14:57:24 -0400

    Added Person and Instructorto project

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,28 @@
 // CODE here for your Lambda Classes
+
+class Person {
+    constructor(attributes) {
+        this.name = attributes.name;
+        this.age = attributes.age;
+        this.location = attributes.location;
+        this.gender = attributes.gender;
+    }
+    speak() {
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}.`);
+    }
+}
+
+class Instructor extends Person {
+    constructor(instruct) {
+        super(instruct);
+        this.specialty = instruct.specialty;
+        this.favLanguage = instruct.favLanguage;
+        this.catchPhrase = instruct.catchPhrase;
+    }
+    demo(subject) {
+        console.log(`Today we are learning ${this.subject}`);
+    }
+    grade(student, subject) {
+        console.log(`${student.name} receives a perfect score on ${subject}.`);
+    }
+}
