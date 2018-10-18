8d7136896e44cced1cce056267aa92960009b276
Trai Lynne <trailynne@outlook.com>
2018-10-18 13:53:50 -0600

    Classes: wrote Person as ES6 class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -9,26 +9,42 @@
     * speak() // prototype method -> returns a log of `Hello my name is ${this.name}, I am from ${this.location}`
 */
 
+class Person {
+    // Class Initializer and Attributes
+    constructor(props) {
+        this.name = props.name;
+        this.age = props.age;
+        this.location = props.location;
+        this.gender = props.gender;
+    }
+
+    // Class Methods
+    speak() {
+        return `Hello my name is ${this.name}, I am from ${this.location}`;
+    }
+}
+
+
 
 
 // Tests
 const adam = new Person({
-    name: 'adam',
+    name: 'Adam',
     age: 28,
     location: 'North America',
     gender: 'm'
 });
 
 const eve = new Person({
-    name: 'eve',
+    name: 'Eve',
     age: 25,
     location: 'Africa',
     gender: 'f'
 });
 
 
-console.log(johnny);
-console.log(johnny.speak());
+console.log(adam);
+console.log(adam.speak());
 
-console.log(janny);
-console.log(janny.speak());
+console.log(eve);
+console.log(eve.speak());
