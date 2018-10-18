d06b99135eebf50f2be921b89478f7ac28f17186
chasefulks <chase.fulks@gmail.com>
2018-10-18 16:04:47 -0500

    Classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -34,10 +34,10 @@ class Student extends Person{
         this.favSubjects = stud.favSubjects;
     }
     listsSubjects(){
-        console.log();
+        this.favSubjects.forEach(item => console.log(item));
     }
     PRAssignment(){
-        console.log();
+        console.log(`${this.name} has submitted a PR for ${this.subject}`);
     }
     sprintChallenge(){
     console.log();
@@ -56,3 +56,53 @@ class ProjectManagers extends Instructor{
         console.log(`${this.name} debugs ${this.student.name}'s code on ${this.subject}`);
     }
 }
+
+const student = new Student({
+    name: 'Chase',
+    age: '20',
+    gender: 'M',
+    location: 'Texas',
+    previousBackground: 'C++ in college',
+    className: 'FSW15',
+    favSubjects: ['HTML', 'CSS'],
+});
+
+const instructor = new Instructor({
+    name: 'josh',
+    age: '30',
+    gender: 'M',
+    favLanguage: 'JavaScript',
+    specialty: 'I-know-it-all',
+    catchPhrase: 'LEARN-IT',
+});
+
+const projectmanager = new ProjectManagers({
+    name: 'kamal',
+    age: '25',
+    gender: 'M',
+    favLanguage: 'Java',
+    specialty: 'Helping-the-clueless',
+    catchPhrase: 'Make-more-meaningful-commits',
+    gradClassName: 'CS1',
+    favInstructor: 'josh',
+});
+
+
+console.log(student);
+
+console.log(instructor);
+
+console.log(pm);
+
+instructor.speak();
+instructor.demo('JS');
+instructor.grade(student, 'JS');
+
+student.speak();
+student.listSubject();
+student.prAssignment('C++');
+student.sprintChallenge('JSVI');
+
+pm.speak();
+pm.standUp('FSW15');
+pm.debugsCode(student, 'CSS');
