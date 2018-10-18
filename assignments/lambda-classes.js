21e0a9c7bce63d6132565512925583f2b3646657
jamespagedev <jamespagedev@outlook.com>
2018-10-18 14:08:07 -0700

    mvp complete

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,148 @@
 // CODE here for your Lambda Classes
+/*****************************************************************
+ *************************** Class(es) ***************************
+ *****************************************************************/
+//================================================================
+//============================ Person ============================
+class Person {
+  // *********************** Constructor *************************
+  constructor(obj){
+    this.name = obj.name;
+    this.age = obj.age;
+    this.location = obj.location;
+    this.gender = obj.gender;
+  }
+  // ************************ Method(s) **************************
+  speak(){
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
+  }
+}
+
+//================================================================
+//========================== Instructor ==========================
+class Instructor extends Person {
+  // *********************** Constructor *************************
+  constructor(obj){
+    super(obj);
+    this.specialty = obj.specialty;
+    this.favLanguage = obj.favLanguage;
+    this.catchPhrase = obj.catchPhrase;
+  }
+  // ************************ Method(s) **************************
+  demo(subject){
+    return `Today we are learning about ${subject}`;
+  }
+  grade(student, subject){
+    return `${student.name} receives a perfect score on ${subject}`;
+  }
+}
+
+//================================================================
+//============================ Student ===========================
+class Student extends Person {
+  // *********************** Constructor *************************
+  constructor(obj){
+    super(obj);
+    this.previousBackground = obj.previousBackground;
+    this.className = obj.className;
+    this.favSubjects = obj.favSubjects;
+  }
+  // ************************ Method(s) **************************
+  listsSubjects(){
+    console.log(`${this.name}\'s favorite subjects are:\n`);
+    this.favSubjects.forEach(function(element) {
+      console.log(element)
+    });
+  }
+  PRAssignment(subject){
+    return `${this.name} has submitted a PR for ${subject}`;
+  }
+  sprintChallenge(subject){
+    return `${this.name} has begun sprint challenge on ${subject}`;
+  }
+}
+
+//================================================================
+//======================== ProjectManager ========================
+class ProjectManager extends Instructor {
+  // *********************** Constructor *************************
+  constructor(obj){
+    super(obj);
+    this.gradClassName = obj.gradClassName;
+    this.favInstructor = obj.favInstructor;
+  }
+  // ************************ Method(s) **************************
+  standUp(channel){
+    return `${this.name} announces to ${channel} @channel standy times!​​​​​`
+  }
+  debugsCode(student, subject){
+    return `${this.name} debugs ${student.name}\'s code on ${subject}`;
+  }
+}
+
+/*****************************************************************
+ **************************** Objects ****************************
+ *****************************************************************/
+//================================================================
+//=========================== Example ============================
+const fred = new Instructor({
+  name: 'Fred',
+  location: 'Bedrock',
+  age: 37,
+  gender: 'male',
+  favLanguage: 'JavaScript',
+  specialty: 'Front-end',
+  catchPhrase: `Don't forget the homies`
+});
+
+//================================================================
+//=========================== Instructor ============================
+const josh = new Instructor({
+  name: 'Josh',
+  location: 'In Da House',
+  age: 42,
+  gender: 'male',
+  favLanguage: 'JavaScript',
+  specialty: 'Back-end',
+  catchPhrase: `Study hard my friends`
+});
+
+//================================================================
+//=========================== Student ============================
+const james = new Student({
+  name: 'James',
+  location: 'Arizona',
+  age: 35,
+  gender: 'male',
+  previousBackground: 'Lots of things',
+  className: 'FSW15',
+  favSubjects: ['Algorithms', 'Data Structures', 'Automation', 'Design Patterns', `Time Complexity Notation`,
+                'HTML', 'CSS', 'JavaScript', 'Java', 'Python', 'C++', 'Gui Implementation', 'Architecture', 'TDD']
+})
+
+//================================================================
+//======================= Project Manager ========================
+const sawyer = new ProjectManager({
+  name: 'Sawyer',
+  location: 'Earth',
+  age: 31,
+  gender: 'male',
+  gradClassName: 'FSW15',
+  favInstructor: josh.name
+})
+
+/*****************************************************************
+ ***************************** Main ******************************
+ *****************************************************************/
+console.log('*************************** lambda-classes.js ***************************');
+// Class Method's Use Cases
+james.listsSubjects();
+console.log(josh.speak());
+console.log(sawyer.speak());
+console.log(james.speak());
+console.log(`${josh.name} says: ${josh.demo(josh.favLanguage)}`);
+console.log(james.sprintChallenge(josh.favLanguage));
+console.log(james.PRAssignment(josh.favLanguage));
+console.log(`${josh.name} says: ${josh.grade(james, josh.favLanguage)}`);
+console.log(`${sawyer.standUp('FSW15_sawyer')}`);
+console.log(`${sawyer.debugsCode(james, james.favSubjects[7])}`)
--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -7,3 +7,239 @@ Prototype Refactor
 2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
 
 */
+/*****************************************************************
+ *************************** Class(es) ***************************
+ *****************************************************************/
+//================================================================
+//========================== GameObject ==========================
+class GameObject{
+  // *********************** Constructor *************************
+  constructor(obj){
+    this.createdAt = obj.createdAt;
+    this.dimensions = obj.dimensions;
+  }
+  // ************************ Method(s) **************************
+  destroy(){
+    return `${this.name} was removed from the game.`;
+  }
+}
+
+//================================================================
+//======================== CharacterStats ========================
+class CharacterStats extends GameObject{
+  // *********************** Constructor *************************
+  constructor(obj){
+  super(obj);
+  this.hp = obj.hp;
+  this.name = obj.name;
+  }
+  // ************************ Method(s) **************************
+  takeDamage (){
+    return `${this.name} took damage`;
+  }
+}
+
+//================================================================
+//=========================== Humanoid ===========================
+class Humanoid extends CharacterStats{
+  // *********************** Constructor *************************
+  constructor(obj){
+  super(obj);
+  this.faction = obj.faction;
+  this.weapons = obj.weapons;
+  this.language = obj.language;
+  }
+  // ************************ Method(s) **************************
+  greet(){
+    return `${this.name} offers a greeting in ${this.language}`;
+  }
+}
+
+//================================================================
+//============================= Hero =============================
+class Hero extends Humanoid{
+  // *********************** Constructor *************************
+  constructor(obj){
+    super(obj);
+    this.attack = obj.attack;
+    this.throw = obj.throw;
+  }
+}
+
+//================================================================
+//=========================== Villain ============================
+class Villain extends Humanoid{
+  // *********************** Constructor *************************
+  constructor(obj){
+    super(obj);
+    this.cast = obj.cast;
+  }
+}
+
+/*****************************************************************
+ **************************** Objects ****************************
+ *****************************************************************/
+  const mage = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+      length: 2,
+      width: 1,
+      height: 1,
+    },
+    hp: 5,
+    name: 'Bruce',
+    faction: 'Mage Guild',
+    weapons: [
+      'Staff of Shamalama',
+    ],
+    language: 'Common Toungue',
+  });
+
+  const swordsman = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+      length: 2,
+      width: 2,
+      height: 2,
+    },
+    hp: 15,
+    name: 'Sir Mustachio',
+    faction: 'The Round Table',
+    weapons: [
+      'Giant Sword',
+      'Shield',
+    ],
+    language: 'Common Toungue',
+  });
+
+  const archer = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+      length: 1,
+      width: 2,
+      height: 4,
+    },
+    hp: 10,
+    name: 'Lilith',
+    faction: 'Forest Kingdom',
+    weapons: [
+      'Bow',
+      'Dagger',
+    ],
+    language: 'Elvish',
+  });
+
+  const hero = new Hero({
+    createdAt: new Date(),
+    dimensions: {
+      length: 2,
+      width: 2,
+      height: 2,
+    },
+    hp: 9999,
+    name: 'Champion',
+    faction: 'Knight',
+    weapons: [
+      '2H Sword',
+      'Dart'
+    ],
+    language: 'Human',
+    attack: function(monster, dmg){
+      monster.hp -= dmg;
+      if (monster.hp <= 0){
+        return monster.destroy();
+      }
+      return `${this.name} attacks ${monster.name} with a ${this.weapons[0]} and does ${dmg} dmg.`;
+    },
+    throw: function(monster, dmg){
+      monster.hp -= dmg;
+      if (monster.hp <= 0){
+        return monster.destroy();
+      }
+      return `${this.name} throws a ${this.weapons[1]} at ${monster.name} and does ${dmg} dmg.`;
+    }
+  });
+
+  const villain = new Villain({
+    createdAt: new Date(),
+    dimensions: {
+      length: 3,
+      width: 3,
+      height: 3,
+    },
+    hp: 200000,
+    name: 'Dark Lord',
+    faction: 'Boss',
+    weapons: [
+      'Wand'
+    ],
+    language: 'All',
+    cast: function(character, spellName, dmg){
+      character.hp -= dmg;
+      if (character.hp <= 0){
+        return character.destroy();
+      }
+      return `${this.name} casts ${spellName} with the ${this.weapons[0]} on ${character.name} and does ${dmg} dmg.`;
+    }
+  });
+
+/*****************************************************************
+ ****************************** Main *****************************
+ *****************************************************************/
+console.log('************************* prototype-refactor.js *************************');
+console.log(mage.createdAt); // Today's date
+console.log(JSON.stringify(archer.dimensions)); // { length: 1, width: 2, height: 4 }
+console.log(swordsman.hp); // 15
+console.log(mage.name); // Bruce
+console.log(swordsman.faction); // The Round Table
+console.log(mage.weapons.toString()); // Staff of Shamalama
+console.log(archer.language); // Elvish
+console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+console.log(mage.takeDamage()); // Bruce took damage.
+console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+// Stretch task: 
+// * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
+// * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
+// * Create two new objects, one a Villain and one a hero and fight it out with methods!
+console.log(hero.attack(villain, 5000));
+console.log(hero.throw(villain, 9999));
+console.log(villain.cast(hero, 'Flare', 100));
+console.log(hero.attack(villain, 5000));
+console.log(hero.throw(villain, 9999));
+console.log(villain.cast(hero, 'Ultima', 150));
+console.log(hero.attack(villain, 5000));
+console.log(hero.throw(villain, 9999));
+console.log(villain.cast(hero, 'Super Nova', 200));
+console.log(hero.attack(villain, 5000));
+console.log(hero.throw(villain, 9999));
+console.log(villain.cast(hero, 'Flare', 100));
+console.log(hero.attack(villain, 5000));
+console.log(hero.throw(villain, 9999));
+console.log(villain.cast(hero, 'Ultima', 150));
+console.log(hero.attack(villain, 5000));
+console.log(hero.throw(villain, 9999));
+console.log(villain.cast(hero, 'Super Nova', 200));
+console.log(hero.attack(villain, 5000));
+console.log(hero.throw(villain, 9999));
+console.log(villain.cast(hero, 'Flare', 100));
+console.log(hero.attack(villain, 5000));
+console.log(hero.throw(villain, 9999));
+console.log(villain.cast(hero, 'Ultima', 150));
+console.log(hero.attack(villain, 5000));
+console.log(hero.throw(villain, 9999));
+console.log(villain.cast(hero, 'Super Nova', 200));
+console.log(hero.attack(villain, 5000));
+console.log(hero.throw(villain, 9999));
+console.log(villain.cast(hero, 'Flare', 100));
+console.log(hero.attack(villain, 5000));
+console.log(hero.throw(villain, 9999));
+console.log(villain.cast(hero, 'Ultima', 150));
+console.log(hero.attack(villain, 5000));
+console.log(hero.throw(villain, 9999));
+console.log(villain.cast(hero, 'Super Nova', 200));
+console.log(hero.attack(villain, 5000));
+console.log(hero.throw(villain, 9999));
+console.log(villain.cast(hero, 'Flare', 100));
+console.log(hero.attack(villain, 5000));
+console.log(hero.throw(villain, 9999));
+console.log(`${hero.name} HP Left: ${hero.hp}`);
