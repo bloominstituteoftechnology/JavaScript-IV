afb4101e7ecbc25107fc3f0a6ccea0432c2e68f0
Trai Lynne <trailynne@outlook.com>
2018-10-18 13:39:39 -0600

    Classes: Commented in requirements for Person Class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,10 @@
-// CODE here for your Lambda Classes
+/*
+    === Person ===
+    * name
+    * age
+    * location
+    * gender
+
+
+    * speak() // prototype method -> returns a log of `Hello my name is ${this.name}, I am from ${this.location}`
+*/
