d6b42771730c49d1f7273222fef86cd62adaf68a
Andrew Yurlov <yurlovandrew@gmail.com>
2018-10-25 15:40:41 -0700

    Project complete

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,110 @@
-// CODE here for your Lambda Classes
+class Person {
+  constructor(attributes) {
+    this.name = attributes.name;
+    this.location = attributes.location;
+    this.age = attributes.age;
+    this.gender = attributes.gender;
+  }
+  speak() {
+    return `Hello my name is ${this.name} and I am from ${this.location}.`;
+  }
+}
+
+class Instructor extends Person {
+  constructor(childAttributes) {
+    super(childAttributes);
+    this.favLanguage = childAttributes.favLanguage;
+    this.specialty = childAttributes.specialty;
+    this.catchPhrase = childAttributes.catchPhrase;
+  }
+  demo(subject) {
+    return `Today we are learning about ${subject}`;
+  }
+  grade(student, subject) {
+    return `${student} receives a perfect score on ${subject}`;
+  }
+}
+
+class Student extends Instructor {
+  constructor(grandChildAttributes) {
+    super(grandChildAttributes);
+    this.previousbackground = grandChildAttributes.previousbackground;
+    this.className = grandChildAttributes.className;
+    this.favSubjects = grandChildAttributes.favSubjects;
+  }
+  listsSubject() {
+    return `These are my favorite subjects in Lambda School: ${this.favSubjects}`;
+  }
+  PRAssignment() {
+    return `${this.name} has submitted a PR for ${this.subject}`;
+  }
+  sprintChallenge() {
+    return `${this.name} has begun spring challenge ${this.subject}`;
+  }
+}
+
+class ProjectManager extends Student {
+  constructor(greatGrandChildAttributes) {
+    super(greatGrandChildAttributes);
+    this.gradClassName = greatGrandChildAttributes.gradClassName;
+    this.favInstructor = greatGrandChildAttributes.favInstructor;
+  }
+  standUp(channel) {
+    return `${this.name} announces to ${this.channel}, @channel standy times!​​​​​`;
+  }
+  debugsCode() {
+    return `${this.name} debugs ${andrew.name} 's code on ${this.subject}`;
+  }
+}
+
+const fred = new Person({
+  name: 'Fred',
+  location: 'Bedrock',
+  age: 100,
+  gender: 'male',
+});
+
+
+const john = new Instructor({
+  name: 'Josh',
+  location: 'Utah',
+  age: 34,
+  gender: 'male',
+  favLanguage: "JavaScript, Python, Elm",
+  specialty: "Redux",
+  catchPhrase: "Don't forget the homies"
+});
+
+const andrew = new Student({
+  name: 'Em',
+  location: 'Seattle',
+  age: 65,
+  gender: 'female',
+  previousbackground: "healthcare",
+  className: "CS132",
+  favSubjects: ["HTML", "CSS", "Javascript"],
+});
+
+const kels = new ProjectManager({
+  name: 'Johnathan',
+  location: 'Unknown',
+  age: 100,
+  gender: 'male',
+  gradClassName: "CSI",
+  favInstructor: "Sean PDiddy Combs"
+});
+
+console.log(fred.name);
+console.log(fred.speak());
+console.log(fred.age);
+console.log(john);
+console.log(john.demo());
+console.log(andrew.listsSubject());
+console.log(andrew);
+console.log(kels.favInstructor);
+console.log(kels.debugsCode());
+console.log(andrew.PRAssignment());
+console.log(andrew.sprintChallenge('today'));
+console.log(kels.standUp());
+console.log(john.grade('LaLa', 'math'));
+console.log(john.grade('Rosco', 'music'));
--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -1,9 +1,85 @@
-/* 
+class GameObject {
+  constructor(options) {
+    this.createdAt = options.createdAt;
+    this.dimensions = options.dimensions;
+  }
+  destroy() {
+    return `${this.name} was removed from the game.`;
+  }
+}
 
-Prototype Refactor
+class CharacterStats extends GameObject {
+  constructor(characterStatsOptions) {
+    super(characterStatsOptions);
+    this.hp = characterStatsOptions.hp;
+    this.name = characterStatsOptions.name;
+  }
+  takeDamage() {
+    return `${this.name} took damage.`;
+  }
+}
 
-1. Copy and paste your code or the solution from yesterday
+class Humanoid extends CharacterStats {
+  constructor(humanoidOptions) {
+    super(humanoidOptions);
+    this.faction = humanoidOptions.faction;
+    this.weapons = humanoidOptions.weapons;
+    this.language = humanoidOptions.language;
+  }
+  greet() {
+    return `${this.name} offers a greeting in ${this.language}.`;
+  }
+}
 
-2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
+const mage = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 1,
+    height: 1
+  },
+  hp: 5,
+  name: 'Bruce',
+  faction: 'Mage Guild',
+  weapons: ['Staff of Shamalama'],
+  language: 'Common Toungue'
+});
 
-*/
+const swordsman = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 2,
+    height: 2
+  },
+  hp: 15,
+  name: 'Sir Mustachio',
+  faction: 'The Round Table',
+  weapons: ['Giant Sword', 'Shield'],
+  language: 'Common Toungue'
+});
+
+const archer = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 1,
+    width: 2,
+    height: 4
+  },
+  hp: 10,
+  name: 'Lilith',
+  faction: 'Forest Kingdom',
+  weapons: ['Bow', 'Dagger'],
+  language: 'Elvish'
+});
+
+console.log(mage.createdAt);
+console.log(archer.dimensions);
+console.log(swordsman.hp);
+console.log(mage.name);
+console.log(swordsman.faction);
+console.log(mage.weapons);
+console.log(archer.language);
+console.log(archer.greet());
+console.log(mage.takeDamage());
+console.log(swordsman.destroy());
