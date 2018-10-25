2b31b621663ca90c1bcbddbc015297f36049316a
Bill Geist <wcgeist@gmail.com>
2018-10-24 23:49:25 -0400

    added 2 objects for each

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -22,11 +22,11 @@ class Instructor extends Person {
         this.catchPhrase = quals.catchPhrase;
     }
 
-    demo (subject) {
+    demo(subject) {
         `Today we are learning about ${subject}`;
     }
 
-    grade (student, subject) {
+    grade(student, subject) {
         `${student.name} receives a perfect score on ${subject}`;
     }
 
@@ -44,23 +44,23 @@ class Student extends Person {
         console.log(this.favSubjects);
     }
 
-    PRAssignment (subject) {
+    PRAssignment(subject) {
         `${this.name} has submitted a PR for ${subject}`;
     }
 
-    sprintChallenge (subject) {
+    sprintChallenge(subject) {
         `${this.name} has begun sprint challenge on ${subject}`;
     }
 }
 
-class ProjectManagers extends Instructor {
+class ProjectManager extends Instructor {
     constructor(quals) {
         super(quals);
         this.gradClassName = quals.gradClassName;
         this.favInstructor = quals.favInstructor;
     }
 
-    standUp (channel) {
+    standUp(channel) {
         `${this.name} announces to ${channel}, @channel standy times!`;
     }
 
@@ -71,12 +71,63 @@ class ProjectManagers extends Instructor {
 }
 
 
-const fred = new Instructor({
-    name: 'Fred',
+const igor = new Instructor({
+    name: 'Igor',
     location: 'Bedrock',
     age: 37,
     gender: 'male',
     favLanguage: 'JavaScript',
     specialty: 'Front-end',
     catchPhrase: `Don't forget the homies`
-  });
+});
+
+const ilya = new Instructor({
+    name: 'Ilya',
+    location: 'Springfield',
+    age: 24,
+    gender: 'male',
+    favLanguage: 'Java',
+    specialty: 'Back-end',
+    catchPhrase: `wocka wocka wocka`
+});
+
+const sam = new Student({
+    name: 'Sam',
+    location: 'Connecticut',
+    age: 27,
+    gender: 'male',
+    previousBackground: 'power',
+    className: 'CSPT3',
+    favSubjects: ['CS101', 'Lunch', 'recess']
+});
+
+const susan = new Student({
+    name: 'Susan',
+    location: 'New York',
+    age: 25,
+    gender: 'female',
+    previousBackground: 'fashion',
+    className: 'CSFT3',
+    favSubjects: ['fashion', 'drama', 'gossip']
+});
+
+const peter = new ProjectManager({
+    name: 'Susan',
+    location: 'New York',
+    age: 25,
+    gender: 'male',
+    gradClassName: 'CSPT3',
+    favInstructor: 'Ilya'
+});
+
+const priya = new ProjectManager({
+    name: 'Priya',
+    location: 'Cali',
+    age: 25,
+    gender: 'male',
+    gradClassName: 'CSPT3',
+    favInstructor: 'Igor'
+});
+
+
+console.log(priya.speak());
