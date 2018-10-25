06d427c45bc58933639cfaffb0b95c7597ed7eb8
unknown <bj2336@email.vccs.edu>
2018-10-24 21:36:54 -0700

    Instructor Class Built

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,13 +1,30 @@
 // CODE here for your Lambda Classes
 class Person {
   constructor(props) {
-    this.name = attributes.name
-    this.age = attributes.age
-    this.location = attributes.location
-    this.gender= attributes.gender
+    this.name = props.name
+    this.age = props.age
+    this.location = props.location
+    this.gender= props.gender
   }
 
   speak() {
     console.log(`Hello my name is ${this.name}, and I am from ${this.location]}`)
+  }
+}
+
+class Intructor extends Person {
+  constructor(props) {
+    super(props)
+    this.specialty = props.specialty
+    this.favLanguage = props.favLanguage
+    this.catchPhrase = props.catchPhrase
+  }
+
+  demo(subject) {
+    console.log(`Today we are learning about ${subject}`)
+  }
+
+  grade(student, subject) {
+    console.log(`${student.name} recieves a perfect score on ${subject}`)
   }
 }
