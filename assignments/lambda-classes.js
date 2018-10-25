7de35a3e648cfbfc3d7f5c0db9f9d6d1d180256a
unknown <bj2336@email.vccs.edu>
2018-10-24 22:11:47 -0700

    Project Manager Class Built

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -8,11 +8,11 @@ class Person {
   }
 
   speak() {
-    console.log(`Hello my name is ${this.name}, and I am from ${this.location]}`)
+    console.log(`Hello my name is ${this.name}, and I am from ${this.location}`)
   }
 }
 
-class Intructor extends Person {
+class Instructor extends Person {
   constructor(props) {
     super(props)
     this.specialty = props.specialty
@@ -26,5 +26,42 @@ class Intructor extends Person {
 
   grade(student, subject) {
     console.log(`${student.name} recieves a perfect score on ${subject}`)
+  }
+}
+
+class Student extends Person {
+  constructor(props){
+    super(props)
+    this.previousBackground = props.previousBackground
+    this.className = props.className
+    this.favSubjects = props.favSubjects
+  }
+
+  listSubjects() {
+    this.favSubjects.map(item => console.log(item))
+  }
+
+  PRAssignment(subject) {
+    console.log(`${student.name} has submitted a PR for ${subject}`)
+  }
+
+  sprintChallenge(subject) {
+    console.log(`${student.name} has begun sprint challenge on ${subject}`)
+  }
+}
+
+class ProjectManager extends Instructor {
+  constructor(props) {
+    super(props)
+    this.gradClassName = props.gradClassName
+    this.favInstructor = props.favInstructor
+  }
+
+  standUp(channel) {
+    console.log(`${this.name} announces to ${channel} stand up time!`)
+  }
+
+  debugCode(student, subject) {
+    console.log(`${this.name} debugs ${student.name}\'s code on ${subject}`)
   }
 }
