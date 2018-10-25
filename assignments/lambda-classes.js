e5d214a2dabaa7453a038170ec80fddba1dfa98e
unknown <bj2336@email.vccs.edu>
2018-10-24 21:27:02 -0700

    Person Class Built

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,13 @@
 // CODE here for your Lambda Classes
+class Person {
+  constructor(props) {
+    this.name = attributes.name
+    this.age = attributes.age
+    this.location = attributes.location
+    this.gender= attributes.gender
+  }
+
+  speak() {
+    console.log(`Hello my name is ${this.name}, and I am from ${this.location]}`)
+  }
+}
