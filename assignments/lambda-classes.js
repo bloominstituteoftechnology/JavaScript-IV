986006c58774e45a71274b5db37d8e8be3bf0ad4
Alex Bezruchko <alex.bezruchko@gmail.com>
2018-10-25 20:58:55 -0400

    add instructor class and constructor
    - add instructor class/constructor, add specialty, favLanguage, catchPhrase, demo and grade method

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,24 +1,6 @@
 // CODE here for your Lambda Classes
 
-const john = new Humanoid({
-    
-});
 
-const jane = new Humanoid({
-    
-});
-
-const jimmy = new Humanoid({
-
-});
-
-const liz = new Humanoid({
-
-});
-
-const steph = new Humanoid({
-
-});
 
 /*
     #### Person
@@ -38,10 +20,73 @@ class Person {
         this.age = person.age;
         this.location = person.location;
         this.gender = person.gender;
-
     }
+    // Person speak function
     speak() {
         return `Hello my name is ${this.name}, I am from ${this.location}`
     }
 }
 
+/*
+
+#### Instructor
+
+* Now that we have a Person as our base class, we'll build our Instructor class.
+* Instructor uses the same attributes that have been set up by Person
+* Instructor has the following unique props:
+  * `specialty` what the Instructor is good at i.e. 'redux'
+  * `favLanguage` i.e. 'JavaScript, Python, Elm etc.'
+  * `catchPhrase` i.e. `Don't forget the homies`
+* Instructor has the following methods:
+  * `demo` receives a `subject` string as an argument and logs out the phrase 'Today we are learning about {subject}' where subject is the param passed in.
+  * `grade` receives a `student` object and a `subject` string as arguments and logs out '{student.name} receives a perfect score on {subject}'
+  * 
+*/
+
+class Instructor extends Person {
+    
+    constructor(instructor) {
+        super(instructor);
+        this.specialty = instructor.specialty;
+        this.favLanguage = instructor.favLanguage;
+        this.cathPhrase = instructor.cathPhrase;
+    }
+    // Instructor demo subject function
+    demo(subject) {
+        return `Today we are learning about ${this.subject}`;
+    }
+    // Instructor grade student function
+    grade(student, subject) {
+        return `${student.name} receives a perfect score on ${this.subject}`;
+    }
+}
+
+
+
+const fred = new Instructor({
+    name: 'Fred',
+    location: 'Bedrock',
+    age: 37,
+    gender: 'male',
+    favLanguage: 'JavaScript',
+    specialty: 'Front-end',
+    catchPhrase: `Don't forget the homies`
+  });
+
+// const jane = new Humanoid({
+    
+// });
+
+// const jimmy = new Humanoid({
+
+// });
+
+// const liz = new Humanoid({
+
+// });
+
+// const steph = new Humanoid({
+
+// });
+
+console.log(fred.name);
