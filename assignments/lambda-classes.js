8f797cf922b3b2d4036c872b50983728a651232b
Angie Lawrence <lawrenceangie45@gmail.com>
2018-10-25 23:31:19 -0400

    Finished Project

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,118 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(attributes) {
+        this.name = attributes.name;
+        this.location = attributes.location;
+        this.age = attributes.age;
+        this.gender = attributes.gender;
+    }
+    speak() {
+        return `Hello my name is ${this.name} and I am from ${this.location}.`;
+    }
+}
+
+//Child Class
+
+class Instructor extends Person {
+    constructor(childAttributes) {
+        super(childAttributes);
+        this.favLanguage = childAttributes.favLanguage;
+        this.specialty = childAttributes.specialty; 
+        this.catchPhrase = childAttributes.catchPhrase;
+        this.subject = childAttributes.subject
+    }
+    demo(subject) {
+        return `Hello today we will be learning about ${this.subject}`;
+    }
+}
+
+//Grandchild Class
+
+class Student extends Instructor {
+    constructor(grandChildAttributes) {
+        super(grandChildAttributes);
+        this.previousbackground = grandChildAttributes.previousbackground;
+        this.className = grandChildAttributes.className;
+        this.faveSubjects = grandChildAttributes.faveSubjects;
+    }
+    listsSubjects() {
+        return `These are my favorite subjects in Lambda School: ${this.faveSubjects}`;
+    }
+    PRAssignment() {
+        return `${this.name} has submitted a PR for ${this.subject}`;
+    }
+    sprintChallenge() {
+        return `${this.name} has begun the sprint challenge for ${this.subject}`;
+    }
+}
+
+//Great-Grandchild class
+
+class ProjectManager extends Student {
+    constructor(greatGrandChildAttributes) {
+        super(greatGrandChildAttributes);
+        this.gradClassName = greatGrandChildAttributes.gradClassName;
+        this.faveInstructor = greatGrandChildAttributes.faveInstructor;
+    }
+    standUp(channel) {
+        return `${this.name} announces to ${this.channel}, @channel It's StandUp time!`;
+    }
+    debugsCode() {
+        return `${this.name} debugs ${David.name}'s code on ${this.subject}`;
+    }
+}
+
+//Attributes
+
+const Isaiah = new Person ({
+    name: 'Isaiah',
+    location: 'Merced',
+    age: 22,
+    gender: 'Male'
+});
+
+const Chase = new Instructor ({
+    name: 'Chase',
+    location: 'Atwater',
+    age: 30,
+    gender: 'Male',
+    favLanguage: "Python, JavaScript",
+    specialty: "JavaScript",
+    catchPhrase: "That's Sick!",
+    subject: "JavaScript"
+});
+
+const David = new Student ({
+    name: 'David',
+    location: 'Mariposa',
+    age: 45,
+    gender: 'Male',
+    subject: "JavaScript",
+    previousbackground: "Construction/Painting",
+    className: "CS114",
+    faveSubjects: ["HTML", "CSS", "JavaScript"],
+});
+
+const Misty = new ProjectManager ({
+    name: 'Misty',
+    location: 'Cathy"s Valley',
+    age: 37,
+    gender: 'Female',
+    gradClassName: "CS2",
+    faveInstructor: "Keanu Reeves",
+    channel: "CSPT1",
+    subject: "JavaScript"
+});
+
+//Testing
+console.log(Isaiah.name);
+console.log(Isaiah.age);
+console.log(Isaiah.speak());
+console.log(Chase);
+console.log(David);
+console.log(David.listsSubjects());
+console.log(Chase.demo('JavaScript'));
+console.log(David.PRAssignment());
+console.log(David.sprintChallenge('today'));
+console.log(Misty.standUp());
+console.log(Misty.debugsCode());
--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -7,3 +7,105 @@ Prototype Refactor
 2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
 
 */
+
+class GameObject {
+    constructor(gameObjectAttributes) {
+    this.createdAt = gameObjectAttributes.createdAt;
+    this.dimensions = gameObjectAttributes.dimensions;
+  }
+  destroy() {
+      return `${this.name} was removed from the game.`;
+  }
+}
+  
+
+  class CharacterStats extends GameObject{
+      constructor(characterStatsAttribute) {
+        super(characterStatsAttribute)
+        this.hp = characterStatsAttribute.hp;
+        this.name = characterStatsAttribute.name;  
+  }
+  takeDamage() {
+      return `${this.name} took damage.`;
+  }
+}
+
+  class Humanoid extends CharacterStats {  
+      constructor(humanoidAttributes) {
+    super(humanoidAttributes);
+    this.faction = humanoidAttributes.faction; 
+    this.weapons = humanoidAttributes.weapons; 
+    this.language = humanoidAttributes.language; 
+  }
+  greet() {
+      return `${this.name} offers a greeting in ${this.language}.`;
+  }
+  }
+ 
+  const mage = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+      length: 2,
+      width: 1,
+      height: 1,
+    },
+    hp: 5,
+    name: 'Joseph',
+    faction: 'Mage Guild',
+    weapons: [
+      'Staff of Magic',
+    ],
+    language: 'Common',
+  });
+  
+  
+  const swordsman = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+      length: 2,
+      width: 2,
+      height: 2,
+    },
+    hp: 15,
+    name: 'Sir David',
+    faction: 'Knights Templar',
+    weapons: [
+      'large Sword',
+      'Shield',
+    ],
+    language: 'Common',
+  });
+  
+  const archer = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+      length: 1,
+      width: 2,
+      height: 4,
+    },
+    hp: 10,
+    name: 'Samantha',
+    faction: 'Kingdom of the Forest',
+    weapons: [
+      'Bow and Arrow',
+      'Small Dagger',
+    ],
+    language: 'Elvish',
+  });
+
+
+  
+  console.log(mage.createdAt);
+  console.log(archer.dimensions); 
+  console.log(swordsman.hp); 
+  console.log(mage.name); 
+  console.log(swordsman.faction); 
+  console.log(mage.weapons);
+  console.log(archer.language); 
+  console.log(archer.greet()); 
+  console.log(mage.takeDamage()); 
+  console.log(swordsman.destroy()); 
+  
+  
+
+  
