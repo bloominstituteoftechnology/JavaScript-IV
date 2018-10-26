1d37f1f1f6e8246276e00a0b7a3119d5e7b8d19d
JHaydenDev <34730927+JHaydenDev@users.noreply.github.com>
2018-10-25 21:38:54 -0500

    fixed some object code.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -2,128 +2,131 @@
 
 //BASE CLASS///////////////////////////
 class Person {
-    constructor (attributes){ // attributes
-        this.name = attributes.name
-        this.age = attributes.age
-        this.location = attributes.location
-        this.gender = attributes.gender
-    }
+  constructor(attributes) {
+    // attributes
+    this.name = attributes.name;
+    this.age = attributes.age;
+    this.location = attributes.location;
+    this.gender = attributes.gender;
+  }
 
-    speak(){
-      console.log(`Hello my name is ${this.name}, I am from ${this.location}.` ) 
-    }
+  speak() {
+    console.log(`Hello my name is ${this.name}, I am from ${this.location}.`);
+  }
 }
 //Instructor class////////////////////
-class Instructor extends Person{
-constructor (attributes,){
+class Instructor extends Person {
+  constructor(attributes) {
     super(attributes);
-    this.specialty = attributes.specialty
-    this.favLanguage = attributes.favLanguage
-    this.catchPhrase = attributes.catchPhrase
-}
-demo (subject){
-console.log(`Today we are learning about ${subject}`)
-}
-grade (student, subject){
-console.log(`${student.name} receives a perfect score on ${subject}`)
-}
+    this.specialty = attributes.specialty;
+    this.favLanguage = attributes.favLanguage;
+    this.catchPhrase = attributes.catchPhrase;
+  }
+  demo(subject) {
+    console.log(`Today we are learning about ${subject}`);
+  }
+  grade(student, subject) {
+    console.log(`${student.name} receives a perfect score on ${subject}`);
+  }
 }
 
 //Student Class//////////////////////////////////////
 
-class Student extends Person{
-    constructor (attributes,){
-        super(attributes);
-        this.previousBackground  = attributes.previousBackground 
-        this.className  = attributes.className 
-        this.favSubjects = attributes.favSubjects
-    }
-    listsSubjects  (){
-    console.log(this.favSubjects)
-    }
-    PRAssignment  (student,subject){
-    console.log(`${student.name} has submitted a PR for ${subject}`)
-    }
-    sprintChallenge  (student, subject){
-        console.log(`${student.name} has begun sprint challenge on ${subject}`)
-        }
-    }
+class Student extends Person {
+  constructor(attributes) {
+    super(attributes);
+    this.previousBackground = attributes.previousBackground;
+    this.className = attributes.className;
+    this.favSubjects = attributes.favSubjects;
+  }
+  listsSubjects() {
+    console.log(this.favSubjects);
+  }
+  PRAssignment(student, subject) {
+    console.log(`${student.name} has submitted a PR for ${subject}`);
+  }
+  sprintChallenge(student, subject) {
+    console.log(`${student.name} has begun sprint challenge on ${subject}`);
+  }
+}
 
-    //Project Manager Class//////////////////////////////////////
+//Project Manager Class//////////////////////////////////////
 
-    class ProjectManager extends Person{
-        constructor (attributes,){
-            super(attributes);
-            this.gradClassName  = attributes.gradClassName 
-            this.favInstructor  = attributes.favInstructor 
-        }
-        standUp (channel){
-        console.log(`${this.name} announces to ${channel}, @channel standy times!​​​​​`)
-        }
-        debugsCode (student, subject){
-        console.log(`${this.name} debugs ${student.name}'s code on ${subject}`)
-        }
-    }
+class ProjectManager extends Person {
+  constructor(attributes) {
+    super(attributes);
+    this.gradClassName = attributes.gradClassName;
+    this.favInstructor = attributes.favInstructor;
+  }
+  standUp(channel) {
+    console.log(
+      `${this.name} announces to ${channel}, @channel standy times!​​​​​`
+    );
+  }
+  debugsCode(student, subject) {
+    console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
+  }
+}
 
-    //New Instructor Objects////////////////////
-    const Fred = new Instructor({
-        name: 'Fred',
-        location: 'Bedrock',
-        age: 37,
-        gender: 'male',
-        favLanguage: 'JavaScript',
-        specialty: 'Front-end',
-        catchPhrase: `Don't forget the homies`
-      });
+//New Instructor Objects////////////////////
+const Fred = new Instructor({
+  name: "Fred",
+  location: "Bedrock",
+  age: 37,
+  gender: "male",
+  favLanguage: "JavaScript",
+  specialty: "Front-end",
+  catchPhrase: `Don't forget the homies`
+});
 
-       const Cali = new Instructor({
-        name: 'Cali',
-        location: 'Chicago',
-        age: 25,
-        gender: 'female',
-        favLanguage: 'python',
-        specialty: 'Back-End',
-        catchPhrase: `The homies have been forgotten`
-      });
+const Cali = new Instructor({
+  name: "Cali",
+  location: "Chicago",
+  age: 25,
+  gender: "female",
+  favLanguage: "python",
+  specialty: "Back-End",
+  catchPhrase: `The homies have been forgotten`
+});
 
-      //New Student Objects//////////////////////////////////
+//New Student Objects//////////////////////////////////
 
-      const Jack = new Student({
-        name: 'Jack',
-        location: 'up the hill',
-        age: 88,
-        gender: 'male',
-        previousBackground: 'Tiger Juggling', 
-        className: 'fswp3',
-        favSubject: 'Python basics',
-      });
+const Jack = new Student({
+  name: "Jack",
+  location: "up the hill",
+  age: 88,
+  gender: "male",
+  previousBackground: "Tiger Juggling",
+  className: "fswp3",
+  favSubject: "Python basics"
+});
 
-      const Jill = new Student({
-        name: 'Jill',
-        location: 'up the hill',
-        age: 99,
-        gender: 'female',
-        previousBackground: 'Lion Catching', 
-        className: 'fswp3',
-        favSubject: 'React',
-      });
+const Jill = new Student({
+  name: "Jill",
+  location: "up the hill",
+  age: 99,
+  gender: "female",
+  previousBackground: "Lion Catching",
+  className: "fswp3",
+  favSubject: "React"
+});
 
-      //New Project Manager Objects//////////////////////////////////
+//New Project Manager Objects//////////////////////////////////
 
-      const Conan = new ProjectManager({
-        name: 'Conan',
-        location: 'in battle bathing in the blood of his enemies',
-        age: 'imortal so age has lost all meaning',
-        gender: 'male',
-        gradClassName: 'Ancient battleaxing of time immemorial' , 
-        favInstructor: 'Great Shaman Blargggg',
-      });
+const Conan = new ProjectManager({
+  name: "Conan",
+  location: "in battle bathing in the blood of his enemies",
+  age: "imortal so age has lost all meaning",
+  gender: "male",
+  gradClassName: "Ancient battleaxing of time immemorial",
+  favInstructor: "Great Shaman Blargggg"
+});
 
-      const Conan = new ProjectManager({
-        name: 'Nathan',
-        location: 'LA',
-        age: '3',
-        gender: 'male',
-        gradClassName: 'Xavier school for the gifted', 
-        favInstructor: 'Professor Xavier',
-      });
+const Conan = new ProjectManager({
+  name: "Nathan",
+  location: "LA",
+  age: "3",
+  gender: "male",
+  gradClassName: "Xavier school for the gifted",
+  favInstructor: "Professor Xavier"
+});
