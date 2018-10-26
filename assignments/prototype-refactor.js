143a5b5d859a810fd046781b310eae7decca1842
Andrew <andrewryanogle@gmail.com>
2018-10-25 18:38:32 -0700

    finished refactoring to classes

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -18,20 +18,84 @@ class GameObject {
     }
 }
 class CharacterStats extends GameObject {
-    constructor(objectParams) {
-        super(objectParams);
-        this.hp = objectParams.hp;
-        this.name = objectParams.name;
+    constructor(characterParams) {
+        super(characterParams);
+        this.hp = characterParams.hp;
+        this.name = characterParams.name;
     }
     takeDamage() {
         return `${this.name} took damage`;
     }
 }
 class Humanoid extends CharacterStats {
-    constructor(objectParams) {
-        super(objectParams);
-        this.faction = objectParams.faction;
-        this.weapons = objectParams.weapons;
-        this.language = objectParams.language;
+    constructor(humanoidParams) {
+        super(humanoidParams);
+        this.faction = humanoidParams.faction;
+        this.weapons = humanoidParams.weapons;
+        this.language = humanoidParams.language;
     }
+    greet(){
+        return `${this.name} offers a greeting in ${this.language}`;
+      }
 }
+const mage = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+      length: 2,
+      width: 1,
+      height: 1,
+    },
+    hp: 5,
+    name: 'Bruce',
+    faction: 'Mage Guild',
+    weapons: [
+      'Staff of Shamalama',
+    ],
+    language: 'Common Toungue',
+  });
+
+  const swordsman = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+      length: 2,
+      width: 2,
+      height: 2,
+    },
+    hp: 15,
+    name: 'Sir Mustachio',
+    faction: 'The Round Table',
+    weapons: [
+      'Giant Sword',
+      'Shield',
+    ],
+    language: 'Common Toungue',
+  });
+
+  const archer = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+      length: 1,
+      width: 2,
+      height: 4,
+    },
+    hp: 10,
+    name: 'Lilith',
+    faction: 'Forest Kingdom',
+    weapons: [
+      'Bow',
+      'Dagger',
+    ],
+    language: 'Elvish',
+  });
+  
+
+  console.log(mage.createdAt); // Today's date
+  console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+  console.log(swordsman.hp); // 15
+  console.log(mage.name); // Bruce
+  console.log(swordsman.faction); // The Round Table
+  console.log(mage.weapons); // Staff of Shamalama
+  console.log(archer.language); // Elvish
+  console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+  console.log(mage.takeDamage()); // Bruce took damage.
+  console.log(swordsman.destroy());
