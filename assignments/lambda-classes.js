74d7a4b7bb74ef6e64ad9254f75f272b2000ced2
Brandon <Blent1050@yahoo.com>
2018-11-29 15:10:21 -0500

    lambda-classes constructors updated for Person and Instructor

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,27 @@
 // CODE here for your Lambda Classes
+class Person{
+    constructor(attributes){
+        this.name = attributes.name;
+        this.age = attributes.age;
+        this.location = attributes.location;
+        this.gender = attributes.gender;
+    }
+    speak(){
+        return `Hello. my name is ${this.name}, I am from ${this.location}`;
+    }
+}
+
+class Instructor extends Person{
+    constructor(instructorAttr){
+        this.specialty = instructorAttr.specialty;
+        this.favLanguage = instructorAttr.favLanguage;
+        this.catchPhrase = instructorAttr.catchPhrase;
+    }
+    demo(subject){
+        return `Today we are learning about ${subject}`
+    }
+    grade(student, subject){
+        return `${student.name} receives a perfect score on ${subject}`;
+    }
+}
+
