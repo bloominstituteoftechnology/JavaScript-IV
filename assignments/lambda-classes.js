5b8e08ea02d9601bd6cf19df23a83dffbe5d858d
Nathan Thomas <nwthomas@me.com>
2018-11-29 13:12:45 -0800

    Finish Person class in lambda-classes.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,14 @@
 // CODE here for your Lambda Classes
+
+class Person {
+  constructor(attributes) {
+    this.name = attributes.name;
+    this.age = attributes.age;
+    this.location = attributes.location;
+    this.gender = attributes.gender;
+  }
+
+  speak() {
+    return `Hello my name is ${this.name}, and I am from ${this.location}.`;
+  }
+}
