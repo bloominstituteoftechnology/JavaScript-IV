6622f640ecdd8cd7b0e56f03a05d34c834e64dd6
Matt Basile <matthewbasile2@gmail.com>
2018-11-29 14:44:21 -0500

    Added Instructors Class. LINES: lambda-classes.js 16-29.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,9 +1,32 @@
 // CODE here for your Lambda Classes
 // Person Class - Parent
-
+class Person{
+    constructor(attributes){
+        this.name = attributes.name;
+        this.age = attributes.age;
+        this.location = attributes.location;
+        this.gender = attributes.gender;
+    }
+    speak(){
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}.`)
+    }
+}
 
 // Instructors - Child
-
+class Instructors{
+    constructor(instructAttrs){
+        super(instructAttrs);
+        this.specialty = instructAttrs.specialty;
+        this.favLanguage = instructAttrs.favLanguage;
+        this.catchPhrase = instructAttrs.catchPhrase;
+    }
+    demo(subject){
+        console.log(`Today we are learning about ${subject}.`);
+    }
+    grade(student, subject){
+        console.log(`${student.name} receives a perfect score on ${subject}`);
+    }
+}
 
 // Students - Child 
 
