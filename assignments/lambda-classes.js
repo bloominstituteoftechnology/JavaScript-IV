317f7a4eb1ff5b75865bc50eae07594b412660f1
ltrii <lelandtr@gmail.com>
2018-11-29 15:05:07 -0800

    Finished main goals

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -21,4 +21,42 @@ class Instructor extends Person {
     demo(subject) {
         console.log(`Today we are learning about {subject}`);
     }
+
+    grade(student, subject) {
+        console.log(`${student.name} receives a perfect score on ${subject}`);
+    }
 }
+
+class Student extends Person {
+    constructor(att) {
+        this.previousBackground = att.previousBackground;
+        this.className = att.className;
+        this.favSubjects = att.favSubjects;
+    }
+    listSubjects() {
+        this.favSubjects.map(favSu => {
+            console.log(favSu);
+        });
+    }
+    PRAssignment(subject) {
+        console.log(`${this.name} has submitted a PR for ${subject}`);
+      }
+
+      sprintChallenge(subject) {
+        console.log(`${this.name} has begun sprint challenge on ${subject}`);
+      }
+}
+
+class ProjectManager extends Instructor {
+    constructor(att) {
+      super(att);
+      this.gradClassName = att.gradClassName;
+      this.faveInstructor = att.faveInstructor;
+    }
+     standUp(channel) {
+      console.log(`${this.name} announces to ${channel}, @channel standy times!`);
+    }
+     debugsCode(student, subject) {
+      console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
+    }
+  }
