e9038badf00b61ab40cd01129e563395399b7faf
Angel Torres <alas4224@gmail.com>
2018-11-29 15:37:10 -0600

    working on my lambda-classes. finished prototype refactor

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,48 @@
 // CODE here for your Lambda Classes
+
+class Person {
+    constructor(individual) {
+    this.name = individual.name
+    this.age = individual.age
+    this.location = individual.location
+    this.gender = individual.gender
+    }
+    
+    speak() {
+        return `Hello my name is ${this.name}, I am from ${this.location}`
+    }
+}
+
+class Instructor extends Person{
+    super(indivudual);
+    constructor(individual) {
+        this.specialty = individual.specialty;
+        this.favLanguage = individual.favLanguage;
+        this.catchPhrase = individual.catchPhrase;
+    }
+
+    demo(subject) {
+        return `Today we are learning about ${subject}`
+    }
+
+    grade(student, subject) {
+        return `${student.name} receives a perfect score on ${subject}`
+    }
+}
+
+class Student extends Person {
+    super(stu);
+    constructor(stu) {
+        this.previousBackground = stu.previousBackground;
+        this.className = stu.className;
+        this.favSubjects = stu.favSubjects;
+    }
+    listsSubjects() {
+        this.favSubjects.forEach(element => {
+            return element;
+        });
+    }
+    PRAssignment(subject) {
+        return `${this.name}has submitted a PR for ${subject}`
+    }
+}
