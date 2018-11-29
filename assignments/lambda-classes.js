3c7b2ef315cab5e1466e689460ec2b53f2b9014e
Kingaa6 <Kingaa6@gmail.com>
2018-11-29 13:16:11 -0800

    Base class and childred classes established

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,36 @@
 // CODE here for your Lambda Classes
+
+// base class
+class Person {
+  constructor(attributes) {
+    this.name = attributes.name;
+    this.location = attributes.location;
+    this.age = attributes.age;
+    this.gender = attributes.gender;
+    this.favLanguage = attributes.favLanguage;
+    this.specialty = attributes.specialty;
+    this.catchPhrase = attributes.catchPhrase;
+  }
+  speak() {
+    console.log(`Hello my name is ${this.name} I am from ${this.location}`);
+  }
+}
+
+// child classes of Person
+class Instuctors extends Person {
+  constructor(instructorAttrs) {
+    super(instructorAttrs);
+  }
+}
+
+class Students extends Person {
+  constructor(studentAttrs) {
+    super(studentAttrs);
+  }
+}
+
+class ProjectManagers extends Person {
+  constructor(projectManagerAttrs) {
+    super(projectManagerAttrs);
+  }
+}
