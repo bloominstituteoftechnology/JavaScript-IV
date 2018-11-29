96cfd8757500d9ba296523cc55b415def71bbfc6
siratl <elishubax@gmail.com>
2018-11-29 15:57:06 -0800

    Lambda Classes: Built Project Manager Class and began Stretch problems

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -3,41 +3,42 @@
 
 // Create parent 'person' class
 class Person {
-    constrtuctor (attributes) {
+    constructor (attributes) {
         this.name = attributes.name;
         this.age = attributes.age;
         this.location = attributes.location;
         this.gender = attributes.gender;
-    };
+    }
     speak () {
         return `Hello my name is ${this.name}, I am from ${this.location}.`
     }
 };
 
 class Instructor extends Person {
-    constructor (attributes) {
-        super(attributes);
-        this.speciality = attributes.spaciality; //"redux",
-        this.favLanguage = attributes.favLanguage; //"JavaScript, Python, Elm",
-        this.catchPhrase = attributes.catchPhrase //"Don't forget the homies"
-    };
+    constructor (insAttributes) {
+        super(insAttributes);
+        this.specialty = insAttributes.specialty; //"redux",
+        this.favLanguage = insAttributes.favLanguage; //"JavaScript, Python, Elm",
+        this.catchPhrase = insAttributes.catchPhrase //"Don't forget the homies"
+    }
     demo (subject) {
         return `Today we are learning about ${subject}.`;
-    };
+    }
     grade (student, subject) {
         return `${student.name} receives a perfect score on ${subject}.`
-    };
+    }
 };
 
 class Student extends Person {
     constructor (attributes) {
         super(attributes);
         this.previousBackground = attributes.previousBackground;
-        this.className = attributes.className; // "CS116",
+        this.className = attributes.className; // "CS116"
         this.favSubjects = attributes.favSubjects; // ['Html', 'CSS', 'JavaScript']
+        this.grade = attributes.grade;
     };
     listsSubjects () {
-        return ` ${this.favSubjects}.`;
+        return this.favSubjects;
     };
     PRAssignment (subject) {
         return `${student.name} has submitted a PR for ${subject}.`
@@ -60,3 +61,75 @@ class ProjectManagers extends Instructor {
         return `${this.name} debugs ${student.name}'s code on ${subject}.`
     };
 };
+
+
+
+// ========================================= Create Characters/persons ============>
+
+const fred = new Instructor({
+    name: 'Fred',
+    location: 'Bedrock',
+    age: 37,
+    gender: 'male',
+    favLanguage: 'JavaScript',
+    specialty: 'Front-end',
+    catchPhrase: `Don't forget the homies`
+});
+
+//========================================= STUDENTS ================>
+const tom = new Student({
+    name: 'Tom',
+    location: 'Los Angeles',
+    age: 31,
+    gender: 'male',
+    favSubjects: ['Preprocessing', 'CSS', 'JavaScript'],
+    className: 'FSW177',
+    previousBackground: `Attorney`,
+    grade: 70
+});
+
+const sam = new Student({
+    name: 'Samantha',
+    location: 'Korea',
+    age: 28,
+    gender: 'female',
+    favSubjects: ['Html', 'JavaScript', 'CSS'],
+    className: 'FSW107',
+    previousBackground: `Architect`,
+    grade: 79
+});
+
+const alexis = new Student({
+    name: 'Alexis',
+    location: 'Phili',
+    age: 25,
+    gender: 'female',
+    favSubjects: ['Html', 'Flexbox', 'JavaScript'],
+    className: 'FSW107',
+    previousBackground: `Architect`,
+    grade: 87
+});
+
+//===================================== PROJECT MANAGERS =================>
+const bill = new Instructor({
+    name: 'Bill',
+    location: 'Hamrock',
+    age: 47,
+    gender: 'male',
+    gradClassName: 'CS1',
+    favInstructor: 'Josh',
+});
+const hancock = new Instructor({
+    name: 'Hancock',
+    location: 'Nowhere',
+    age: 33,
+    gender: 'male',
+    gradClassName: 'CS12',
+    favInstructor: 'Sean',
+});
+
+//=========================================== TEST =================>
+
+console.log(fred);
+console.log(tom);
+console.log(sam.speak());
