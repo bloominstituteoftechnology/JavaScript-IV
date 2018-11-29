5bf6ea5f0f4cde09903e69e2593035cb5c2129ab
RachelKolk <rach.m.obrien@gmail.com>
2018-11-29 15:34:16 -0500

    base class person created and saved

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,21 @@
 // CODE here for your Lambda Classes
+
+
+//=========== Base class
+
+class Person {
+    constructor(attributes) {
+        this.name = attributes.name;
+        this.age = attributes.age;
+        this.location = attributes.location;
+        this.gender = attributes.gender;
+    }
+    speak() {
+        console.log(`Hello, my name is ${this.name}, I am from ${this.location}.`);
+    }
+}
+
+
+//=========== Instructor class
+
+
