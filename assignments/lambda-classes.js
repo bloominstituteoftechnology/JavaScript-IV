4f8a369c02c726a8b4244d6af126ed59e8db98d7
Jaouad Sarouti <jadsarouti@outlook.com>
2018-11-29 15:03:44 -0500

    Stretches done

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,9 +1,9 @@
 // CODE here for your Lambda Classes
 class Person {
-    constructor(name,location,age, gender) {
+    constructor(name, location, age, gender) {
         this.name = name;
         this.location = location;
-        this.age=age;
+        this.age = age;
         this.gender = gender;
     }
     speak() {
@@ -12,8 +12,8 @@ class Person {
 }
 
 class Instructor extends Person {
-    constructor(name, location,age,gender, favLanguage,speciality, catchPhrase) {
-        super(name, location,age, gender);
+    constructor(name, location, age, gender, favLanguage, speciality, catchPhrase) {
+        super(name, location, age, gender);
         this.speciality = speciality;
         this.favLanguage = favLanguage;
         this.catchPhrase = catchPhrase;
@@ -29,22 +29,33 @@ class Instructor extends Person {
 }
 
 class Student extends Person {
-    constructor(name,location, age, gender, previousBackground, className, favSubjects,grade) {
-        super(name,location, age, gender);
+    constructor(name, location, age, gender, previousBackground, className, favSubjects, grade) {
+        super(name, location, age, gender);
         this.previousBackground = previousBackground;
         this.className = className;
         this.favSubjects = favSubjects;
-        this.grade = 99;
+        this.grade = 100;
     }
 
-    randomMethod(){
+    randomMethod() {
+        let res = this.grade - Math.floor((Math.random() * Math.floor(Math.random() * 100)));
+        return `${res}`;
+    }
 
+    graduateMethod(randomMethod) {
+        let str = "";
+        if (randomMethod >= 70) {
+            str = "success, you will graduate";
+        } else {
+            str = "You have to try again";
+        }
+        return str;
     }
 
     listsSubjects(favSubjects) {
-            for(let i = 0 ; i < favSubjects.length ; i++){
-                console.log(favSubjects[i]);
-            }
+        for (let i = 0; i < favSubjects.length; i++) {
+            console.log(favSubjects[i]);
+        }
     }
 
     PRAssignments(subject) {
@@ -57,8 +68,8 @@ class Student extends Person {
 }
 
 class ProjectManager extends Instructor {
-    constructor(name, age, location,gender, speciality, favLanguage, catchPhrase, gradeClassName, favInstructor) {
-        super(name, age,location, gender, speciality, favLanguage, catchPhrase);
+    constructor(name, age, location, gender, speciality, favLanguage, catchPhrase, gradeClassName, favInstructor) {
+        super(name, age, location, gender, speciality, favLanguage, catchPhrase);
         this.gradeClassName = gradeClassName;
         this.favInstructor = favInstructor;
 
@@ -74,26 +85,31 @@ class ProjectManager extends Instructor {
     }
 }
 
-const person = new Person("Raw3a","Finland",999,"M")
+const person = new Person("Raw3a", "Finland", 999, "M")
 console.log(person.speak());
 
-const instructor = new Instructor("Vandam","Sweden",999,"M","Arabic","Mid food","never give up");
+const instructor = new Instructor("Vandam", "Sweden", 999, "M", "Arabic", "Mid food", "never give up");
 console.log(instructor.speak());
 console.log(instructor.demo("Math"));
-console.log(instructor.grade(new Student("Ghizlan","IRELAND",100,"M","JAVA","JAVA"),"JAVA"));
+console.log(instructor.grade(new Student("Ghizlan", "IRELAND", 100, "M", "JAVA", "JAVA"), "JAVA"));
 
-let favSbubjectArray = ["Math","Physics","Arabic","history","Astronomy"];
-const student = new Student("Brianna","Germany",1000,"M","Python","programming-III",favSbubjectArray);
+let favSbubjectArray = ["Math", "Physics", "Arabic", "history", "Astronomy"];
+const student = new Student("Brianna", "Germany", 1000, "M", "Python", "programming-III", favSbubjectArray);
 student.listsSubjects(favSbubjectArray);
 console.log(student.PRAssignments("Astronomy"))
 console.log(student.sprintChallenge())
 console.log(student.speak());
 
-let projectManager = new ProjectManager("x-Men",88,"Arlington,Va","F","Python",
-"arrogance mixed with ignorance will take you a long way lol hahahah","C language","hulk");
+let projectManager = new ProjectManager("x-Men", 88, "Arlington,Va", "F", "Python",
+    "arrogance mixed with ignorance will take you a long way lol hahahah", "C language", "hulk");
 console.log(projectManager.demo("c language"));
-console.log(projectManager.grade(new Student("Stalon","scandinavia",1000,"M","JAVA","programming-III",favSbubjectArray),"Arabic"));
+console.log(projectManager.grade(new Student("Stalon", "scandinavia", 1000, "M", "JAVA", "programming-III", favSbubjectArray), "Arabic"));
 console.log(projectManager.standUp("FSW16"));
-console.log(projectManager.debugsCode(new Student("Brianna","Germany",1000,"M","Python","programming-III",favSbubjectArray),"Physics"));
+console.log(projectManager.debugsCode(new Student("Brianna", "Germany", 1000, "M", "Python", "programming-III", favSbubjectArray), "Physics"));
 
 console.log("========================================Stretches====================================================")
+
+let std2 = new Student("Mathieu", "France", 500, "M", "Architecture", "linear Algebra");
+res = std2.randomMethod();
+console.log("Your current automatic grade is " + res);
+console.log(std2.graduateMethod(res));
--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -105,3 +105,4 @@ const mage = new Humanoid({
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+  console.log("============================================Lambda-classes==========================================")
