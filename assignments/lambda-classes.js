0dbd71edfaecf69f5c82459a9c3b1810dae36a89
ltrii <lelandtr@gmail.com>
2018-11-29 14:53:52 -0800

    Init. Conversions done and finished some lambda classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,24 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(att){
+    this.name = att.name;
+    this.age = att.age;
+    this.location = att.location;
+    }
+    speak() {
+        console.log(`Hello my name is ${this.name}, I am from {this.location}`);
+    }
+}
+
+class Instructor extends Person {
+    constructor(InstrAtt) {
+        super(InstrAtt);
+        this.specialty = InstrAtt.specialty;
+        this.favLanguage = InstrAtt.favLanguage;
+        this.catchPhrase = InstrAtt.catchPhrase;
+    }
+
+    demo(subject) {
+        console.log(`Today we are learning about {subject}`);
+    }
+}
