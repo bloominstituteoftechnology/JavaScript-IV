0915c8fee0602656672a35ea6f7dd5e67fabf177
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2018-11-29 12:47:10 -0600

    created the base class Person for the lamda-classes.js file

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,14 @@
 // CODE here for your Lambda Classes
+
+class Person {
+    constructor(attr){
+        this.name = attr.name;
+        this.age = attr.age;
+        this.location = attr.location;
+        this.gender = attr.gender;
+    }
+
+    speak() {
+        return `Hello my name is ${this.name}, I am from ${this.location}!`
+    }
+}
