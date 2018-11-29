5bd2522c040461a005b0dd7cd02b4b282ee57757
Marc Gutnayer <mgutnayer@gmail.com>
2018-11-29 17:10:35 -0600

    Finished refactoring of prototype-refactor.js file, beginning work on lambda-classes.js file

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -1,11 +1,7 @@
 /* 
-
 Prototype Refactor
-
 1. Copy and paste your code or the solution from yesterday
-
 2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
-
 */
 
 /*
@@ -15,14 +11,16 @@ Prototype Refactor
   * destroy() // prototype method -> returns the string: 'Object was removed from the game.'
 */
 
-function GameObject(attributes) {
-    this.createdAt = attributes.createdAt;
-    this.dimensions = attributes.dimensions;
-    this.destroy = function() {
-      return(`${this.name} was removed from the game`);
+  class GameObject {
+    constructor(attributes) {
+        this.createdAt = attributes.createdAt;
+        this.dimensions = attributes.dimensions;
+        this.destroy = function() {
+          return(`${this.name} was removed from the game`);
+        }
     }
   }
-  
+
   /*
     === CharacterStats ===
     * healthPoints
@@ -30,15 +28,19 @@ function GameObject(attributes) {
     * takeDamage() // prototype method -> returns the string '<object name> took damage.'
     * should inherit destroy() from GameObject's prototype
   */
-  function CharacterStats(attributes) {
-    GameObject.call(this, attributes);
-    this.healthPoints = attributes.healthPoints;
-    this.name = attributes.name;
-    this.takeDamage = function () {
-      return `${this.name} took damage.`
+  
+class CharacterStats extends GameObject {
+    constructor(attributes) {
+        super(attributes);
+        
+        this.healthPoints = attributes.healthPoints;
+        this.name = attributes.name;
+        this.takeDamage = function () {
+          return `${this.name} took damage.`
+        }
     }
   }
-  
+
   /*
     === Humanoid (Having an appearance or character resembling that of a human.) ===
     * team
@@ -48,47 +50,56 @@ function GameObject(attributes) {
     * should inherit destroy() from GameObject through CharacterStats
     * should inherit takeDamage() from CharacterStats
   */
-  
-  function Humanoid(attributes) {
-    CharacterStats.call(this, attributes);
-    this.team = attributes.team;
-    this.weapons = attributes.weapons;
-    this.language = attributes.language;
-    this.greet = function () {
-      return `${this.name} offers a greeting in ${this.language}`
+
+class Humanoid extends CharacterStats {
+    constructor(attributes) {
+        super(attributes);
+
+        this.team = attributes.team;
+        this.weapons = attributes.weapons;
+        this.language = attributes.language;
+        this.greet = function () {
+          return `${this.name} offers a greeting in ${this.language}`
+        }
     }
   }
-  
-  function Hero(attributes) {
-    Humanoid.call(this, attributes);
-    this.punch = function (person) {
-      const dmg = Math.ceil(Math.random() * 10);
-  
-      person.healthPoints -= dmg;
+
+class Hero extends Humanoid {
+    constructor(attributes) {
+        super(attributes);
+
+        this.punch = function (person) {
+          const dmg = Math.ceil(Math.random() * 10);
       
-      if(person.healthPoints <= 0) {
-        return person.name + " has died!"
-      } else {
-        return `${this.name} punches ${person.name} for ${dmg}`
-      }
+          person.healthPoints -= dmg;
+          
+          if(person.healthPoints <= 0) {
+            return person.name + " has died!"
+          } else {
+            return `${this.name} punches ${person.name} for ${dmg}`
+          }
+        }
     }
   }
-  
-  function Villain(attributes) {
-    Humanoid.call(this, attributes);
-    this.throw = function (person) {
-      const dmg = Math.ceil(Math.random() * 10);
-  
-      person.healthPoints -= dmg;
-  
-      if(person.healthPoints <= 0) {
-        return person.name + " has died!"
-      } else {
-        return `${this.name} throws acid at ${person.name} for ${dmg}`
-      }
+
+class Villain extends Humanoid {
+    constructor(attributes) {
+        super(attributes);
+
+        this.throw = function (person) {
+          const dmg = Math.ceil(Math.random() * 10);
+      
+          person.healthPoints -= dmg;
+      
+          if(person.healthPoints <= 0) {
+            return person.name + " has died!"
+          } else {
+            return `${this.name} throws acid at ${person.name} for ${dmg}`
+          }
+        }
     }
   }
-   
+
   /*
     * Inheritance chain: GameObject -> CharacterStats -> Humanoid
     * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
