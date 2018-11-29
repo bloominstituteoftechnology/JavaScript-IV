3adae3e9e18ebb01e493638aceb4cadcbe6cd7f9
Lenna <lenna@lennamau.com>
2018-11-29 15:27:29 -0700

    JavaScript 4 - completed
    working on stretch

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,143 @@
 // CODE here for your Lambda Classes
+
+// parent Person
+class Person {
+    constructor(attributes){
+      this.name = attributes.name; 
+      this.age = attributes.age;
+      this.location = attributes.location;
+      this.gender = attributes.gender;
+    }   
+    speak() {
+      return (`Hello my name is ${this.name}, I am from ${this.location}.`);
+    }
+  }
+  // child Instructor of Person
+
+  class Instructor extends Person {
+    constructor(instrAttributes) {
+      super(instrAttributes);
+      this.specialty = instrAttributes.specialty;
+      this.favLanguage = instrAttributes.favLanguage;
+      this.catchPhrase = instrAttributes.catchPhrase;
+    }
+    demo(subject) {
+      return (`Today we are learning about ${(subject)}.`);
+    }
+    grade(subject) {
+       return `${student.name} receives a perfect score on ${(subject)}`;
+    }
+  }
+  
+// child Student of Person
+
+class Student extends Person {
+    constructor(studentAttributes) {
+      super(studentAttributes);
+      this.previousBackground = studentAttributes.previousBackground;
+      this.className = studentAttributes.className;
+      this.favSubjects = studentAttributes.favSubjects;
+    }
+    listsSubjects() {
+      console.log(student.favSubjects);
+    }
+    PRAssignment(subject) {
+       return `${student.name} has submitted a PR for ${(subject)}`;
+    }
+    sprintChallenge(subject) {
+        return `${student.name} has begun sprint challenge on ${(subject)}`;
+    }
+  }
+ /// child of Instructor
+  class ProjectManager extends Instructor {
+    constructor(ProjManAttributes) {
+      super(ProjManAttributes);
+      this.gradClassName = ProjManAttributes.gradClassName;
+      this.favInstructor = ProjManAttributes.favInstructor;
+    }
+
+    standUp(channel) {
+       console.log(`${this.name} announces to ${(channel)}, @channel standy times!`);
+    }
+    debugsCode(Student, subject) {
+        console.log(`${this.name} debugs ${Student.name}'s code on  ${(subject)}`);
+    }
+  }
+
+  const fred = new Instructor({
+    name: 'Fred',
+    location: 'Bedrock',
+    age: 37,
+    gender: 'male',
+    favLanguage: 'JavaScript',
+    specialty: 'Front-end',
+    catchPhrase: `Don't forget the homies`
+  });
+
+  const george = new Instructor({
+    name: 'George',
+    location: 'Atlanta',
+    age: 47,
+    gender: 'male',
+    favLanguage: 'CSS',
+    specialty: 'Front-end',
+    catchPhrase: `Yeah, that's the ticket!`
+  });
+
+  const elaine = new Student ({
+    name: 'Elaine',
+    location: 'New York',
+    age: 27,
+    gender: 'female',
+    previousBackground: 'Book Editor',
+    className: 'FSW22',
+    favSubjects: ['HTML', 'CSS', 'React']
+  });
+
+  const kramer = new Student ({
+    name: 'Kramer',
+    location: 'Denver',
+    age: 24,
+    gender: 'male',
+    previousBackground: 'Doctor',
+    className: 'FSW24',
+    favSubjects: ['HTML', 'Python', 'React']
+  });
+
+  const katie = new Student ({
+    name: 'Katie',
+    location: 'Detroit',
+    age: 34,
+    gender: 'female',
+    previousBackground: 'Artist',
+    className: 'FSW21',
+    favSubjects: ['CSS', 'Python', 'React']
+  });
+
+  const brendan = new ProjectManager({
+    name: 'Brendan',
+    location: 'Phoenix',
+    age: 27,
+    gender: 'male',
+    gradClassName: 'CS3',
+    favInstructor: 'Josh'
+  });
+
+  const tyler = new ProjectManager({
+    name: 'Tyler',
+    location: 'Los Angeles',
+    age: 31,
+    gender: 'male',
+    gradClassName: 'CS2',
+    favInstructor: 'Josh'  
+  });
+
+  const ryan = new ProjectManager({
+    name: 'Ryan',
+    location: 'Orlando',
+    age: 39,
+    gender: 'male',
+    gradClassName: 'CS6',
+    favInstructor: 'Josh'  
+  });
+
