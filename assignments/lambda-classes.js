d387331f082929ee247a7161497465739f3ae0d5
Kingaa6 <Kingaa6@gmail.com>
2018-11-29 13:33:17 -0800

    Instructor methods added and tested working

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -7,9 +7,7 @@ class Person {
     this.location = attributes.location;
     this.age = attributes.age;
     this.gender = attributes.gender;
-    this.favLanguage = attributes.favLanguage;
-    this.specialty = attributes.specialty;
-    this.catchPhrase = attributes.catchPhrase;
+
   }
   speak() {
     console.log(`Hello my name is ${this.name} I am from ${this.location}`);
@@ -17,20 +15,55 @@ class Person {
 }
 
 // child classes of Person
-class Instuctors extends Person {
+class Instuctor extends Person {
   constructor(instructorAttrs) {
     super(instructorAttrs);
+    this.favLanguage = instructorAttrs.favLanguage;
+    this.specialty = instructorAttrs.specialty;
+    this.catchPhrase = instructorAttrs.catchPhrase;
+  }
+
+  demo(subject) {
+    console.log(`Today we are learning about ${subject}!`);
+  }
+  grade(student, subject) {
+    console.log(`${student.name} receives a perfect score on ${subject}!`);
   }
 }
 
-class Students extends Person {
+class Student extends Person {
   constructor(studentAttrs) {
     super(studentAttrs);
   }
+
 }
 
-class ProjectManagers extends Person {
+class ProjectManager extends Person {
   constructor(projectManagerAttrs) {
     super(projectManagerAttrs);
   }
 }
+
+
+
+const fred = new Instuctor({
+  name: 'Fred',
+  location: 'Bedrock',
+  age: 37,
+  gender: 'male',
+  favLanguage: 'JavaScript',
+  specialty: 'Front-end',
+  catchPhrase: `Don't forget the homies`
+})
+
+const tom = new Student({
+  name: 'Tom',
+  location: 'Seattle',
+  age: 28,
+  gender: 'male'
+})
+
+
+fred.grade(tom, "Java");
+
+console.log(fred);
