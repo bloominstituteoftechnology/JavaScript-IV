bb0827ef6962f88e03d16f112a61b4ba7efd7109
Nate Boyette <nateboyette@icloud.com>
2018-11-29 14:30:52 -0800

    Added PM Class. Wrote logs for testing.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -25,7 +25,7 @@ class Instructor extends Person {
     }
 
     demo(subject) {
-        return `Today we are learning about ${subject}`;
+        return `Today we are learning about ${subject}.`;
     }
 
     grade(student, subject) {
@@ -42,13 +42,13 @@ class Student extends Person {
         super(studentAttrs);
 
         this.previousBackground = studentAttrs.previousBackground;
-        this.favSubject = studentAttrs.favSubject;
+        this.favSubjects = studentAttrs.favSubjects;
 
     }
 
-    listsSubjects(subjects) {
-        return subjects.forEach((subject) => {
-            return `${subject}`;
+    listSubjects() {
+        return this.favSubjects.forEach((subject) => {
+            console.log(`${subject}`);
         })
     }
 
@@ -63,3 +63,96 @@ class Student extends Person {
 
 
 }
+
+
+class ProjectManager extends Instructor {
+    constructor(pmAttrs) {
+        super(pmAttrs);
+
+        this.gradClassName = pmAttrs.gradClassName;
+        this.favInstructor = pmAttrs.favInstructor;
+    }
+
+    standUp(channel) {
+        return `@${this.name} announces to @${channel}, Standy time!`
+    }
+
+    debugsCode(student, subject) {
+        return `${this.name} degugs ${student.name}'s code on ${subject}.`
+    }
+
+}
+
+
+const john = new Instructor({
+    name: 'John',
+    age: 40,
+    location: 'San Francisco',
+    gender: 'M',
+
+    specialty: 'Front-End Development',
+    favLanguage: 'JavaScript',
+    catchPhrase: 'Secret Sauce'
+
+
+})
+
+
+const lisa = new Student({
+    name: 'Lisa',
+    age: 23,
+    location: 'Dallas',
+    gender: 'F',
+
+    previousBackground: 'Art',
+    favSubjects: ['React', 'CSS', 'JavaScript', 'Back-End']
+})
+
+
+const jason = new ProjectManager({
+    name: 'Jason',
+    age: 28,
+    location: 'Los Angeles',
+    gender: 'M',
+
+    specialty: 'React',
+    favLanguage: 'TypeScript',
+    catchPhrase: 'You got this',
+
+    gradClassName: `CS10`, 
+    favInstructor: 'John'
+})
+
+
+// Instructor Tests
+console.log(``);
+console.log(`**** INSTRUCTOR TESTS ****`);
+console.log(``);
+
+
+console.log(john.greeting());
+console.log(john.demo('closures'));
+console.log(john.grade(lisa, 'JavaScript Fundamentals'));
+
+
+//Student Tests
+console.log(``);
+console.log(`***** STUDENT TESTS*****`)
+console.log(``);
+
+
+console.log(lisa.greeting());
+console.log(lisa.listSubjects());
+
+
+// PM Tests
+console.log(``);
+console.log(`***** PRODUCT MANAGER TESTS*****`)
+console.log(``);
+
+console.log(jason.greeting());
+console.log(jason.standUp('fsw16'));
+console.log(jason.debugsCode(lisa, 'JavaScript-IV'));
+
+
+
