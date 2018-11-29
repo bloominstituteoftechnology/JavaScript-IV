26cd67836b8fa868a99e65a1e775524f14fe067c
siratl <elishubax@gmail.com>
2018-11-29 14:54:37 -0800

    Lambda Classes: Building rooster of lambda School personnel

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,12 @@
 // CODE here for your Lambda Classes
+
+
+// Create parent 'person' class
+class Person {
+    constrtuctor (attributes) {
+        this.name = attributes.name;
+        this.age = attributes.age;
+        this.location = attributes.location;
+        this.gender = attributes.gender;
+    };
+};
