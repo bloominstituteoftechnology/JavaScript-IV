55b8487e2ff1d41e5f45093d9ad8ddf89c4f4cad
cecil john tantay <cjbtantay@gmail.com>
2018-11-29 10:01:50 -0800

    created person class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,13 @@
 // CODE here for your Lambda Classes
+
+class Person {
+  constructor(personAtr) {
+    this.name = personAtr.name;
+    this.age = personAtr.age;
+    this.location = personAtr.location;
+    this.gender = personAtr.gender;
+  }
+  speak() {
+    return `Hello, my name is ${this.name}, I am from ${this.location}`
+  }
+}
