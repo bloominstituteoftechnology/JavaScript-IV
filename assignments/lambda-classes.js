e0a979ae7840ffa3e414b0654e05e73fecb666fd
aljoebacus <aljoebacus@gmail.com>
2018-11-29 12:56:36 -0700

    Created Person object for lambda-classes.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,22 @@
 // CODE here for your Lambda Classes
+
+class Person {
+    constructor(attributes) {
+        this.name = attributes.name;
+        this.age = attributes.age;
+        this.location = attributes.location;
+        this.gender = attributes.gender;
+    }
+    speak() {
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
+    }
+}
+
+let bruh = new Person({
+    name: "bruh",
+    age: -999,
+    location: "unknown",
+    gender: "helicopter"
+});
+
+bruh.speak();
