14687372318d7f84dad5edaf1997d833286bbb07
David Flack <davidscottflack@gmail.com>
2018-11-29 16:08:47 -0600

    Create ProjectManagers class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -43,3 +43,30 @@ class Student extends Person {
         return `${this.name} has begun sprint challenge on ${subject}`;
     }
 }
+
+
+class ProjectManagers extends Instructor {
+    constructor(pmAttributes) {
+        super(pmAttributes);
+        this.gradClassName = pmAttributes.gradClassName;
+        this.favInstructor = pmAttributes.favInstructor;
+    }
+    standUp(channel) {
+        return `${this.name} announces to ${channel}, @channel standy times!`
+    }
+    debugsCode(student, subject) {
+        return `${this.name} debugs ${student.name}'s code on ${subject}`;
+    }
+}
+
+/*Stretch Problem
+Extend the functionality of the Student by adding a prop called grade and setting it equal to a number between 1-100.
+
+Now that our students have a grade build out a method on the Instructor (this will be used by BOTH instructors and PM's) that will randomly
+add or subtract points to a student's grade. Math.random will help.
+
+Add a graduate method to a student.
+
+This method, when called, will check the grade of the student and see if they're ready to graduate from Lambda School
+
+If the student's grade is above a 70% let them graduate! Otherswise go back to grading their assignments to increase their score.*/
