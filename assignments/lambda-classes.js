ca2426bbed096e4cfac1ffc07224fcce53d4ddf2
RachelKolk <rach.m.obrien@gmail.com>
2018-11-29 15:41:53 -0500

    instructor class added

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -18,4 +18,17 @@ class Person {
 
 //=========== Instructor class
 
-
+class Instructor extends Person {
+    constructor(instructAttribs) {
+        super(instructAttribs);
+        this.specialty = instructAttribs.specialty;
+        this.favLanguage = instructAttribs.favLanguage;
+        this.catchPhrase = instructAttribs.catchPhrase;
+    }
+    demo(subject) {
+        console.log(`Today we are learning about ${subject}`);
+    }
+    grade(student, subject) {
+        console.log(`${student} receives a perfect score on ${subject}`);
+    }
+}
