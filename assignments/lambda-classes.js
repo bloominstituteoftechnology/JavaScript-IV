a8ffafd0c01862d2fa65709bf18e0be4500ff511
Imon Ovbude <imonovbude@gmail.com>
2018-11-29 12:44:55 -0800

    created person class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,14 @@
 // CODE here for your Lambda Classes
+// -----------Person Class-----------
+class Person {
+	constructor(human) {
+		this.name = human.name;
+		this.age = human.age;
+		this.location = human.location;
+		this.gender = human.gender;
+	}
+
+	speak() {
+		return `Hello my name is ${this.name}, I am from ${this.location}.`;
+	}
+}
