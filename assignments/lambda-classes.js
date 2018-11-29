55ddcc3e9b91387a9766a4ba60041be643e2a2d7
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2018-11-29 15:57:11 -0500

    lambda-classes.js created, not tested

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,23 +1,62 @@
 class Person {
-    constructor(){
-        
+    constructor(pAtts){
+        this.name = pAtts.name;
+        this.age = pAtts.age;
+        this.location = pAtts.location;
+        this.gender = pAtts.gender;
+    }
+    speak() {
+        return `Hello my name is ${this.name}, I am from ${this.location}.`;
     }
 }
+
 
 class Instructors extends Person {
-    constructor(){
-        super();
+    constructor(iAtts){
+        super(iAtts);
+        this.specialty = iAtts.specialty;
+        this.favLanguage = iAtts.favLanguage;
+        this.catchPhrase = iAtts.catchPhrase;
+    }
+    demo(subject) {
+        return `Today we are learning about ${subject}`;
+    }
+    grade(student, subject) {
+        return `${student.name} receives a perfect store on ${subject}`;
     }
 }
 
+
 class Students extends Person {
-    constructor(){
-        super();
+    constructor(sAtts){
+        super(sAtts);
+        this.previousBackground = sAtts.previousBackground;
+        this.className = sAtts.className;
+        this.favSubjects = sAtts.favSubjects;
+    }
+    listsSubjects(student) {
+        student.favSubjects.forEach(function(subject) {
+            return subject;
+        })
+    }
+    PRAssignment(subject) {
+        return `${this.name} has submitted a PR for ${subject}`;
+    }
+    sprintChallenge(subject) {
+        return `${this.name} has begun sprint challenge on ${subject}`;
     }
 }
 
 class ProjectManagers extends Instructors {
-    constructor(){
-        super();
+    constructor(pmAtts){
+        super(pmAtts);
+        this.gradClassName = pmAtts.gradClassName;
+        this.favInstructor = pmAtts.favInstructor;
+    }
+    standUp(channel) {
+        return `${this.name} announces to ${channel}, @channel standby times!`;
+    }
+    debugsCode(student, subject) {
+        return `${this.name} debugs ${student.name}'s code on ${subject}`;
     }
 }
