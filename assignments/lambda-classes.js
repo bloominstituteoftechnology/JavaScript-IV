574ee85ae931adca393b952d2f223c73ae14e56c
jjamaltwin <43683321+jjamaltwin@users.noreply.github.com>
2018-11-29 17:26:10 -0600

    tweaking Instructor class methods

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,13 +1,31 @@
 // CODE here for your Lambda Classes
 
-class people {
+class Person {
     constructor(props) {
         this.name = props.name;
         this.age = props.age;
         this.location = props.location;
         this.gender = props.gender;
+        this.student = props.student;
     }
     speak() {
         return `Hello my name is ${this.name}, I am from ${this.location} `;
+    }
+}
+
+class Instructor extends Person {
+    constructor(props) {
+        super(props);
+        this.speciality = props.speciality;
+        this.favLanguage = props.favLanguage;
+        this.cathPhrase = props.cathPhrase;
+        this.subject = props.subject;
+    }
+    demo() {
+        return `Today we are learning about ${this.subject}`;
+
+    }
+    grade() {
+        return `${this.student}`
     }
 }
