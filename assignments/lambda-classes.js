4532deef493445ffbbda8ef2da87e7c26f69291e
Marc Gutnayer <mgutnayer@gmail.com>
2018-11-29 19:53:47 -0600

    Completed Lambda-classes.js file and confirmed that it is working correctly

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -81,8 +81,45 @@ const fred = new Instructor({
     catchPhrase: `Don't forget the homies`
 })
 
+const marc = new Student({
+    name: 'Marc',
+    location: 'Chicago',
+    age: 32,
+    gender: 'Male',
+    previousBackground: ['Economics', 'Trading'],
+    className: 'FSW 16',
+    favSubjects: ['HTML', 'CSS', 'Javascript']
+})
+
+const sam = new ProjectManager({
+    name: 'Sam',
+    location: 'Los Angeles',
+    age: 27,
+    gender: 'Female',
+    specialty: 'redux',
+    favLanguage: 'Python',
+    catchPhrase: 'Programming is fun!',
+    gradClassName: 'CS13',
+    favInstructor: 'Josh'
+})
+
 // ==================== testing (Instructor class)
 console.log(fred);
 fred.speak();
 fred.demo("math");
-// fred.grade() -- needs a Student object
+fred.grade(marc, "history");
+
+// ==================== testing (Student class)
+console.log(marc);
+marc.speak();
+marc.listsSubjects();
+marc.PRAssignment("web development");
+marc.sprintChallenge("javascript");
+
+// ==================== testing (ProjectManager class)
+console.log(sam);
+sam.speak();
+sam.demo("algebra");
+sam.grade(marc, "programming");
+sam.standUp("FSW16");
+sam.debugsCode(marc, "programming");
