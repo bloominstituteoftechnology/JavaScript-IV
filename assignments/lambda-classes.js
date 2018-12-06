dd247d8632353b423b0016d07480548ce6c747d6
jjamaltwin <43683321+jjamaltwin@users.noreply.github.com>
2018-12-06 15:58:31 -0600

    finished first LAMBDA class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,2 +1,15 @@
 // CODE here for your Lambda Classes
-
+class Person {
+    constructor(props) {
+        this.name = "Jason";
+        this.age = 33;
+        this.location = "Chicago";
+        this.gender = "Male";
+        this.student = "FSW16 Grad!";
+    }
+    speak() {
+        console.log(`Hello my name is ${this.name}, I am from ${this.location} `);
+    }
+}
+const personnel = new Person("Jason");
+personnel.speak();
--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -1,3 +1,10 @@
+/* 
+Prototype Refactor
+1. Copy and paste your code or the solution from yesterday
+2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
+*/
+
+
 class GameObject {
     constructor(attribute) {
         this.createdAt = attribute.createdAt;
