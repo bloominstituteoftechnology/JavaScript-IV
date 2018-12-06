7cbad364478b6ca9243529c346453f49366d4219
drkfogle <rodriguez.kevin.094@gmail.com>
2018-12-06 18:50:45 -0500

    Updated classes with objects

--- assignments/lambda-classes.1.js
+++ assignments/lambda-classes.1.js
@@ -2,7 +2,7 @@
 class Person {
     constructor (attrs){
         this.name = attrs.name;
-        this.age = atts.age;
+        this.age = attrs.age;
         this.location = attrs.location;
         this.gender = attrs.gender;
     }
@@ -18,15 +18,15 @@ class Instructor extends Person {
         this.favLanguage = attrs.favLanguage;
         this.catchPhrase = attrs.catchPhrase;
     }
-    demo(){
-        return (`Today we are learning about ${this.subject}.`)
+    demo(subject){
+        return (`Today we are learning about ${subject}.`)
     }
-    grade(){
-        return (`${student.name} receives a perfect score on ${this.subject}.`)
+    grade(subject){
+        return (`${student.name} receives a perfect score on ${subject}.`)
     }
 }
 
-class Student extends Person {
+class Student extends Instructor {
     constructor (attrs){
         super(attrs);
         this.previousBackground = attrs.previousBackground;
@@ -36,10 +36,10 @@ class Student extends Person {
     listsSubjects(){
         return (`${this.favSubjects}`);
     }
-    PRAssignment(){
+    PRAssignment(subject){
         return (`${this.name} has submitted a PR for ${subject}.`);
     }
-    sprintChallenge(){
+    sprintChallenge(subject){
         return (`${this.name} has begun sprint challenge on ${subject}.`);
     }
 }
@@ -50,10 +50,84 @@ class Project_Manager extends Person {
         this.gradClassName = attrs.gradClassName;
         this.favInstructor = attrs.favInstructor;
     }
-    standUp(){
+    standUp(channel){
         return (`${this.name} announces to ${channel}, @channel stand time! `)
     }
-    debugsCode(){
+    debugsCode(subject){
         return (`${this.name} debugs ${this.name}'s code on ${subject}`)
     }
 }
+
+const fred = new Student({
+  name: 'Fred',
+  age: 34,
+  location: 'Albuquerque',
+  gender: 'Male',
+  previousBackground: 'Teacher',
+  className: 'fsd12',
+  favSubjects: 'Python, Ruby, Elixir, JS.'
+  
+})
+
+const tim = new Student({
+  name: 'Tim',
+  age: 18,
+  location: 'Oswego',
+  gender: 'Male',
+  previousBackground: 'Stock Boy',
+  className: 'cs13',
+  favSubjects: 'Spanish, English, French, CompSci.'
+})
+
+const tina = new Student({
+  name: 'Tina',
+  age: 19,
+  location: 'New Jersey',
+  gender: 'Female',
+  previousBackground: 'Cashier',
+  favSubjects: 'Engineering, Shop, Physics, Python, C#'
+})
+
+const rick = new Instructor({
+  name: 'Rick',
+  age: 43,
+  location: 'Who Knows?',
+  gender: 'Male',
+  specialty: 'Science',
+  favLanguage: 'Drunken English',
+  catchPhrase: 'IM PICKLE RICCCCCKKKKK!'
+})
+
+const jimbo = new Project_Manager({
+  name: 'Jim',
+  age: 21,
+  location: 'Madagascar',
+  gender: 'Male',
+  specialty: 'Not getting eaten by wild animals.',
+  favLanguage: 'PHP',
+  catchPhrase: 'Well, that just happened.',
+  gradClassName: 'fsw1',
+  favInstructor: 'Philip'
+})
+
+const terry = new Project_Manager({
+  name: 'Terry',
+  age: 25,
+  location: 'Philadelphia',
+  gender: 'Male',
+  specialty: 'Sleep',
+  favLanguage: 'Swift',
+  catchPhrase: 'SNORE',
+  favInstructor: 'Life',
+  gradClassName: 'fsw2'
+})
+
+const phil = new Instructor({
+  name: 'Philip',
+  age: 22,
+  location: 'California',
+  gender: 'Male',
+  specialty: 'Waking up Terry',
+  favLanguage: 'JS',
+  catchPhrase: '*bullhorn* hey, you awake Terry?'
+})
