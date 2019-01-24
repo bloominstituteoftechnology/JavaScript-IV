40327d610e48cf32fe4360c56e81a0f7abda8467
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-01-24 14:17:19 -0800

    Finished MVP now. Taking onto stretch

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -35,12 +35,13 @@ class Person{
   
   class Student extends Person{
     constructor (props) {
+        super(props);
         this.previousBackground = props.previousBackground;
         this.className = props.age;
         this.favSubjects = props.favSubjects;
     }
 
-    listsSubjects (){
+    listSubjects (){
     this.favSubjects.forEach(subject => {console.log(subject)}) ;
     }
 
@@ -52,13 +53,8 @@ class Person{
         console.log(`${this.name} has begun sprint challenge on ${subject}`)
     }
 }
-#### Project Mananger
 
-* ProjectManangers have the following Methods:
-  * `standUp` a method that takes in a slack channel and logs `{name} announces to {channel}, @channel standy times!​​​​​
-  * `debugsCode` a method that takes in a student object and a subject and logs out `{name} debugs {student.name}'s code on {subject}`
-
-class ProjectManagers extends Instructor{
+class ProjectManager extends Instructor{
     constructor(props){
         super(props);
         this.gradClassName = props.gradClassName;
@@ -74,4 +70,102 @@ class ProjectManagers extends Instructor{
     }
 
 }
+
+const austenAllred = new Person({
+    name: 'Austen',
+    location: 'Utah'
+});
+
+const ashtonKutcher = new Person({
+    name: 'Ashton',
+    location: 'Hollywood'
+});
+
+const omarSalah = new Student({
+    name: 'Omar',
+    age: 29,
+    location: 'Los Angeles',
+    gender: 'Male',
+    previousBackground: 'Security',
+    className: 'Web17',
+    favSubjects: ['Python', 'HTML', 'CSS', 'LESS', 'Javascript']
+});
+
+const javontayMcElroy = new Student({
+    name: 'Javontay',
+    favSubjects: ['HTML', 'CSS', 'LESS', 'Javascript']
+
+});
+
+const justinDavis = new Student({
+    name: 'Justin',
+    favSubjects: ['HTML', 'CSS', 'LESS', 'Javascript']
+});
+
+const joshKnell = new Instructor({
+    name: 'Josh',
+    gender: 'Male',
+    specialty: 'Front-end web development',
+    favLanguage: 'Javascript',
+    catchPhrase: 'Banjo'
+});
+
+const ryanHamblin = new Instructor({
+    name: 'Ryan',
+    location: 'Utah',
+    gender: 'Male',
+    specialty: 'Full-Stack web development',
+    favLanguage: 'Javascript'
+});
+
+const joeBugajski = new ProjectManager({
+    name: 'Joe',
+    gender: 'Male',
+});
+
+const ryanBoris = new ProjectManager({
+    name: 'Ryan',
+    gender: 'Male',
+});
+
+// Console.log Tests
+console.log('Person Tests');
+austenAllred.speak();
+ashtonKutcher.speak();
+
+
+console.log('Student Tests - Omar Salah');
+omarSalah.listSubjects();
+omarSalah.PRAssignment('Javascript IV');
+omarSalah.sprintChallenge('Javascript IV');
+
+
+console.log('Student Tests - Javontay McElroy ');
+javontayMcElroy.listSubjects();
+javontayMcElroy.PRAssignment('Javascript III');
+javontayMcElroy.sprintChallenge('Javascript III');
+
+
+
+console.log('Student Tests - Justin Davis ');
+justinDavis.listSubjects();
+justinDavis.PRAssignment('Javascript II');
+justinDavis.sprintChallenge('Javascript II');
+
+
+console.log('Instructor Tests - Josh Knell');
+joshKnell.demo('Javascript');
+joshKnell.grade(omarSalah, "Javscript IV");
+
+
+console.log('Instructor Tests - Ryan Hamblin');
+ryanHamblin.demo('Javascript');
+ryanHamblin.grade(omarSalah, "Javscript IV");
+
+
+console.log('Project Manager Tests - Ryan Borgis');
+ryanBoris.standUp('#web17_ryan');
+ryanBoris.debugsCode(omarSalah, 'Javascript IV');
+
+
   
