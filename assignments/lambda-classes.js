c9f804286709be756f703fd03150d582d1968da8
Wes-White <wesleyrwhite86@gmail.com>
2019-01-24 18:34:35 -0800

    Class person compelted

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,15 @@
 // CODE here for your Lambda Classes.
+
+
+class Person {
+    constructor (attributes){ 
+    this.name = attributes.name
+    this.age = attributes.age
+    this.location = attributes.location
+    this.gender = attributes.gender
+    }
+ 
+ speak () { 
+   console.log(`Hello my name is ${this.name}, I am from ${this.location}`)
+   }
+ }
