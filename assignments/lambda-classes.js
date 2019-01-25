64c04a200ffcfc385e971d57d6794ee1869f12ad
Wes-White <wesleyrwhite86@gmail.com>
2019-01-24 18:55:56 -0800

    Instructors

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -13,3 +13,20 @@ class Person {
    console.log(`Hello my name is ${this.name}, I am from ${this.location}`)
    }
  }
+
+ class Instructors extends Person{ 
+     constructor(instAttributes) {
+          super(instAttributes)
+          this.speciality = instAttributes.speciality
+          this.favLanguage = instAttributes.favLanguage
+          this.catchPhrase = instAttributes.catchPhrase
+     }
+
+     demo(subject) { 
+         console.log(`Today we are learning about ${subject}`)
+     }
+
+     grade(student, subject){ 
+         console.log(`${student.name} receives a perfect score on ${subject}'`)
+     }
+ }
