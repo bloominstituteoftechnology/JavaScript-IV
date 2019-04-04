a2f8113b230c3cc1eabd3c31a5970de20f1f1542
abtorres <abtorres079@gmail.com>
2019-04-04 15:26:18 -0600

    added all the classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,65 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(obj) {
+        this.name = obj.name;
+        this.age = obj.age;
+        this.location = obj.location;
+        this.gender = obj.gender;
+    } 
+    speak(){
+        return `Hello my name is ${this.name}, I am from ${this.location}`
+    }
+}
+
+class Instructor extends Person {
+    constructor(obj) {
+        super(obj);
+        this.specialty = obj.specialty;
+        this.favLanguage = obj.favLanguage;
+        this.catchPhrase = obj.catchPhrase;
+    }
+    demo(subject) {
+        console.log('Today we are learning about ' + subject)
+    }
+    grade(student) {
+        console.log(`${this.name} receives a perfect score on ${this.subject}`)
+    }
+}
+
+class Student extends Person {
+    constructor(obj) {
+        super(obj);
+        this.previousBackground = obj.previousBackground;
+        this.className = obj.className;
+        this.favSubjects = obj.favSubjects;
+    }
+    listSubjects() {
+
+    }
+    PRAssignment() {
+
+    }
+    sprintChallenge() {
+
+    }
+}
+
+class ProjectManagers extends Instructor {
+    constructor(obj) {
+        super(obj);
+        this.gradClassName = obj.gradClassName;
+        this.favInstructor = obj.favInstructor;
+    }
+    standUp() {
+
+    }
+    debugsCode() {
+
+    }
+}
+
+
+
+
+
+
