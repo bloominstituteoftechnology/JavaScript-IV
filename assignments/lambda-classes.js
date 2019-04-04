d162baff71b06451857b187dc244b1ac39a34d6c
Mylynh Nguyen <nmylynh@gmail.com>
2019-04-04 16:19:17 -0700

    Update lambda-classes.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,21 @@
 // CODE here for your Lambda Classes
+
+function Person(personAttributes)  {
+  this.name = personAttributes.name;
+  this.age = personAttributes.age;
+  this.location = personAttributes.location;
+  this.gender = personAttributes.gender;
+//methods
+
+  speak () {
+  	return `Hello my name is ${this.name}, I am from ${this.location}`;
+  }
+}
+
+const fred = new Person({
+  name: 'Fred',
+  age: 37,
+  location: 'Bedrock'
+});
+
+
