d88aca359857939868ac75d98c0a7b394d5fccfc
abtorres <abtorres079@gmail.com>
2019-04-04 14:55:15 -0600

    prototype refactoring done

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -1,4 +1,4 @@
-/* 
+/*
 
 Prototype Refactor
 
@@ -7,27 +7,19 @@ Prototype Refactor
 2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
 
 */
-
-
-
-
 /********************************************pasted code**********************************************/
-
-
-
 /*
-  Object oriented design is commonly used in video games. 
-  For this part of the assignment you will be implementing 
+  Object oriented design is commonly used in video games.
+  For this part of the assignment you will be implementing
   several constructor functions with their correct inheritance hierarchy.
 
-  In this file you will be creating three constructor functions: GameObject, CharacterStats, Humanoid.  
+  In this file you will be creating three constructor functions: GameObject, CharacterStats, Humanoid.
 
-  At the bottom of this file are 3 objects that all end up inheriting from Humanoid.  
+  At the bottom of this file are 3 objects that all end up inheriting from Humanoid.
   Use the objects at the bottom of the page to test your constructor functions.
   
   Each constructor function has unique properties and methods that are defined in their block comments below:
 */
-  
 /*
   === GameObject ===
   * createdAt
@@ -35,32 +27,36 @@ Prototype Refactor
   * dimensions (These represent the character's size in the video game)
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
-function GameObject(obj){
-    this.createdAt = obj.createdAt,
-    this.name = obj.name,
-    this.dimensions = obj.dimensions
-  }
+class GameObject {
+    constructor(obj) {
+        this.createdAt = obj.createdAt;
+        this.name = obj.name;
+        this.dimensions = obj.dimensions;
+    }
+    destroy() {
+        return `${this.name} was removed from the game.`;
+    }
+}
   
-  GameObject.prototype.destroy = function(){
-    return `${this.name} was removed from the game.`
-  }
   
-  /*
+/*
     === CharacterStats ===
     * healthPoints
     * takeDamage() // prototype method -> returns the string '<object name> took damage.'
     * should inherit destroy() from GameObject's prototype
   */
-  function CharacterStats(obj){
-    GameObject.call(this, obj),
-    this.healthPoints = obj.healthPoints
-  }
+class CharacterStats extends GameObject{
+    constructor(obj) {
+        super(obj);
+        this.healthPoints = obj.healthPoints;
+    }
+    takeDamage() {
+        return `${this.name} took damage.`;
+    }
+}
   CharacterStats.prototype = new GameObject(this)
-  CharacterStats.prototype.takeDamage = function(){
-    return `${this.name} took damage.`
-  }
   
-  /*
+/*
     === Humanoid (Having an appearance or character resembling that of a human.) ===
     * team
     * weapons
@@ -69,59 +65,69 @@ function GameObject(obj){
     * should inherit destroy() from GameObject through CharacterStats
     * should inherit takeDamage() from CharacterStats
   */
-  function Humanoid(obj){
-    CharacterStats.call(this, obj),
-    this.team = obj.team,
-    this.weapons = obj.weapons,
-    this.language = obj.language
-  }
+class Humanoid extends CharacterStats{
+    constructor(obj) {
+        super(obj);
+        this.team = obj.team;
+        this.weapons = obj.weapons;
+        this.language = obj.language;
+    }
+    greet() {
+        return `${this.name} offers a greeting in ${this.language}`;
+    }
+}
   Humanoid.prototype = new CharacterStats(this)
-  Humanoid.prototype.greet = function(){
-    return `${this.name} offers a greeting in ${this.language}`
-  }
   
-  //villain constructor
-  function Villain(obj){
-    Humanoid.call(this, obj)
-  }
+//villain constructor
+class Villain extends Humanoid{
+    constructor(obj) {
+        super(obj);
+    }
+    villainAttack() {
+        console.log(`${this.name} is attacking!`);
+        let attack = Math.floor(Math.random() * 11); // returns a random integer from 0 to 10 
+        hero.healthPoints = hero.healthPoints - attack;
+        if (attack > 0) {
+            console.log(`${this.name} hit for ` + attack + ' damage points.');
+        }
+        else {
+            console.log(`${this.name} missed.`);
+        }
+        ;
+        console.log(`${hero.name} has ${hero.healthPoints} health points left.`);
+        if (hero.healthPoints <= 0) {
+            fight = false;
+            console.log(hero.destroy());
+        }
+    }
+}
   Villain.prototype = new Humanoid(this)
-  Villain.prototype.villainAttack = function(){
-    console.log(`${this.name} is attacking!`);
-    let attack =  Math.floor(Math.random() * 11);      // returns a random integer from 0 to 10 
-    hero.healthPoints = hero.healthPoints - attack;
-    if(attack > 0){
-      console.log(`${this.name} hit for ` + attack + ' damage points.')
-    }else{
-      console.log(`${this.name} missed.`)
-    };
-    console.log(`${hero.name} has ${hero.healthPoints} health points left.`)
-    if(hero.healthPoints <= 0){
-      fight = false;
-      console.log(hero.destroy())
+  
+  
+//hero constructor
+class Hero extends Humanoid{
+    constructor(obj) {
+        super(obj);
     }
-  }
-  
-  
-  //hero constructor
-  function Hero(obj){
-    Humanoid.call(this, obj)
-  }
+    heroAttack() {
+        console.log(`${this.name} is attacking!`);
+        let attack = Math.floor(Math.random() * 11); // returns a random integer from 0 to 10 
+        villain.healthPoints = villain.healthPoints - attack;
+        if (attack > 0) {
+            console.log(`${this.name} hit for ` + attack + ' damage points.');
+        }
+        else {
+            console.log(`${this.name} missed.`);
+        }
+        ;
+        console.log(`${villain.name} has ${villain.healthPoints} health points left.`);
+        if (villain.healthPoints <= 0) {
+            fight = false;
+            console.log(villain.destroy());
+        }
+    }
+}
   Hero.prototype = new Humanoid(this)
-  Hero.prototype.heroAttack = function(){
-    console.log(`${this.name} is attacking!`);
-    let attack =  Math.floor(Math.random() * 11);      // returns a random integer from 0 to 10 
-    villain.healthPoints = villain.healthPoints - attack;
-    if(attack > 0){
-      console.log(`${this.name} hit for ` + attack + ' damage points.')
-    }else{
-      console.log(`${this.name} missed.`)
-    };
-    console.log(`${villain.name} has ${villain.healthPoints} health points left.`)
-    if(villain.healthPoints <= 0){
-      fight = false;
-      console.log(villain.destroy())
-    }
-  }
   
   
   //villain and hero objects
@@ -216,16 +222,16 @@ function GameObject(obj){
       language: 'Elvish',
     });
   
-    // console.log(mage.createdAt); // Today's date
-    // console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
-    // console.log(swordsman.healthPoints); // 15
-    // console.log(mage.name); // Bruce
-    // console.log(swordsman.team); // The Round Table
-    // console.log(mage.weapons); // Staff of Shamalama
-    // console.log(archer.language); // Elvish
-    // console.log(archer.greet()); // Lilith offers a greeting in Elvish.
-    // console.log(mage.takeDamage()); // Bruce took damage. 
-    // console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+    console.log(mage.createdAt); // Today's date
+    console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+    console.log(swordsman.healthPoints); // 15
+    console.log(mage.name); // Bruce
+    console.log(swordsman.team); // The Round Table
+    console.log(mage.weapons); // Staff of Shamalama
+    console.log(archer.language); // Elvish
+    console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+    console.log(mage.takeDamage()); // Bruce took damage. 
+    console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
     let fight = true;
     while(fight){
       if(villain.healthPoints > 0){
