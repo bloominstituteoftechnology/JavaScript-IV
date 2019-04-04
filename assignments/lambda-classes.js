f76f899c70c776e5a51d6104eecbff4b2829705d
victorN <victornguyen082@gmail.com>
2019-04-04 14:37:48 -0400

    prototype copy and lambda-classes Person and Instructor

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,44 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(attributes){
+        this.newAge = attributes.age;
+        this.newName = attributes.name;
+        this.newLocation = attributes.location;
+        this.newGender = attributes.gender;
+    }
+    speak() {
+        return `Hello, this is ${this.newName} and I am from ${this.newLocation}.`
+    }
+}
+class Instructor extends Person {
+    constructor(instructorAttr) {
+        super(instructorAttr);
+        this.newSpecialty = instructorAttr.specialty;
+    }
+}
+
+
+const god = new Person({
+    age: 'Old',
+    name: 'God',
+    location: 'Heaven',
+    gender: 'divine'
+});
+const josh = new Instructor({
+    age: 30,
+    name: 'Josh',
+    location: 'Utah',
+    gender: 'Male',
+    specialty: 'playing the banjo',
+    favLanguage: 'Hmong',
+    catchPhrase: 'I promise you yesterday was worse'
+});
+const josh = new Instructor({
+    age: 30,
+    name: 'Josh',
+    location: 'Utah',
+    gender: 'Male',
+    specialty: 'playing the banjo',
+    favLanguage: 'Hmong',
+    catchPhrase: 'I promise you yesterday was worse'
+});
