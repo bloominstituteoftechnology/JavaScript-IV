e9df30657edd321b1bc51e94f00973fb107a4e0d
Mylynh Nguyen <nmylynh@gmail.com>
2019-04-08 04:01:55 -0700

    Update lambda-classes.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,14 +1,35 @@
 // CODE here for your Lambda Classes
+class Person {
+  constructor(personAttributes) {
+      this.name = personAttributes.name;
+      this.age = personAttributes.age;
+      this.location = personAttributes.location;
+      this.gender = personAttributes.gender;
+  }
+
+  speak() {
+      return `Hello my name is ${this.name}, I am from ${this.location}`;
+  }
+}
+
+
+class CharacterStats extends GameObject {
+  constructor(csAttributes) {
+    super(csAttributes);
+    this.healthPoints = csAttributes.healthPoints;
+  }
+
+takeDamage() {
+  return `${this.name} took damage.`
+  }
+}
 
 function Person(personAttributes)  {
-  this.name = personAttributes.name;
-  this.age = personAttributes.age;
-  this.location = personAttributes.location;
-  this.gender = personAttributes.gender;
+
 //methods
 
   speak () {
-  	return `Hello my name is ${this.name}, I am from ${this.location}`;
+  	
   }
 }
 
