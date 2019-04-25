b76e52ef5a024ee4dd45ce4b2418cd0a1606c7fa
orlando rivera jr <oriverajr84@gmail.com>
2019-04-25 19:39:25 -0400

    MVP met

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -119,47 +119,47 @@ Prototype Refactor
       language: 'Common Tongue',
     });
   
-    // const swordsman = new Humanoid({
-    //   createdAt: new Date(),
-    //   dimensions: {
-    //     length: 2,
-    //     width: 2,
-    //     height: 2,
-    //   },
-    //   healthPoints: 15,
-    //   name: 'Sir Mustachio',
-    //   team: 'The Round Table',
-    //   weapons: [
-    //     'Giant Sword',
-    //     'Shield',
-    //   ],
-    //   language: 'Common Tongue',
-    // });
+    const swordsman = new Humanoid({
+      createdAt: new Date(),
+      dimensions: {
+        length: 2,
+        width: 2,
+        height: 2,
+      },
+      healthPoints: 15,
+      name: 'Sir Mustachio',
+      team: 'The Round Table',
+      weapons: [
+        'Giant Sword',
+        'Shield',
+      ],
+      language: 'Common Tongue',
+    });
   
-    // const archer = new Humanoid({
-    //   createdAt: new Date(),
-    //   dimensions: {
-    //     length: 1,
-    //     width: 2,
-    //     height: 4,
-    //   },
-    //   healthPoints: 10,
-    //   name: 'Lilith',
-    //   team: 'Forest Kingdom',
-    //   weapons: [
-    //     'Bow',
-    //     'Dagger',
-    //   ],
-    //   language: 'Elvish',
-    // });
+    const archer = new Humanoid({
+      createdAt: new Date(),
+      dimensions: {
+        length: 1,
+        width: 2,
+        height: 4,
+      },
+      healthPoints: 10,
+      name: 'Lilith',
+      team: 'Forest Kingdom',
+      weapons: [
+        'Bow',
+        'Dagger',
+      ],
+      language: 'Elvish',
+    });
   
     console.log(mage.createdAt); // Today's date
-    // console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
-    // console.log(swordsman.healthPoints); // 15
-    // console.log(mage.name); // Bruce
-    // console.log(swordsman.team); // The Round Table
-    // console.log(mage.weapons); // Staff of Shamalama
-    // console.log(archer.language); // Elvish
-    // console.log(archer.greet()); // Lilith offers a greeting in Elvish.
-    // console.log(mage.takeDamage()); // Bruce took damage.
-    // console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+    console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+    console.log(swordsman.healthPoints); // 15
+    console.log(mage.name); // Bruce
+    console.log(swordsman.team); // The Round Table
+    console.log(mage.weapons); // Staff of Shamalama
+    console.log(archer.language); // Elvish
+    console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+    console.log(mage.takeDamage()); // Bruce took damage.
+    console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
