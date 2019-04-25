03658eca9a91dd7e9f38f26b82920d74c51a0fbd
mikey <manicdesigner@gmail.com>
2019-04-25 09:49:14 -0400

    feat(lambda..) add Person Class Person class with properties and speak func added

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,12 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(props) {
+        this.name = props.name;
+        this.age = props.age;
+        this.location = props.location;
+        this.gender = props.gender;
+    }
+    speak() {
+        return `Hello my name is ${this.name}, I amd from ${this.location}.`;
+    }
+}
