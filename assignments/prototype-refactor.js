e38df60788f677390449014b4ea7dec3cf508cd7
mikey <manicdesigner@gmail.com>
2019-04-25 09:22:09 -0400

    refactor(proto-refac) removed unneeded code commented out things cleaner

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -14,14 +14,6 @@ Prototype Refactor
   * dimensions (These represent the character's size in the video game)
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
-// function GameObject(attributes) {
-//     this.createdAt = attributes.createdAt;
-//     this.name = attributes.name;
-//     this.dimensions = attributes.dimensions;
-// }
-// GameObject.prototype.destroy = function () {
-//     return `${this.name} was removed from the game.`;
-// }
 class GameObject {
     constructor(gameStats) {
         this.createdAt = gameStats.createdAt;
@@ -38,16 +30,6 @@ class GameObject {
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
-// function CharacterStats(stats) {
-//     GameObject.call(this, stats); // inheriting GameObject properties.
-//     this.healthPoints = stats.healthPoints;
-// }
-// // inherit destroy() from GameObject prototype
-// CharacterStats.prototype = Object.create(GameObject.prototype);
-
-// CharacterStats.prototype.takeDamage = function () {
-//     return `${this.name} took damage.`;
-// }
 class CharacterStats extends GameObject {
     constructor(stats) {
         super(stats);
@@ -67,16 +49,6 @@ class CharacterStats extends GameObject {
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
-// function Humanoid(attributes) {
-//     CharacterStats.call(this, attributes); // gameobject => characterstats inherited into Humanoid.
-//     this.team = attributes.team;
-//     this.weapons = attributes.weapons;
-//     this.language = attributes.language;
-// }
-// Humanoid.prototype = Object.create(CharacterStats.prototype); // inherit characterstats prototypes.
-// Humanoid.prototype.greet = function () {
-//     return `${this.name} offers a greeting in ${this.language}`;
-// }
 class Humanoid extends CharacterStats {
     constructor(attributes) {
         super(attributes);
@@ -88,8 +60,6 @@ class Humanoid extends CharacterStats {
         return `${this.name} offers a greeting in ${this.language}`;
     }
 }
-
-
 /*
  * Inheritance chain: GameObject -> CharacterStats -> Humanoid
  * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
@@ -99,20 +69,6 @@ class Humanoid extends CharacterStats {
 /** 
  * Stretch problems 
  */
-// function Hero(attributes) {
-//     Humanoid.call(this, attributes); // inherit Humanoid attributes.
-//     this.heroVillian = attributes.heroVillian;
-// }
-// Hero.prototype = Object.create(Humanoid.prototype); // Hero inherits Humanoid proto's.
-
-// Hero.prototype.attack = function (character) {
-//     if (character.healthPoints > 0) {
-//         character.healthPoints -= 2;
-//         return `${this.name} attacked ${character.name} of the ${character.team} and reduced their health to ${character.healthPoints}`;
-//     } else {
-//         return `${this.name} killed ${character.name} of ${character.team}. \n${character.destroy()}`;
-//     }
-// }
 class Hero extends Humanoid {
     constructor(props) {
         super(props);
@@ -127,21 +83,6 @@ class Hero extends Humanoid {
         }
     }
 }
-
-// function Villain(attributes) {
-//     Humanoid.call(this, attributes);
-//     this.heroVillian = attributes.heroVillian;
-// }
-// Villain.prototype = Object.create(Humanoid.prototype);
-
-// Villain.prototype.attack = function (character) {
-//     if (character.healthPoints > 0) {
-//         character.healthPoints -= 2;
-//         return `${this.name} attacked ${character.name} of the ${character.team} and reduced their health to ${character.healthPoints}`;
-//     } else {
-//         return `${this.name} killed ${character.name} of ${character.team}. \n${character.destroy()}`;
-//     }
-// }
 
 class Villain extends Humanoid {
     constructor(props) {
@@ -243,15 +184,20 @@ const bowser = new Villain({
     language: 'Elvish',
     heroVillian: 'villain'
 });
+/*
 console.log(bowser.attack(mario));
 console.log(bowser.attack(mario));
 console.log(bowser.attack(mario));
 console.log(bowser.attack(mario));
+console.log(mario.attack(bowser));
 console.log(bowser.attack(mario));
+console.log(mario.attack(bowser));
 console.log(bowser.attack(mario));
-//console.log(mario.attack(bowser));
+*/
+
 // console.log(healer);
 // console.log(mage);
+
 /*
 console.log(mage.createdAt); // Today's date
 console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
