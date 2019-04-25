beb1dd324b33690eb63fa44ad94384f91225a617
mikey <manicdesigner@gmail.com>
2019-04-25 11:07:00 -0400

    feat(lambda...) object created made Instructor object, comment out ProjectManager and building out Student

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -26,33 +26,45 @@ class Instructor extends Person {
     }
 }
 
-class Student extends Person {
-    constuctor(props) {
-        super(props);
-        this.previousBackground = props.previousBackground;
-        this.className = props.className;
-        this.favSubjects = props.favSubjects; // array of fav subjects.
-    }
-    // listSubjects() {
-    // }
-    PrAssignment(subject) {
-        console.log(`${this.name} has submitted a PR for ${subject}`);
-    }
-    sprintChallenge(subject) {
-        console.log(`${this.name} has begun sprint challenge on ${subject}`);
-    }
-}
+// class Student extends Person {
+//     constuctor(props) {
+//         super(props);
+//         this.previousBackground = props.previousBackground;
+//         this.className = props.className;
+//         this.favSubjects = props.favSubjects; // array of fav subjects.
+//     }
+//     // listSubjects() {
+//     // }
+//     PrAssignment(subject) {
+//         console.log(`${this.name} has submitted a PR for ${subject}`);
+//     }
+//     sprintChallenge(subject) {
+//         console.log(`${this.name} has begun sprint challenge on ${subject}`);
+//     }
+// }
 
-class ProjectManagers extends Instructor {
-    constructor(props) {
-        super(props);
-        this.gradClassName = props.gradClassName;
-        this.favInstructor = props.favInstructor;
-    }
-    standup(channel) {
-        console.log(`${this.name} announces to ${channel}, @channel standey times!`);
-    }
-    debugsCode(student, subject) {
-        console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
-    }
-}
+// class ProjectManagers extends Instructor {
+//     constructor(props) {
+//         super(props);
+//         this.gradClassName = props.gradClassName;
+//         this.favInstructor = props.favInstructor;
+//     }
+//     standup(channel) {
+//         console.log(`${this.name} announces to ${channel}, @channel standey times!`);
+//     }
+//     debugsCode(student, subject) {
+//         console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
+//     }
+// }
+
+const jordan = new Instructor({
+    name: 'Jordan',
+    age: 27,
+    location: 'Orlando',
+    gender: 'Male',
+    specialty: 'JavaScript',
+    favLanguage: 'Elm',
+    catchPhrase: 'That is how the news goes!',
+});
+
+jordan.demo('Javascript Objects');
