7db5a02408fbbebb633405988678f24ea43c059b
mikey <manicdesigner@gmail.com>
2019-04-25 09:57:49 -0400

    feat(lambda..) Intrcutor class extends Person with demo, grade functions added. fixed console.log from returns

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -7,6 +7,21 @@ class Person {
         this.gender = props.gender;
     }
     speak() {
-        return `Hello my name is ${this.name}, I amd from ${this.location}.`;
+        console.log(`Hello my name is ${this.name}, I amd from ${this.location}.`);
+    }
+}
+
+class Instructor extends Person {
+    constructor(props) {
+        super(props);
+        this.specialty = props.specialty;
+        this.favLanguage = props.favLanguage;
+        this.catchPhrase = props.catchPhrase;
+    }
+    demo(subject) {
+        console.log(`Today we are learning about ${subject}`);
+    }
+    grade(student, subject) {
+        console.log(`${student.name} recieves a perfect socre on ${subject}`);
     }
 }
