52812e79300a9328fcd0c3cc65cf6539d78688ba
littleknightgames <38872872+littleknightgames@users.noreply.github.com>
2019-05-09 13:40:43 -0400

    i forgot to commit this out i apologize. Though it is done..

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,102 @@
 // CODE here for your Lambda Classes
+class Person {
+  constructor(attributes) {
+    this.name = attributes.name;
+    this.age = attributes.age;
+    this.location = attributes.location;
+  }
+  Speak() {
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
+  }
+}
+class Instructor extends Person {
+  constructor(attributes) {
+    super(attributes);
+    this.specialty = attributes.specialty;
+    this.favLanguage = attributes.favLanguage;
+    this.catchPhrase = attributes.catchPhrase;
+  }
+  Demo(subject) {
+    return `Today we are learning about ${subject}`;
+  }
+  Grade(student, subject) {
+    return `${student.name} receives a perfect score on ${subject}`;
+  }
+}
+class Student extends Person {
+  constructor(attributes) {
+    super(attributes);
+    this.previousBackground = attributes.previousBackground;
+    this.className = attributes.className;
+    this.favSubjects = attributes.favSubjects;
+  }
+  listsSubjects() {
+    const subjects = this.favSubjects.map(
+      index => `${index} is one of ${this.name}'s favorite subjects`
+    );
+    return subjects;
+  }
+  PRAssignment(subject) {
+    return `${this.name} has submitted a PR for ${subject}`;
+  }
+  sprintChallenge(subject) {
+    return `${this.name} has begun sprint challenge on ${subject}`;
+  }
+}
+class ProjectManager extends Instructor {
+  constructor(attributes) {
+    super(attributes);
+    this.gradClassName = attributes.gradClassName;
+    this.favInstructor = attributes.favInstructor;
+  }
+  standUp(channel) {
+    return `${this.name} announces to ${channel}, @channel standy times!`;
+  }
+  debugsCode(student, subject) {
+    return `${this.name} debugs ${student.name}'s code on ${subject}`;
+  }
+}
+
+const student = new Student({
+  name: "bob",
+  age: "32",
+  location: "alabama",
+  previousBackground: "HTML",
+  className: "web21",
+  favSubjects: ["Html", "CSS", "JavaScript"]
+});
+const instructor = new Instructor({
+  name: "robert",
+  age: "32",
+  location: "alabama",
+  specialty: "react.js",
+  favLanguage: "python",
+  catchPhrase: "Don't put the pill in the drink bill"
+});
+const projectManager = new ProjectManager({
+  name: "Dan",
+  age: "32",
+  location: "alabama",
+  specialty: "react.js",
+  favLanguage: "python",
+  catchPhrase: "Don't put the pill in the drink bill",
+  gradClassName: "cs1",
+  favInstructor: "Sean"
+});
+console.log(student);
+console.log(student.Speak());
+console.log(student.listsSubjects());
+console.log(student.PRAssignment("WebDev"));
+console.log(student.sprintChallenge("WebDev"));
+console.log("\n\n");
+console.log(instructor);
+console.log(instructor.Speak());
+console.log(instructor.Demo("JS 4"));
+console.log(instructor.Grade(student, "JS 4"));
+console.log("\n\n");
+console.log(projectManager);
+console.log(projectManager.Speak());
+console.log(projectManager.Demo("JS 2"));
+console.log(projectManager.Grade(student, "JS 2"));
+console.log(projectManager.standUp("Sprint 3"));
+console.log(projectManager.debugsCode(student, "JS 2"));
--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -1,4 +1,4 @@
-/* 
+/*
 
 Prototype Refactor
 
@@ -7,3 +7,167 @@ Prototype Refactor
 2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
 
 */
+class GameObject {
+  constructor(object) {
+    this.createdAt = object.createdAt;
+    this.name = object.name;
+    this.dimensions = object.dimensions;
+  }
+  destroy() {
+    return `${this.name} was removed from the game.`;
+  }
+}
+
+class CharacterStats extends GameObject {
+  constructor(object) {
+    super(object);
+    this.healthPoints = object.healthPoints;
+  }
+  takeDamage() {
+    return `${this.name} took damage.`;
+  }
+}
+
+class Humanoid extends CharacterStats {
+  constructor(object) {
+    super(object);
+    this.team = object.team;
+    this.weapons = object.weapons;
+    this.language = object.language;
+  }
+  greet() {
+    return `${this.name} offers a greeting in ${this.language}.`;
+  }
+}
+
+const mage = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 1,
+    height: 1
+  },
+  healthPoints: 5,
+  name: "Bruce",
+  team: "Mage Guild",
+  weapons: ["Staff of Shamalama"],
+  language: "Common Tongue"
+});
+
+const swordsman = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 2,
+    height: 2
+  },
+  healthPoints: 15,
+  name: "Sir Mustachio",
+  team: "The Round Table",
+  weapons: ["Giant Sword", "Shield"],
+  language: "Common Tongue"
+});
+
+const archer = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 1,
+    width: 2,
+    height: 4
+  },
+  healthPoints: 10,
+  name: "Lilith",
+  team: "Forest Kingdom",
+  weapons: ["Bow", "Dagger"],
+  language: "Elvish"
+});
+
+console.log(mage.createdAt); // Today's date
+console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+console.log("\n" + swordsman.healthPoints + "\n"); // 15
+console.log(mage.name + "\n"); // Bruce
+console.log(swordsman.team + "\n"); // The Round Table
+console.log(mage.weapons + "\n"); // Staff of Shamalama
+console.log(archer.language + "\n"); // Elvish
+console.log(archer.greet() + "\n"); // Lilith offers a greeting in Elvish.
+console.log(mage.takeDamage() + "\n"); // Bruce took damage.
+console.log(swordsman.destroy() + "\n" + "\n" + "\n" + "\n"); // Sir Mustachio was removed from the game.
+
+// Stretch task:
+// * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.
+// * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
+// * Create two new objects, one a villain and one a hero and fight it out with methods!
+
+const Hero = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 1,
+    width: 2,
+    height: 4
+  },
+  healthPoints: 45,
+  name: "Hero",
+  team: "Good guy",
+  weapons: ["Bow", "Dagger"],
+  language: "Elvish"
+});
+
+const Villan = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 1,
+    width: 2,
+    height: 4
+  },
+  healthPoints: 1000,
+  name: "Villan",
+  team: "bad guy",
+  weapons: ["Dagger"],
+  language: "common"
+});
+
+console.log(Hero.greet() + "\n");
+console.log(Villan.greet() + "\n");
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+while (Villan.healthPoints > 0 || Hero.healthPoints > 0) {
+  let herodamage = getRandomInt(50);
+  let villandamage = getRandomInt(3);
+  if (Villan.healthPoints <= 0) {
+    break;
+  }
+  if (Hero.healthPoints <= 0) {
+    break;
+  }
+  if (Hero.healthPoints >= 0) {
+    if (herodamage === 0) {
+      console.log(`Hero misses`);
+    } else if (herodamage < 30) {
+      console.log(Villan.takeDamage());
+      console.log(`Hero deals ${herodamage} damage`);
+    } else {
+      console.log(Villan.takeDamage());
+      console.log(`Hero lands a critical hit dealing ${herodamage} damage`);
+    }
+    console.log(`Villan's health is ${(Villan.healthPoints -= herodamage)}\n`);
+  }
+  if (Villan.healthPoints >= 0) {
+    if (villandamage === 0) {
+      console.log(`Villan misses`);
+    } else {
+      console.log(Hero.takeDamage());
+      console.log(`Villan deals ${villandamage} damage`);
+    }
+    console.log(`Hero's health is ${(Hero.healthPoints -= villandamage)}\n`);
+  }
+}
+
+if (Hero.healthPoints <= 0) {
+  console.log(Hero.destroy());
+}
+if (Villan.healthPoints <= 0) {
+  console.log(Villan.destroy());
+}
