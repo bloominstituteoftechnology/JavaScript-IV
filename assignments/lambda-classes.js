c2a90676ab1f06f6a60ed957d450ee2fb1963ba1
Josemaria <josemarianriagu25@gmail.com>
2019-05-09 13:42:49 +0100

    - Create PM class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -29,7 +29,7 @@ class Person {
 
 class Instructor {
     constructor(name, age, location, gender, specialty, favLanguage, catchPhrase) {
-        super (name, age, location, gender),
+        super (name, age, location, gender);
         this.specialty = specialty,
         this.favLanguage = favLanguage,
         this.catchPhrase = catchPhrase
@@ -42,16 +42,16 @@ class Instructor {
     }
 }
 
-* Now we need some students!
-* Student uses the same attributes that have been set up by Person
-* Student has the following unique props:
-  * `previousBackground` i.e. what the Student used to do before Lambda School
-  * `className` i.e. CS132
-  * `favSubjects`. i.e. an array of the student's favorite subjects ['Html', 'CSS', 'JavaScript']
-* Student has the following methods:
-  * `listsSubjects` a method that logs out all of the student's favoriteSubjects one by one.
-  * `PRAssignment` a method that receives a subject as an argument and logs out that the `student.name has submitted a PR for {subject}`
-  * `sprintChallenge` similar to PRAssignment but logs out `student.name has begun sprint challenge on {subject}`
+// * Now we need some students!
+// * Student uses the same attributes that have been set up by Person
+// * Student has the following unique props:
+//   * `previousBackground` i.e. what the Student used to do before Lambda School
+//   * `className` i.e. CS132
+//   * `favSubjects`. i.e. an array of the student's favorite subjects ['Html', 'CSS', 'JavaScript']
+// * Student has the following methods:
+//   * `listsSubjects` a method that logs out all of the student's favoriteSubjects one by one.
+//   * `PRAssignment` a method that receives a subject as an argument and logs out that the `student.name has submitted a PR for {subject}`
+//   * `sprintChallenge` similar to PRAssignment but logs out `student.name has begun sprint challenge on {subject}`
 
 class Student {
     constructor(name, age, location, gender, previousBackground, className, favSubjects) {
@@ -71,15 +71,28 @@ class Student {
     }
 }
 
-  
-#### Project Manager
+// * Now that we have instructors and students, we'd be nowhere without our PM's
+// * ProjectManagers are extensions of Instructors
+// * ProjectManagers have the following unique props:
+//   * `gradClassName`: i.e. CS1
+//   * `favInstructor`: i.e. Sean
+// * ProjectManagers have the following Methods:
+//   * `standUp` a method that takes in a slack channel and logs `{name} announces to {channel}, @channel standy times!​​​​​
+//   * `debugsCode` a method that takes in a student object and a subject and logs out `{name} debugs {student.name}'s code on {subject}`
 
-* Now that we have instructors and students, we'd be nowhere without our PM's
-* ProjectManagers are extensions of Instructors
-* ProjectManagers have the following unique props:
-  * `gradClassName`: i.e. CS1
-  * `favInstructor`: i.e. Sean
-* ProjectManagers have the following Methods:
-  * `standUp` a method that takes in a slack channel and logs `{name} announces to {channel}, @channel standy times!​​​​​
-  * `debugsCode` a method that takes in a student object and a subject and logs out `{name} debugs {student.name}'s code on {subject}`
-class 
+// Create student object
+student = new Student(name, age, location, gender, previousBackground, className, favSubjects)
+
+class ProjectManagers {
+    constructor(name, age, location, gender, specialty, favLanguage, catchPhrase, gradClass, favInstructor) {
+        super(name, age, location, gender, specialty, favLanguage, catchPhrase),
+        this.gradClass = gradClass,
+        this.favInstructor = favInstructor
+    }
+    standUp(channel) {
+        return `${this.name} announces to ${channel}, @channel standy times!​​​​​`
+    }
+    debugsCode(student, subject) {
+        return `${this.name} debugs ${student.name}'s code on ${subject}`
+    }
+}
