14e7b0d8270190c6c57ba0e7ebe0fa5601638f1e
Josemaria <josemarianriagu25@gmail.com>
2019-05-09 12:33:40 +0100

    - Create Person class in lambda-classes.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,30 @@
 // CODE here for your Lambda Classes
+
+// * First we need a Person class. This will be our `base-class`
+// * Person receives `name` `age` `location` `gender` all as props
+// * Person receives `speak` as a method.
+// * This method logs out a phrase `Hello my name is Fred, I am from Bedrock` where `name` and `location` are the object's own props
+
+class Person {
+    constructor(name, age, location, gender) {
+        this.name = name,
+        this.age = age,
+        this.location = location,
+        this.gender = gender
+    }
+    speak() {
+        return `Hello my name is ${this.name}, I am from ${this.location}`
+    }
+}
+
+Now that we have a Person as our base class, we'll build our Instructor class.
+* Instructor uses the same attributes that have been set up by Person
+* Instructor has the following unique props:
+  * `specialty` what the Instructor is good at i.e. 'redux'
+  * `favLanguage` i.e. 'JavaScript, Python, Elm etc.'
+  * `catchPhrase` i.e. `Don't forget the homies`
+* Instructor has the following methods:
+  * `demo` receives a `subject` string as an argument and logs out the phrase 'Today we are learning about {subject}' where subject is the param passed in.
+  * `grade` receives a `student` object and a `subject` string as arguments and logs out '{student.name} receives a perfect score on {subject}'
+
+class
