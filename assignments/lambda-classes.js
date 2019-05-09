0cecd7682026fbf52541b4a6cc65764186275011
Josemaria <josemarianriagu25@gmail.com>
2019-05-09 12:53:39 +0100

    - Create Student class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -42,3 +42,44 @@ class Instructor {
     }
 }
 
+* Now we need some students!
+* Student uses the same attributes that have been set up by Person
+* Student has the following unique props:
+  * `previousBackground` i.e. what the Student used to do before Lambda School
+  * `className` i.e. CS132
+  * `favSubjects`. i.e. an array of the student's favorite subjects ['Html', 'CSS', 'JavaScript']
+* Student has the following methods:
+  * `listsSubjects` a method that logs out all of the student's favoriteSubjects one by one.
+  * `PRAssignment` a method that receives a subject as an argument and logs out that the `student.name has submitted a PR for {subject}`
+  * `sprintChallenge` similar to PRAssignment but logs out `student.name has begun sprint challenge on {subject}`
+
+class Student {
+    constructor(name, age, location, gender, previousBackground, className, favSubjects) {
+        super(name, age, location, gender),
+        this.previousBackground = previousBackground,
+        this.className = className,
+        this.favSubjects = favSubjects
+    }
+    listsSubjects() {
+        this.favSubjects.forEach((subject) => console.log(subject))
+    }
+    PRAssignment(subject) {
+        return `${this.name} has submitted a PR for ${subject}`
+    }
+    sprintChallenge(subject) {
+        return `${this.name} has begun sprint challenge on ${subject}`
+    }
+}
+
+  
+#### Project Manager
+
+* Now that we have instructors and students, we'd be nowhere without our PM's
+* ProjectManagers are extensions of Instructors
+* ProjectManagers have the following unique props:
+  * `gradClassName`: i.e. CS1
+  * `favInstructor`: i.e. Sean
+* ProjectManagers have the following Methods:
+  * `standUp` a method that takes in a slack channel and logs `{name} announces to {channel}, @channel standy times!​​​​​
+  * `debugsCode` a method that takes in a student object and a subject and logs out `{name} debugs {student.name}'s code on {subject}`
+class 
