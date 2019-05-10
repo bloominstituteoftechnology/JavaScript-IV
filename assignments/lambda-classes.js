7b46f27f83f8a507f241eea9ffb55cde991f3503
chrisdalao <chrisdalao@gmail.com>
2019-05-09 17:13:22 -0700

    stretch challenge completed but inefficient - but completed

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -23,6 +23,9 @@ class Instructor extends Person {
     grade(Student, subject) {
         return `${Student.name} receives a perfect score on ${subject}`
     }
+    pointCalc(Student) {
+        return Student.grade - Math.floor(Math.random() * 100)
+    }
 }
 
 class Student extends Instructor {
@@ -31,32 +34,44 @@ class Student extends Instructor {
         this.previousBackground = studentAttr.previousBackground;
         this.className = studentAttr.className;
         this.favSubjects = studentAttr.favSubjects;
+        this.grade = studentAttr.grade;
     }
     listsSubjects() {
         this.favSubjects.forEach((subjectListed) => {
             console.log(subjectListed);
         });
     }
-    PRAssignment(subject){
+    PRAssignment(subject) {
         return `${this.name} has submitted a PR for ${subject}`;
     }
-    sprintChallenge(subject){
+    sprintChallenge(subject) {
         return `${this.name} has begun sprint challenge on ${subject}`;
+    }
+    graduate(Student) {
+        Student.grade += Student.pointCalc(Student);
+        if (Student.grade > 100) {
+            console.log(`${Student.name} aced the test`);
+            return `${Student.name} has graduated!`;
+        } else {
+            Student.grade = Student.pointCalc(Student);
+            console.log(`${Student.name} received a score of ${Student.grade}%`);
+            return `go back and grade their assignments`;
+        }
     }
 
 }
 
 
-class ProjectManagers extends Instructor{
+class ProjectManagers extends Instructor {
     constructor(pmAttributes) {
         super(pmAttributes);
         this.gradClassName = pmAttributes.gradClassName;
         this.favInstructor = pmAttributes.favInstructor;
     }
-    standUp(channel){
+    standUp(channel) {
         return `${this.name} announces to ${channel}, @channel standy times!`;
     }
-    debugsCode(Student, subject){
+    debugsCode(Student, subject) {
         return `${this.name} debugs ${Student.name}'s code on ${subject}`;
     }
 }
@@ -67,7 +82,8 @@ const instructor = new Instructor({
 
 const student = new Student({
     'name': 'STUDENT NAME',
-    'favSubjects': ['listSubjects', 'METHOD', 'CALLED']
+    'favSubjects': ['listsSubjects', 'METHOD', 'CALLED'],
+    'grade': 100
 })
 
 const projectManager = new ProjectManagers({
@@ -75,10 +91,13 @@ const projectManager = new ProjectManagers({
 })
 
 
-console.log(instructor.demo("DEMO METHOD CALLED"));
-console.log(instructor.grade(student, "GRADE METHOD CALLED"));
-student.listsSubjects();
-console.log(student.PRAssignment("Javascript"));
-console.log(student.sprintChallenge("ES6"));
-console.log(projectManager.standUp("web20_sprint"));
-console.log(projectManager.debugsCode(student, "DEBUGS CODE CALLED"));
+// console.log(instructor.demo("DEMO METHOD CALLED"));
+// console.log(instructor.grade(student, "GRADE METHOD CALLED"));
+console.log(instructor.pointCalc(student));
+// student.listsSubjects();
+// console.log(student.PRAssignment("Javascript"));
+// console.log(student.sprintChallenge("ES6"));
+// console.log(student.grade);
+console.log(student.graduate(student));
+// console.log(projectManager.standUp("web20_sprint"));
+// console.log(projectManager.debugsCode(student, "DEBUGS CODE CALLED"));
