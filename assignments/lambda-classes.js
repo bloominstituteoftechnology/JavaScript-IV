468013b06e5d17a79a5938b21fcb5a86cf28d4ce
Carl Stanley <49922081+Carl-Stanley@users.noreply.github.com>
2019-05-29 09:49:56 -0700

    Adding classes, now.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -2,6 +2,18 @@
 
 
 // Class Person 
+class Person {
+    constructor(name,age,location){
+        this.name = name;
+        this.age = age;
+        this.location = location;
+    }
+
+    speak() {
+        console.log(`Hello my name is ${this.namename}, I am from ${this.location}.`);
+    } 
+}
+
 
 // Class Instructor
 
