81693092c2b0c53f5ba1a0f5f5318dbba1767ead
Carl Stanley <49922081+Carl-Stanley@users.noreply.github.com>
2019-05-29 10:30:45 -0700

    Commitin changes to classes to save work.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,3 +1,4 @@
+
 // CODE here for your Lambda Classes
 
 
@@ -10,18 +11,74 @@ class Person {
     }
 
     speak() {
-        console.log(`Hello my name is ${this.namename}, I am from ${this.location}.`);
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}.`);
     } 
 }
 
-const CorruptDonnie = new Person('Donnie',72,'Washington D.C.');
-
-CorruptDonnie.speak();
-
-
-
 // Class Instructor
+class Instructor extends Person {
+    constructor(specialty,favLanguage,catchPhrase){
+        super(name,age,location);
+        this.specialty = specialty;
+        this.favLanguage = favLanguage; 
+        this.catchPhrase = catchPhrase;
+    }
+
+    demo(subject) {
+
+        console.log(`Today we are learning about ${this.subject}`);
+
+    }
+
+    grade(student,subject) {
+
+        console.log(`${student.name} receives a perfect score on ${subject}`);
+
+    }
+}
+
 
 // Class Student
+class Student extends Person {
+    constructor(previousBackground,className,favSubjects) {
+        super(name,age,location);
+        this.previousBackground = previousBackground;
+        this.className = className;
+        this.favSubjects = favSubjects;
+    }
+
+    listsSubjects() {
+
+        console.log(this.favSubjects);
+
+    }
+
+    PRAssignment(student,subject) {
+
+        console.log(`${student.name} has submitted a PR for ${subject}`)
+    }
+
+    sprintChallenge(student,subject) {
+
+        console.log(`${student.name} has begun sprint challenge on ${subject}`)
+
+    }
+
+}
+
 
 // Class Project Manager 
+
+// Tests 
+
+// Testing Class Person 
+
+const CorruptDon = new Person('Don',72,'New York City, NY');
+
+CorruptDon.speak();
+
+// Testing Class Instructor
+
+// Testing Class Student
+
+// Testing Class Project Manager 
