baae209908644dcd451abac80450162d32cedcc5
noahfranco <noahfra@gmail.com>
2019-06-13 18:08:59 -0400

    helf-way done with this,js file and Prototypes file

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,96 @@
 // CODE here for your Lambda Classes
+//Person Class
+class Person {
+    constructor(acc) {
+        this.name = acc.name; 
+        this.age = acc.age; 
+        this.location = acc.location; 
+    }
+    speak() {
+        console.log(`Hi, my name is ${this.name} and I live in ${this.location}. I am ${this.age} years old`)
+    }
+}; 
+
+// Person Objects
+const Noah = new Person({
+    name: "Noah Franco", 
+    location: "Rockingham NC", 
+    age: 20
+});
+
+const Bob = new Person({
+    name: "Bob The Builder", 
+    location: "Sunflower Valley", 
+    age: 52
+}); 
+
+const Anakin = new Person({
+    name: "Anakin Skywalker", 
+    location: "Tatooine", 
+    age: 32
+}); 
+
+
+// Instructor Class
+class Instructor extends Person {
+    constructor(acc) {
+        super(acc); 
+        this.specialty = acc.specialty;
+        this.favLanguage = acc.favLanguage; 
+        this.catchPhrase = acc.catchPhrase; 
+    }
+    demo(subject) {
+        console.log(`Today we are learning about ${subject}`); 
+    }; 
+    grade(subject) {
+        console.log(`${student.name} receives a perfect score on ${subject}`)
+    };
+}
+
+
+//Instructor Objects 
+const Dan = new Instructor({
+    name: "Dan", 
+    age: Infinity, 
+    location: "Denver", 
+    specialty: "Relentless Debugger", 
+    favLanguage: "JavaScript", 
+    catchPhrase: "If you can do the thing, you can get paid to do the thing!"
+}); 
+
+const John = new Instructor({
+    name: "John Dio",
+    age: 99,
+    location: "NYC", 
+    specialty: "Not being found",
+    favLanguage: "C#", 
+    catchPhrase: "it's showtime"
+});
+
+const Albert = new Instructor({
+    name: "Albert Einstein",
+    age: 99,
+    location: "Ulm Germany", 
+    specialty: "Theoretical Physicist", 
+    favLanguage: "Python", 
+    catchPhrase: "Everybody is a genius. But if you judge a fish by its ability to climb a tree, it will live its whole life believing that it is stupid"
+}); 
+
+// Dan.demo("react")
+// Albert.grade("C#");
+
+
+
+
+// Students Class 
+class Students extends Person {
+    constructor(acc) {
+        super(acc); 
+        this.previousBackground = acc.previousBackgroundl; 
+        this.className = acc.className; 
+        this.favSubjects = acc.favSubjects; 
+    }; 
+    listsSubjects() {
+        console.log()
+    }
+} 
--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -7,3 +7,69 @@ Prototype Refactor
 2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
 
 */
+/*
+=== GameObject ===
+* createdAt
+* name
+* dimensions (These represent the character's size in the video game)
+* destroy() // prototype method that returns: `${this.name} was removed from the game.`
+*/
+
+/*
+function GameObject(attributes) {
+this.createdAt = attributes.createdAt; 
+this.name = attributes.name;
+this.dimensions = attributes.dimensions; 
+}
+
+GameObject.prototype.destroy = function() {
+return this.name + " " + "was removed from the game."; 
+}
+*/
+
+class GameObject {
+    constructor(attributes) {
+        this.createAt = attributes.createdAt; 
+        this.name = attributes.name; 
+        this.dimensions = attributes.dimensions 
+    }
+    
+}
+
+
+
+
+/*
+=== CharacterStats ===
+* healthPoints
+* takeDamage() // prototype method -> returns the string '<object name> took damage.'
+* should inherit destroy() from GameObject's prototype
+*/
+function CharacterStats(info) {
+GameObject.call(this,info); 
+this.healthPoints = info.healthPoints; 
+} 
+CharacterStats.prototype = Object.create(GameObject.prototype); 
+CharacterStats.prototype.takedamage = function() {
+return this.healthPoints + " " + this.name + " " + "took damage"
+}
+/*
+=== Humanoid (Having an appearance or character resembling that of a human.) ===
+* team
+* weapons
+* language
+* greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
+* should inherit destroy() from GameObject through CharacterStats
+* should inherit takeDamage() from CharacterStats
+*/
+
+function Humanoid(human) {
+CharacterStats.call(this,human); 
+this.team = human.team; 
+this.weapons = human.weapons; 
+this.language = human.language; 
+}
+Humanoid.prototype = Object.create(CharacterStats.prototype); 
+Humanoid.prototype.greet = function() {
+return this.name + " " + this.language;  
+}
