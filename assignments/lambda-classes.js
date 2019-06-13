88139457061550e0589f3fe515091c2ae6940aa8
Emily Lagattolla <elagattolla@gmail.com>
2019-06-13 15:34:09 -0500

    RPG update

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,47 @@
 // CODE here for your Lambda Classes
+
+// class Animal {
+//   constructor(name) {
+//     this.name = name;
+//   }
+//
+//   speak() {
+//     console.log(this.name + ' makes a noise.');
+//   }
+// }
+
+
+class Person {
+  constructor(attrs) {
+    this.name = name;
+    this.age = age;
+    this.location = location;
+  }
+
+  speak() {
+    console.log(`Hello, my name is ${this.name}, I am from ${this.location}`);
+  }
+}
+
+// class Dog extends Animal {
+//   constructor(name) {
+//     super(name);
+//   }
+//
+//   speak() {
+//     console.log(this.name + ' barks.');
+//   }
+// }
+
+class Student extends Person {
+  constructor(attrs) {
+    super(attrs);
+    this.previousBackground = previousBackground;
+    this.className = className;
+    this.favSubject = favSubjects;
+    this.listsSujects = listsSubjects;
+    this.PRAssignment = PRAssignment;
+  }
+
+
+}
