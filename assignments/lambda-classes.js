be5c301358b410d02bc67320c84288549bb8c528
Lou <antilou86@gmail.com>
2019-06-13 16:00:40 -0400

    WIP: created Person class and Instructor child within lambda-classes.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,47 @@
 // CODE here for your Lambda Classes
+// * Lambda personnel can be broken down into three different types of `people`.
+//   * **Instructors** - extensions of Person
+//   * **Students** - extensions of Person
+//   * **Project Managers** - extensions of Instructors
+// * **IMPORTANT** - You'll need to create 2 - 3 objects for each class and test them according to their unique Attributes. For example:
+
+
+
+
+// Person parent class
+
+class Person {
+    constructor(props) {
+        this.name=props.name;
+        this.age=props.age;
+    }
+    speak(){
+        console.log(`Hello, my name is ${this.name}, I am from ${this.location}`)
+    };
+}
+// Instructor- child of Person
+
+class Instructor extends Person {
+    constructor(props) {
+        super(props);
+        this.specialty= props.specialty;
+        this.favLanguage= props.favLanguage;
+        this.catchPhrase= props.catchPhrase;
+    }
+    demo(subject){
+        console.log(`Today we are learning about ${subject}`)
+    }
+    grade(student, subject){
+        console.log(`${student.name} receives a perfect score on ${subject}`);
+    }
+}
+
+// objects 
+const fred = new Instructor({
+    name: 'Fred',
+    location: 'Bedrock',
+    age: 37,
+    favLanguage: 'JavaScript',
+    specialty: 'Front-end',
+    catchPhrase: `Don't forget the homies`
+  });
