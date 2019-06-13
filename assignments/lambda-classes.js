2178cd69279c2c668f327ea62438d0fbb419e7b3
Davindar <davdavdav4@gmail.com>
2019-06-13 21:51:42 +0100

    class.js updated

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,30 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(attributes) {
+      this.age = attributes.age;
+      this.location = attributes.location;
+      this.name = attributes.name;
+    }
+    speak() {
+      return `Hello my name is ${this.name}, I am from ${this.location}`;
+    }
+    standUp(channel) {
+        return `${this.name} announces to ${channel}, @channel standy times!`;
+      }
+  }
+
+  class ProjectManager extends Person {
+    constructor(pmAttributes) {
+      super(pmAttributes);
+    }
+  }
+  
+  const sam = new ProjectManager({
+    name: "Sam",
+    age: "28",
+    location: "Wizardtown",
+    gradClassName: "WEB19"
+  });
+
+  console.log(sam.standUp("web21_Sam"));
+
