744c29aa95f2472606278fb4999c49e1f3445bbf
johnschneider1 <johnlawschneider@gmail.com>
2019-06-13 14:45:05 -0500

    second step in classes.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -6,8 +6,21 @@ class Person {
     this.location = location.attrs;
   }
   speak() {
-    return `Hello my name is Fred, I am from Bedrock where ${this.name} and ${
-      this.location
-    } are the objecth's`;
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
+  }
+}
+
+class Instructor extends Person {
+  constructor(attrs) {
+    super(attrs);
+    this.speciality = attrs.speciality;
+    this.favLanguage = attrs.favLanguage;
+    this.catchPhrase = attrs.catchPhrase;
+  }
+  demo(subject) {
+    return `Today we are learning about ${subject}`;
+  }
+  grade(student, subject) {
+    return `${student.name}, receives a perfect score on  ${subject} `;
   }
 }
