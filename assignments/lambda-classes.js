fba37b9c234320a98c101d6ef6ef37b0b339e677
Kelly M <moreira.kd@outlook.com>
2019-06-13 18:02:07 -0400

    new commit

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -22,8 +22,59 @@ class Instructor extends Person {
         super(InstructorAttrs);
         this.specialty = InstructorAttrs.specialty;
         this.favLanguage = InstructorAttrs.favLanguage;
-        this.catchPhrase = 
+        this.catchPhrase = InstructorAttrs.Dontforgetthehomies;
 
     }
+    demo (subject) {
+        return `Today we are learning about + ${this.subject}`;
+       
+    }
+
+    grade (subject, student) {
+        return `${this.student} received a perfect score on ${this.subject}`;
+    }
 }
+
+
+class Student extends Person {
+
+    constructor(StudentAttrs){
+        super(StudentAttrs);
+        this.previousBackground = StudentAttrs.previousBackground;
+        this.className = StudentAttrs.className;
+        this.favSubjects = StudentAttrs.favSubjects;
+    }
+}
+
+
+
+
+class ProjectManager extends Instructor {
+    constructor(ProjectManagerAttrs){
+        super(ProjectManagerAttrs);
+    }
+
+}
+
+
+const Instructor = new Instructor ({
+    favLanguage: 'python',
+    specialty: 'AI',
+    catchPhrase: 'Do not forget the homies'
+
+
+})
+
+instructor.speak();
+
+
+const Student = new Student ({
+    previousBackground: 'none',
+    className: 'web21',
+    favSubjects: ['HTML, CSS, Python']
+})
+
+
+
+
 
