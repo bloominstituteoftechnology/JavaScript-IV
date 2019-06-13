0038ac8bd08aa8552cc4c9cf5bc45709188c0710
Daniel Jones <daniel.jones2000@gmail.com>
2019-06-13 12:42:56 -0700

    formating lambda-classes.js.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,19 @@
 // CODE here for your Lambda Classes
+class person{
+    constructor(attributes){
+        
+    }
+}
+
+class insturctor{
+    constructor(insAttr){
+
+    }
+}
+
+class student{
+    constructor(stdAttr){
+
+    }
+}
+
