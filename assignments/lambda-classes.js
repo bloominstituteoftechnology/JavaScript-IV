ce265c55bdcd491293b07d2eaf0d993390b38730
Fullmetal235 <jjbillupsbrown@gmail.com>
2019-06-13 16:44:28 -0500

    Js-IV lambda classes changes.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -6,4 +6,22 @@ class person {
         this.gender = attributes.gender;
         this.location = attributes.location;
     }
+    speak() {
+        return 'Hello my name is ${this.name}, I am from ${this.location}';
+    }
+}
+
+class instructor extends person {
+    constructor(instructorAttributes){
+        super(instructorAttributes);
+        this.specialty = instructorAttributes.specialty;
+        this.favLanguage = instructorAttributes.favLanguage;
+        this.catchPhrase = instructorAttributes.catchPhrase;
+    }
+    demo(subject){
+        return 'Today we are learning about ${subject}';
+    }
+    grade(student, subject){
+        return '${student.name} receives a perfect score on ${subject}'
+    }
 }
