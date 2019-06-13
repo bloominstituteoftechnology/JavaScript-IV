9128b6584e629bb4d2c32bfe2bff7a41ab96b74a
Fullmetal235 <jjbillupsbrown@gmail.com>
2019-06-13 16:10:50 -0500

    Js-IV changes.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,9 @@
 // CODE here for your Lambda Classes
+class person {
+    constructor(attributes){
+        this.name = attributes.name;
+        this.age = attributes.age;
+        this.gender = attributes.gender;
+        this.location = attributes.location;
+    }
+}
