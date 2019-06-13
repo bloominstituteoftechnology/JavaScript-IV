fa72fa07902cbc673eb1a5c2f9e4cc49fa0c0520
Daniel Jones <daniel.jones2000@gmail.com>
2019-06-13 12:51:18 -0700

    finished person lambda-classe.sjs

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,19 +1,29 @@
 // CODE here for your Lambda Classes
 class person{
     constructor(attributes){
-        
+        this.name = attributes.name
+        this.age = attributes.age
+        this.location = attributes.location
+    }
+    speak(){
+        return `Hello my name is ${name}, I am from ${location}.`
     }
 }
 
-class insturctor{
+class insturctor extends person{
     constructor(insAttr){
 
     }
 }
 
-class student{
+class student extends person{
     constructor(stdAttr){
 
     }
 }
 
+class projectManager extends insturctor{
+    constructor(pmAttr){
+
+    }
+}
