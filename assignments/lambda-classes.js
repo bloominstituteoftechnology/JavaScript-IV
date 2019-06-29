263baeebea7b8f793ded21f0c40803116cb678c9
david-pok <david.pok@gmail.com>
2019-06-29 14:13:32 -0700

    completed

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,88 +1,118 @@
 // CODE here for your Lambda Classes
 class Person {
-  constructor(att) {
-    this.name = att.name;
-    this.age = att.age;
-    this.location = att.location;
+  constructor(options) {
+    this.name = options.name;
+    this.age = options.age;
+    this.location = options.location;
   }
 
   speak() {
-    return `Hello my name is ${this.name}, I am from ${this.location}`;
+    console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
   }
 }
 
 class Instructor extends Person {
-  constructor(opt) {
-    super(opt);
-    this.specialty = opt.specialty;
-    this.favLanguage = opt.favLanguage;
-    this.catchPhrase = opt.catchPhrase;
+  constructor(options) {
+    super(options);
+    this.specialty = options.specialty;
+    this.favLanguage = options.favLanguage;
+    this.catchPhrase = options.catchPhrase;
   }
 
   demo(subject) {
-    return "Today we are learning about ", subject;
+    console.log(`Today we are learning about , ${subject}`);
   }
 
-  grade(student, subject) {
-    return `${this.student} receives a perfect score on ${this.subject}`;
+  grade(Student, subject) {
+    console.log(`${Student.name} receives a perfect score on ${subject}`);
   }
 }
 
 class Student extends Person {
-  constructor(opti) {
-    super(opti);
-    this.previousBackground = opti.previousBackground;
-    this.className = opti.className;
-    this.favSubjects = [];
+  constructor(options) {
+    super(options);
+    this.previousBackground = options.previousBackground;
+    this.className = options.className;
+    this.favSubjects = options.favSubjects;
   }
 
   listSubjects() {
-    return this.favSubjects;
+    for (let i = 0; i < this.favSubjects.length; i++) {
+      console.log(this.favSubjects[i]);
+    }
   }
 
   PRAssignment(subject) {
-    return `${this.name} has submitted a PR for ${this.subject}`;
+    console.log(`${this.name} has submitted a PR for ${subject}`);
   }
 
   sprintChallenge(subject) {
-    return `${this.name} has begun sprint challenge on ${this.subject}`;
+    console.log(`${this.name} has begun sprint challenge on ${subject}`);
   }
 }
 
 class ProjectManager extends Instructor {
-  constructor(optio) {
-    super();
-    this.gradClassName = optio.gradClassName;
-    this.favInstructor = optio.favInstructor;
+  constructor(options) {
+    super(options);
+    this.gradClassName = options.gradClassName;
+    this.favInstructor = options.favInstructor;
   }
 
   standUp(channel) {
-    return `${this.name} announces to ${this.channel} @channel standy time!`;
+    console.log(`${this.name} announces to ${channel}, @channel standy time!`);
   }
 
-  debugsCode(student, subject) {
-    return `${this.name} debugs ${student.name}'s code on ${this.subject}`;
+  debugsCode(Student, subject) {
+    console.log(`${this.name} debugs ${Student.name}'s code on ${subject}`);
   }
 }
 
-const fred = new Instructor({
-  name: "Fred",
-  location: "Bedrock",
-  age: 37,
-  favLanguage: "JavaScript",
-  specialty: "Front-end",
-  catchPhrase: `Don't forget the homies`
-});
-
-const dav = new ProjectManager({
-  name: "David",
-  location: "Here",
-  age: 1,
+const dav = new Instructor({
+  name: "david",
+  age: 2,
+  location: "here",
+  specialty: "front",
   favLanguage: "JS",
-  specialty: "Front end",
-  catchPhrase: "Wut",
-  gradClassName: "CS100000",
-  favInstructor: "Luis"
+  catchPhrase: "Hello!"
 });
 
-fred.demo(wut);
+const mk = new Student({
+  name: "mark",
+  age: 3,
+  location: "there",
+  specialty: "back",
+  favLanguage: "python",
+  catchPhrase: "Goodbye!",
+  previousBackground: "sales",
+  className: "CS1",
+  favSubjects: ["node", "js", "python"]
+});
+
+const ty = new ProjectManager({
+  name: "tyrone",
+  age: 3,
+  location: "anywhere",
+  specialty: "design",
+  favLanguage: "css",
+  catchPhrase: "pow!",
+  gradClassName: "CS2",
+  favInstructor: "luis"
+});
+
+console.log(dav);
+dav.speak();
+dav.demo("node");
+dav.grade(mk, "node");
+
+console.log(mk);
+mk.speak();
+mk.listSubjects();
+mk.PRAssignment("C");
+mk.sprintChallenge("redux");
+
+console.log(ty);
+ty.speak();
+ty.demo("css");
+ty.grade(mk, "css");
+ty.standUp("cs5");
+ty.debugsCode(mk, "css");
