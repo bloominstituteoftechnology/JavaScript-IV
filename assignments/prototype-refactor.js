80f3072d7f539bcfd9f3fd00221fa73a5aea8fa0
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2019-07-25 14:26:54 -0700

    completed prototype

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -4,46 +4,43 @@ Prototype Refactor
 
 1. Copy and paste your code or the solution from yesterday
 
-2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
+2. Your goal is to refactor all of this code to use ES6 Classes. 
+The console.log() statements should still return what is expected of them.
 
 */
 
-function GameObject(obj){
+class GameObject {
+constructor(obj) {
     this.createdAt = obj.createdAt;
     this.dimensions = obj.dimensions;
-  }
-  GameObject.prototype.destroy = function () {
-    return `${this.name} was removed from the game.`
-  }
-  
-  
-  
-  function CharacterStats(obj) {
-      GameObject.call(this,obj);
-  
-      this.healthPoints = obj.healthPoints;
-      this.name = obj.name;
-  }
-  
-  CharacterStats.prototype = Object.create(GameObject.prototype);
-  
-  CharacterStats.prototype.takeDamage = function () {
-    return `${this.name} took damage`;
-  }
-  
- 
-   function Humanoid(obj) {
-     CharacterStats.call(this,obj);
-      this.team = obj.team;
-      this.weapons = obj.weapons;
-      this.language = obj.language;
-   }
-  
-   Humanoid.prototype = Object.create(CharacterStats.prototype);
-  
-   Humanoid.prototype.greet = function () {
-      return `${this.name} offers a greeting in ${this.language}`;
-   }
+    }
+destroy() {
+    return `${this.name} was removed from the game.`;
+}
+}
+
+
+class CharacterStats extends GameObject {
+    constructor(obj) {
+        super(obj);
+        this.healthPoints = obj.healthPoints;
+        this.name = obj.name;
+    }
+    takeDamage() {
+        return `${this.name} took damage`;
+    }
+}
+class Humanoid extends CharacterStats {
+    constructor(obj) {
+        super(obj);
+        this.team = obj.team;
+        this.weapons = obj.weapons;
+        this.language = obj.language;
+    }
+    greet() {
+        return `${this.name} offers a greeting in ${this.language}`;
+    }
+}
   
   
   
