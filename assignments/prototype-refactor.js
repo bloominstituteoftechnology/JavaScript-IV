648603434a520727afeffde43650ed8b7216a040
Jared Brown <jared@Jareds-MBP.lan1>
2019-10-10 20:20:48 -0700

    finished prototype refractor

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -3,7 +3,188 @@
 Prototype Refactor
 
 1. Copy and paste your code or the solution from yesterday
-
-2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
-
 */
+
+// function GameObject(attrs){
+//     this.createdAt = attrs.createdAt;
+//     this.name = attrs.name;
+//     this.dimensions = attrs.dimensions;
+//   }
+  
+//   GameObject.prototype.destroy = function(){`${this.name} was removed from the game.`}
+  
+  
+//   function CharacterStats(attrs){
+//     this.healthPoints = attrs.healthPoints;
+//     GameObject.call(this,attrs);
+//   }
+//   CharacterStats.prototype.takeDamage = function(){`${this.name} took damage.`};
+//   CharacterStats.prototype = Object.create(GameObject.prototype);
+
+//   function Humanoid(attrs){
+//     this.team = attrs.team;
+//     this.weapons = attrs.weapons;
+//     this.language = attrs.language;
+//     CharacterStats.call(this,attrs);
+//   }
+   
+//   Humanoid.prototype.greet = function(){`${this.name} offers a greeting in ${this.language}`};
+//   Humanoid.prototype = Object.create(CharacterStats.prototype);  
+  
+//     const mage = new Humanoid({
+//       createdAt: new Date(),
+//       dimensions: {
+//         length: 2,
+//         width: 1,
+//         height: 1,
+//       },
+//       healthPoints: 5,
+//       name: 'Bruce',
+//       team: 'Mage Guild',
+//       weapons: [
+//         'Staff of Shamalama',
+//       ],
+//       language: 'Common Tongue',
+//     });
+  
+//     const swordsman = new Humanoid({
+//       createdAt: new Date(),
+//       dimensions: {
+//         length: 2,
+//         width: 2,
+//         height: 2,
+//       },
+//       healthPoints: 15,
+//       name: 'Sir Mustachio',
+//       team: 'The Round Table',
+//       weapons: [
+//         'Giant Sword',
+//         'Shield',
+//       ],
+//       language: 'Common Tongue',
+//     });
+  
+//     const archer = new Humanoid({
+//       createdAt: new Date(),
+//       dimensions: {
+//         length: 1,
+//         width: 2,
+//         height: 4,
+//       },
+//       healthPoints: 10,
+//       name: 'Lilith',
+//       team: 'Forest Kingdom',
+//       weapons: [
+//         'Bow',
+//         'Dagger',
+//       ],
+//       language: 'Elvish',
+//     });
+  
+    // console.log(mage.createdAt); // Today's date
+    // console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+    // console.log(swordsman.healthPoints); // 15
+    // console.log(mage.name); // Bruce
+    // console.log(swordsman.team); // The Round Table
+    // console.log(mage.weapons); // Staff of Shamalama
+    // console.log(archer.language); // Elvish
+    // console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+    // console.log(mage.takeDamage()); // Bruce took damage.
+    // console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+  
+
+//2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
+
+class GameObject {
+  constructor(attrs) {
+    this.createdAt = attrs.createdAt;
+    this.name = attrs.name;
+    this.dimensions = attrs.dimensions;
+  }
+  destroy() {
+    return `${this.name} was removed from the game.`;
+  }
+}
+
+class CharacterStats extends GameObject {
+  constructor(attrs) {
+    super(attrs);
+    this.healthPoints = attrs.healthPoints;
+  }
+  takeDamage(){
+    return `${this.name} took damage.`;
+  }
+}
+
+class Humanoid extends CharacterStats {
+  constructor(attrs) {
+    super(attrs);
+    this.team = attrs.team;
+    this.weapons = attrs.weapons;
+    this.language = attrs.language;
+  }
+  greet(){
+    return `${this.name} offers a greeting in ${this.language}`;
+  }
+}
+
+    const mage = new Humanoid({
+      createdAt: new Date(),
+      dimensions: {
+        length: 2,
+        width: 1,
+        height: 1,
+      },
+      healthPoints: 5,
+      name: 'Bruce',
+      team: 'Mage Guild',
+      weapons: [
+        'Staff of Shamalama',
+      ],
+      language: 'Common Tongue',
+    });
+  
+    const swordsman = new Humanoid({
+      createdAt: new Date(),
+      dimensions: {
+        length: 2,
+        width: 2,
+        height: 2,
+      },
+      healthPoints: 15,
+      name: 'Sir Mustachio',
+      team: 'The Round Table',
+      weapons: [
+        'Giant Sword',
+        'Shield',
+      ],
+      language: 'Common Tongue',
+    });
+  
+    const archer = new Humanoid({
+      createdAt: new Date(),
+      dimensions: {
+        length: 1,
+        width: 2,
+        height: 4,
+      },
+      healthPoints: 10,
+      name: 'Lilith',
+      team: 'Forest Kingdom',
+      weapons: [
+        'Bow',
+        'Dagger',
+      ],
+      language: 'Elvish',
+    });
+
+console.log(mage.createdAt); // Today's date
+console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+console.log(swordsman.healthPoints); // 15
+console.log(mage.name); // Bruce
+console.log(swordsman.team); // The Round Table
+console.log(mage.weapons); // Staff of Shamalama
+console.log(archer.language); // Elvish
+console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+console.log(mage.takeDamage()); // Bruce took damage.
+console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
