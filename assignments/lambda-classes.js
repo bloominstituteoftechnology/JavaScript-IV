a23f8c312cf28dbffbad3d40a01aa1ee093a69dc
toddmurphy <toddmurphy93@gmail.com>
2019-10-17 16:54:00 -0300

    build main skeletons of classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,30 @@
 // CODE here for your Lambda Classes
+//Step 1: Create the classes for: Person, Instructor, Student, and Project Manager
+
+//'Person' is the base class
+class Person {
+  constructor(parentAttributes) {}
+}
+
+//'Instructor' class -> inherits(extends) from 'Person' class
+class Instructor extends Person {
+  constructor(instructorAttributes) {
+    //super connects intructorAttributes to Person
+    super(instructorAttributes);
+  }
+}
+
+//'Student' class -> inherits(extends) from the 'Person' class
+class Student extends Person {
+  constructor(studentAttributes) {
+    //super connects studentAttributes to the 'Person' class
+    super(studentAttributes);
+  }
+}
+
+//'Project Manager' class -> inherits(extends) from the 'Instructor' class
+class Project extends Instructor {
+  constructor(projectAttributes) {
+    super(projectAttributes);
+  }
+}
