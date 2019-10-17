7fde31a9b031111d1d2952a780f8e518051470d1
blakelower <blakelower1999@gmail.com>
2019-10-17 19:23:24 +0700

    made a class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,35 @@
-// CODE here for your Lambda Classes
+// // CODE here for your Lambda Classes
+// function LambdaPersonnel(attributes){
+//     this.instructor = attributes.instructor;
+//     this.students = attributes.students;
+//     this.projectManagers = attributes.projectManagers;
+// }
+// LambdaPersonnel.prototype.learn = function(){
+//     return `${this.instructor}: teaches Javascript, ${this.students}`;
+// }
+// function Kids(attributes) {
+//     LambdaPersonnel.call(this, attributes);
+// }
+// Kids.prototype = Object.create(LambdaPersonnel.prototype);
+
+// const webeu4 = new LambdaPersonnel({
+//     instructor: 'Remi',
+//     students: 'webeu4',
+//     projectManagers: 'Matt Locklin',
+// });
+
+// console.log(webeu4);
+
+class LambdaPersonnel{
+    constructor (attributes){
+        this.instructor = attributes.instructor;
+        this.students = attributes.students;
+        this.projectManagers = attributes.projectManagers;
+    }
+}
+const webeu4 = new LambdaPersonnel({
+    instructor: 'Remi',
+    students: 'webeu4',
+    projectManagers: 'Matt Locklin',
+});
+console.log(webeu4);
