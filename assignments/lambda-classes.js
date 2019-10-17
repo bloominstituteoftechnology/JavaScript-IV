468e0efd7761b3a5408acb4ed34af58d28677f43
blakelower <blakelower1999@gmail.com>
2019-10-17 20:39:18 +0700

    push up

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,25 +1,3 @@
-// // CODE here for your Lambda Classes
-// function LambdaPersonnel(attributes){
-//     this.instructor = attributes.instructor;
-//     this.students = attributes.students;
-//     this.projectManagers = attributes.projectManagers;
-// }
-// LambdaPersonnel.prototype.learn = function(){
-//     return `${this.instructor}: teaches Javascript, ${this.students}`;
-// }
-// function Kids(attributes) {
-//     LambdaPersonnel.call(this, attributes);
-// }
-// Kids.prototype = Object.create(LambdaPersonnel.prototype);
-
-// const webeu4 = new LambdaPersonnel({
-//     instructor: 'Remi',
-//     students: 'webeu4',
-//     projectManagers: 'Matt Locklin',
-// });
-
-// console.log(webeu4);
-
 class LambdaPersonnel{
     constructor (attributes){
         this.instructor = attributes.instructor;
@@ -27,7 +5,7 @@ class LambdaPersonnel{
         this.projectManagers = attributes.projectManagers;
     }
 }
-const webeu4 = new LambdaPersonnel({
+let webeu4 = new LambdaPersonnel({
     instructor: 'Remi',
     students: 'webeu4',
     projectManagers: 'Matt Locklin',
