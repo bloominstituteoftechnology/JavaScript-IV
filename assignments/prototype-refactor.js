691a8b7975f843e8361cf38588c8d1efe6ebc8b5
blakelower <blakelower1999@gmail.com>
2019-10-17 19:19:30 +0700

    did prototypes

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -7,3 +7,23 @@ Prototype Refactor
 2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
 
 */
+function LambdaPersonnel(attributes){
+    this.instructor = attributes.instructor;
+    this.students = attributes.students;
+    this.projectManagers = attributes.projectManagers;
+}
+LambdaPersonnel.prototype.learn = function(){
+    return `${this.instructor}: teaches Javascript, ${this.students}`;
+}
+function Kids(attributes) {
+    LambdaPersonnel.call(this, attributes);
+}
+Kids.prototype = Object.create(LambdaPersonnel.prototype);
+
+const webeu4 = new LambdaPersonnel({
+    instructor: 'Remi',
+    students: 'webeu4',
+    projectManagers: 'Matt Locklin',
+});
+
+console.log(webeu4);
