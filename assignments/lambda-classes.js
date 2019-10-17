6f83335f7ced8dbaa2cfdc1c631808ebab4249b1
blakelower <blakelower1999@gmail.com>
2019-10-17 20:51:39 +0700

    fixed bug

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,13 +1,19 @@
-class LambdaPersonnel{
+class Lambda{
     constructor (attributes){
         this.instructor = attributes.instructor;
         this.students = attributes.students;
         this.projectManagers = attributes.projectManagers;
     }
 }
-let webeu4 = new LambdaPersonnel({
+const webeu5 = new Lambda({
     instructor: 'Remi',
-    students: 'webeu4',
+    students: 'webeu5',
     projectManagers: 'Matt Locklin',
 });
+let webeu6 = new Lambda({
+    instructor: 'Petar',
+    students: 'webeu6',
+    projectManagers: 'Debra',
+});
 console.log(webeu4);
+console.log(webeu6);
