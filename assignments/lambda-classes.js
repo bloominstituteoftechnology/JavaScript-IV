4c51f3782e583b6687824b94d068d4b6571b73b1
jarrodskahill <jarrodskahill@gmail.com>
2019-10-17 12:28:23 -0700

    added person class(base)

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,12 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(info) {
+        this.name = info.name;
+        this.age = info.age;
+        this.location = info.location;
+    }
+    speak () {
+        return `Hello, my name is ${this.name}, I am from ${this.location} `
+    }
+    
+}
