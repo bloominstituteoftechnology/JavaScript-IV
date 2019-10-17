0d5f0209d147228d6f8eb0bf484324d22ae2a2c8
blakelower <blakelower1999@gmail.com>
2019-10-17 18:32:20 +0700

    made a class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,26 @@
 // CODE here for your Lambda Classes
+class borkers{
+    constructor(attributes){
+        this.name = attributes.name;
+        this.size = attributes.size;
+        this.weight = attributes.weight;
+        this.habitat = attributes.habitat;
+        this.color = attributes.color;
+        this.food = attributes.food;
+        this.sound = attributes.sound;
+    } 
+    eat(){
+        return `${this.name} goes to the park to eat ${this.food}`;
+    }
+}
+const flork = new borkers({
+    name: 'borky bork',
+    size: 'big tall',
+    weight: 'smol',
+    habitat: 'with the borker',
+    color: 'soft',
+    food: 'candy',
+    sound: 'snerp snerp snerp',
+});
+console.log(borkers);
+console.log(flork);
